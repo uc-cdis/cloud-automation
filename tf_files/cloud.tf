b198d6306ce140b0b5b80182df3efd0a1d53ec25
philloooo <ubuntu@pyt-dev-1.novalocal>
2017-01-12 21:54:29 +0000

    feat(variables): change some config to be variables

--- tf_files/cloud.tf
+++ tf_files/cloud.tf
@@ -1,11 +1,13 @@
 provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
 }
 
 resource "aws_vpc" "main" {
     cidr_block = "172.16.0.0/16"
     enable_dns_hostnames = true
     tags {
-        Name = "main"
+        Name = "${var.vpc_name}"
     }
 }
 
@@ -26,6 +28,18 @@ resource "aws_security_group" "ssh" {
       to_port = 22
       protocol = "TCP"
       cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "login-ssh" {
+  name = "login-ssh"
+  description = "security group that only enables ssh from login node"
+  vpc_id = "${aws_vpc.main.id}"
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "TCP"
+      cidr_blocks = ["${aws_instance.login.private_ip}/32"]
   }
 }
 
@@ -94,6 +108,19 @@ resource "aws_security_group" "out" {
   }
 }
 
+resource "aws_security_group" "proxy" {
+  name = "squid-proxy"
+  description = "allow inbound tcp at 3128"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+      from_port = 0
+      to_port = 3128
+      protocol = "TCP"
+      cidr_blocks = ["172.16.0.0/16"]
+  }
+}
+
 resource "aws_route_table" "public" {
     vpc_id = "${aws_vpc.main.id}"
     route {
@@ -145,87 +172,31 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_instance" "login" {
-    ami = "ami-d05e75b8"
+    ami = "${var.login_ami}"
     subnet_id = "${aws_subnet.public.id}"
     instance_type = "t2.micro"
     key_name = "${var.key_name}"
     monitoring = true
-    security_groups = ["${aws_security_group.ssh.id}", "${aws_security_group.out.id}"]
+    security_groups = ["${aws_security_group.ssh.id}", "${aws_security_group.local.id}"]
     tags {
         Name = "Login Node"
-    }
-    provisioner "file" {
-        source = "./conf"
-        destination = "/home/ubuntu"
-        connection {
-            type = "ssh"
-            user = "ubuntu"
-        }
-    }
-    provisioner "remote-exec" {
-        script = "script"
-        connection {
-            type = "ssh"
-            user = "ubuntu"
-        }
     }
 }
 
 resource "aws_instance" "proxy" {
-    ami = "ami-d05e75b8"
+    ami = "${var.proxy_ami}"
     subnet_id = "${aws_subnet.public.id}"
     instance_type = "t2.micro"
     key_name = "${var.key_name}"
     monitoring = true
-    security_groups = ["${aws_security_group.local.id}","${aws_security_group.ssh.id}", "${aws_security_group.out.id}"]
+    security_groups = ["${aws_security_group.proxy.id}","${aws_security_group.login-ssh.id}", "${aws_security_group.out.id}"]
     tags {
         Name = "HTTP Proxy"
-    }
-    provisioner "remote-exec" {
-        inline = [
-            "sudo apt-get update",
-            "sudo apt-get -y install squid3"
-        ]
-        connection {
-            type = "ssh"
-            user = "ubuntu"
-        }
-    }
-    provisioner "file" {
-        source = "./conf/squid.conf"
-        destination = "squid.conf"
-        connection {
-            type = "ssh"
-            user = "ubuntu"
-        }
-    }
-    provisioner "remote-exec" {
-        inline = [
-            "sudo mv squid.conf /etc/squid3/",
-            "sudo service squid3 restart"
-        ]
-        connection {
-            type = "ssh"
-            user = "ubuntu"
-        }
-
-    }
-}
-
-resource "aws_instance" "test" {
-    ami = "ami-d05e75b8"
-    subnet_id = "${aws_subnet.private.id}"
-    instance_type = "t2.micro"
-    key_name = "${var.key_name}"
-    monitoring = true
-    security_groups = ["${aws_security_group.local.id}"]
-    tags {
-        Name = "test"
     }
 }
 
 resource "aws_route53_zone" "main" {
-    name = "internal.com"
+    name = "internal.io"
     comment = "internal dns server"
     vpc_id = "${aws_vpc.main.id}"
 }
--- tf_files/variables.tf
+++ tf_files/variables.tf
@@ -1,3 +1,16 @@
 variable "key_name" {
     default = "admin"
 }
+variable "vpc_name" {
+    default = "test"
+}
+variable "aws_access_key" {
+}
+variable "aws_secret_key" {
+}
+variable "login_ami" {
+}
+variable "proxy_ami" {
+}
+variable "base_ami" {
+}
