32cd6f29efb5651674bb5467d6c5bbbc8d784bea
Reuben Pasquini <reuben@frickjack.com>
2017-11-13 11:36:27 -0600

    chore(Jenkins): wire up restore from s3

--- Jenkins/Docker/Jenkins/jenkins.sh
+++ Jenkins/Docker/Jenkins/jenkins.sh
@@ -2,12 +2,33 @@
 
 # No! set -e
 
-if [ ! -d "/var/jenkins_home/jobs" ]; then
-  # download latest backup from S3 - collected via Pipelines/Backup :-)
-  latestFile="$(aws s3 ls s3://cdis-terraform-state/JenkinsBackup/ | sort | tail -1 | awk '{ print $4}')"
-  aws s3 cp "s3://cdis-terraform-state/JenkinsBackup/$latestFile" /tmp/backup.tar.xz
-  tar xvf /tmp/backup.tar.xz -C /tmp
-  mv /tmp/var/jenkins_home/* /var/jenkins_home/
+if [ -z "$JENKINS_HOME" ]; then
+  # To simplify testing
+  JENKINS_HOME=/var/jenkins_home
 fi
 
-source /usr/local/bin/jenkins.sh
+if [ "$JENKINS_HOME" = "/tmp/var/jenkins_home" ]; then
+  echo "ERROR: JENKINS_HOME points at temp space that will be overwritten: $JENKINS_HOME"
+  exit 1
+fi
+
+if [ ! -d "$JENKINS_HOME/jobs" ]; then
+  if [ -d "$JENKINS_HOME" ]; then
+    # download latest backup from S3 - collected via Pipelines/Backup :-)
+    latestFile="$(aws s3 ls s3://cdis-terraform-state/JenkinsBackup/ | sort | tail -1 | awk '{ print $4}')"
+    S3PATH="s3://cdis-terraform-state/JenkinsBackup/$latestFile"
+    echo "Downloading $S3PATH"
+    aws s3 cp "$S3PATH" /tmp/backup.tar.xz
+    tar xvf /tmp/backup.tar.xz -C /tmp
+    mv /tmp/var/jenkins_home/* "$JENKINS_HOME/"
+  else
+    echo "Jenkins home not in expected location: /var/jenkins_home"
+  fi
+fi
+
+if [ -f /usr/local/bin/jenkins.sh ]; then
+  source /usr/local/bin/jenkins.sh
+else
+  echo "Jenkins startup script not where expected: /usr/local/bin/jenkins.sh"
+fi
+
