2ec3c8343a8d398a1ca607b52a42e68ed68c2f00
Reuben Pasquini <reuben@frickjack.com>
2018-01-17 17:39:44 -0600

    chore(tf_files/aws_user_vpc): user vpc setup

---
+++ tf_files/aws_user_vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_name" {
+    default = "Commons1"
+}
+variable "vpc_octet" {
+    default = 16
+}
+variable "aws_region" {
+    default = "us-east-1"
+}
+variable "aws_access_key" {
+}
+variable "aws_secret_key" {
+}
+# id of AWS account that owns the public AMI's
+variable "ami_account_id" {
+    default = "707767160287"
+}
