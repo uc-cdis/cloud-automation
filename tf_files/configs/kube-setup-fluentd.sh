0ff2907a3997b162818faa6f9f13f6a16aaafa58
Fauzi <fauzigo@gmail.com>
2018-02-16 17:39:03 -0600

    fix(fluentd): the daemonset is now applied by kube-service

---
+++ tf_files/configs/kube-setup-fluentd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# fluentd would allow log sending from pods to cloudwatch
+#
+
+set -e
+
+vpc_name=${vpc_name:-$1}
+if [ -z "${vpc_name}" ]; then
+   echo "Usage: bash kube-setup-revproxy.sh vpc_name"
+   exit 1
+fi
+if [ ! -d ~/"${vpc_name}" ]; then
+  echo "~/${vpc_name} does not exist"
+  exit 1
+fi
+
+cd ~/${vpc_name}
+sed -i "s/LGN/${vpc_name}/g"  services/fluentd/fluentd.yaml
+kubectl apply -f services/fluentd/fluentd.yaml
