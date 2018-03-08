2b401d8a2b86082a51d435ac0385c1c1d67e0ce7
Reuben Pasquini <reuben@frickjack.com>
2018-03-08 15:35:37 -0600

    fix(Docker/): remove build.sh and push.sh
    quay builds things for us

--- Docker/Jenkins/push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker tag cdis-jenkins:1.0.0 quay.io/cdis/jenkins:1.0.0
-docker push quay.io/cdis/jenkins:1.0.0
--- Docker/awshelper/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t cdis-awshelper:1.0.0 .
-
