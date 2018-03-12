3f371aed3c0c19ef1dbe95d3baf738dee1270a93
Reuben Pasquini <reuben@frickjack.com>
2018-03-12 11:37:19 -0500

    fix(csoc-admin-vm): forgotten provider variables

--- tf_files/csoc_admin_vm/variables.tf
+++ tf_files/csoc_admin_vm/variables.tf
@@ -1,3 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
 # id of AWS account that owns the public AMI's
 variable "ami_account_id" {
   # cdis-test
