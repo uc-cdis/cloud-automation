5f977645839ddc57e975af2b15bba4562c092b19
Reuben Pasquini <reuben@frickjack.com>
2018-03-13 09:47:57 -0500

    refactor(tf_files/aws_...): aws cred handling

--- tf_files/aws_user_vpc/root.tf
+++ tf_files/aws_user_vpc/root.tf
@@ -1,24 +1,20 @@
 terraform {
-    backend "s3" {
-        encrypt = "true"
-    }
+  backend "s3" {
+    encrypt = "true"
+  }
 }
 
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
+provider "aws" {}
 
 resource "aws_key_pair" "automation_dev" {
-    key_name = "${var.vpc_name}_automation_dev"
-    public_key = "${var.ssh_public_key}"
+  key_name   = "${var.vpc_name}_automation_dev"
+  public_key = "${var.ssh_public_key}"
 }
 
 module "cdis_vpc" {
   ami_account_id = "${var.ami_account_id}"
-  source = "../modules/cdis-aws-vpc"
-  vpc_octet = "${var.vpc_octet}"
-  vpc_name = "${var.vpc_name}"
-  ssh_key_name = "${aws_key_pair.automation_dev.key_name}"
+  source         = "../modules/cdis-aws-vpc"
+  vpc_octet      = "${var.vpc_octet}"
+  vpc_name       = "${var.vpc_name}"
+  ssh_key_name   = "${aws_key_pair.automation_dev.key_name}"
 }
--- tf_files/aws_user_vpc/variables.tf
+++ tf_files/aws_user_vpc/variables.tf
@@ -1,19 +1,18 @@
 variable "vpc_name" {
-    default = "Commons1"
+  default = "Commons1"
 }
+
 variable "vpc_octet" {
-    default = 14
+  default = 14
 }
+
 variable "aws_region" {
-    default = "us-east-1"
+  default = "us-east-1"
 }
-variable "ssh_public_key" {
-}
-variable "aws_access_key" {
-}
-variable "aws_secret_key" {
-}
+
+variable "ssh_public_key" {}
+
 # id of AWS account that owns the public AMI's
 variable "ami_account_id" {
-    default = "707767160287"
+  default = "707767160287"
 }
