99f7ebadf4c8138df30b4af9d8e81d5eb8a32f4f
Zac Flamig <zac@weatherwary.com>
2018-03-15 19:08:39 +0300

    feat(jupyterhub): JupyterHub Service (#212)
    * feat(jupyterhub): JupyterHub Service
    * chore(nginx): Pin image & clean rolling updates
    * docs(readme): for jupyterhub

---
+++ tf_files/configs/kube-setup-jupyterhub.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# The optional jupyterhub setup for workspaces
+
+set -e
+
+vpc_name=${vpc_name:-$1}
+if [ -z "${vpc_name}" ]; then
+   echo "Usage: bash kube-setup-jupyterhub.sh vpc_name"
+   exit 1
+fi
+if [ ! -d ~/"${vpc_name}" ]; then
+  echo "~/${vpc_name} does not exist"
+  exit 1
+fi
+
+cd ~/${vpc_name}
+
+# If you change this name you need to change it in the jupyterhub-config.yaml too
+namespaceName="jupyter-pods"
+
+# Create the namespace for user pods
+if ! kubectl get namespace "$namespaceName" > /dev/null 2>&1; then
+  echo "Creating k8s namespace: ${namespaceName}" 
+  kubectl create namespace "${namespaceName}"
+else
+  echo "I think k8s namespace ${namespaceName} already exists"
+fi
+
+
+kubectl apply -f services/jupyterhub/jupyterhub-config.yaml
+kubectl apply -f services/jupyterhub/jupyterhub-service.yaml
+kubectl apply -f services/jupyterhub/jupyterhub-storage.yaml
+kubectl apply -f services/jupyterhub/jupyterhub-deployment.yaml
+
