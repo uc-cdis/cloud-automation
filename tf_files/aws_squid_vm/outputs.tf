bc658fb081f3936a40d0922026fc991d9aaa6be9
Renuka Arya <renukarya@MacBook-Pro-53.local>
2018-03-28 12:52:06 -0500

    chore(squidvm): added the private ip as an output too

--- tf_files/aws_squid_vm/outputs.tf
+++ tf_files/aws_squid_vm/outputs.tf
@@ -3,3 +3,7 @@ output "squid_public_ip" {
 }
 
 
+output "squid_private_ip" {
+ value = "${module.squid_vm.squid_private_ip}"
+}
+
--- tf_files/modules/cdis-aws-squid/outputs.tf
+++ tf_files/modules/cdis-aws-squid/outputs.tf
@@ -4,3 +4,7 @@
 output "squid_public_ip" {
   value = "${aws_instance.proxy.public_ip}"
 }
+
+output "squid_private_ip" {
+  value = "${aws_instance.proxy.private_ip}"
+}
