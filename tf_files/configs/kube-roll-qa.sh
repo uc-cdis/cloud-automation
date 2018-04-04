7d1bc4dfd493a3a99c4aa4e63ac8e2637787dc21
Reuben Pasquini <reuben@frickjack.com>
2018-04-04 09:54:05 -0500

    chore(kube-roll-qa): script to roll qa namespaces

---
+++ tf_files/configs/kube-roll-qa.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# A little helper script that runs "g3k roll all"
+# against every 'qa-*' namespace.
+#
+_KUBE_ROLL_QA=$(dirname "${BASH_SOURCE:-$0}")  # $0 supports zsh
+export GEN3_HOME="${GEN3_HOME:-$(cd "${_KUBE_ROLL_QA}/../.." && pwd)}"
+
+if [[ -z "$_KUBES_SH" ]]; then
+  source "$GEN3_HOME/kube/kubes.sh"
+fi # else already sourced this file ...
+
+vpc_name=${vpc_name:-$1}
+
+if [ -z "${vpc_name}" ]; then
+  echo "ERROR: vpc_name variable not set - bailing out"
+  exit 1
+fi
+
+#
+# First - setup a clean and Jenkins-friendly workspace, so we don't inadvertantly
+#   load secrets off the file system into the cluster.
+#   We want to work under
+#
+export WORKSPACE="${WORKSPACE:-${XDG_RUNTIME_DIR:-/tmp}}"
+
+namespaceList=$(g3kubectl get namespace -o json | jq -r '.items[].metadata.name')
+for name in $namespaceList; do
+  if [[ "$name" == "default" || $name =~ ^qa-.+$ ]]; then
+    echo $name
+    (
+      export WORKSPACE=$(mktemp -d -p "$WORKSPACE" "qaroll_${name}_XXXXXX")
+      # g3kubectl keys on KUBECTL_NAMESPACE environment variable
+      export KUBECTL_NAMESPACE="$name"
+      cd "$WORKSPACE"
+      echo "Rolling namespace $name"
+      g3k roll all
+      echo "-------------"
+      echo ""
+    )
+  fi
+done
