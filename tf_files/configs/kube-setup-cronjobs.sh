ecbf0f93529fddb21656bed6e4de841e06d01167
avantol <avantol@uchicago.edu>
2018-04-10 09:39:33 -0500

    fix(path): fix path to yaml files for k8s jobs for google

--- tf_files/configs/kube-setup-cronjobs.sh
+++ tf_files/configs/kube-setup-cronjobs.sh
@@ -11,9 +11,9 @@ export WORKSPACE="${WORKSPACE:-$HOME}"
 export GEN3_HOME="${GEN3_HOME:-$(cd "${_KUBE_SETUP_CRONJOBS}/../.." && pwd)}"
 
 if ! kubectl get cronjob google-manage-keys > /dev/null 2>&1; then
-   kubectl create -f "${GEN3_HOME}/kube/services/jenkins/google-manage-keys-cronjob.yaml"
+   kubectl create -f "${GEN3_HOME}/kube/services/jobs/google-manage-keys-cronjob.yaml"
 fi
 
 if ! kubectl get cronjob google-manage-account-access > /dev/null 2>&1; then
-   kubectl create -f "${GEN3_HOME}/kube/services/jenkins/google-manage-account-access-cronjob.yaml"
+   kubectl create -f "${GEN3_HOME}/kube/services/jobs/google-manage-account-access-cronjob.yaml"
 fi
