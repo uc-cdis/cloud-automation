147a688e3d33c0a91942a390c48e6d0767abab1e
Renuka Arya <renukarya@MacBook-Pro-53.local>
2018-04-24 10:09:32 -0500

    chrore(dbroute): fixing route

--- tf_files/aws/cloud.tf
+++ tf_files/aws/cloud.tf
@@ -67,10 +67,9 @@ resource "aws_route_table" "private_kube" {
   }
 }
 
-resource "aws_default_route_table" “default” {
+resource "aws_default_route_table" "default" {
   vpc_id = "${module.cdis_vpc.vpc_id}"
   #default_route_table_id = "${aws_vpc.foo.default_route_table_id}"
-
   route {
     #from the commons vpc to the csoc vpc via the peering connection
     cidr_block                = "${var.csoc_cidr}"
@@ -87,7 +86,7 @@ resource "aws_route_table_association" "private_kube" {
   route_table_id = "${aws_route_table.private_kube.id}"
 }
 
-resource "aws_main_route_table_association" “default” {
+resource "aws_main_route_table_association" "default" {
   subnet_id      = "${aws_subnet.private_db_alt.id}"
   route_table_id = "${aws_default_route_table.default.id}"
   vpc_id = "${module.cdis_vpc.vpc_id}"
