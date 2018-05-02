f101d549bcb4c831a7ccb589ae6f75db7a4fac03
Reuben Pasquini <reuben@frickjack.com>
2018-05-02 14:26:00 -0500

    refactor(kubes.sh): restore place holder
    for legacy .bashrc support

---
+++ kube/kube.sh
@@ -0,0 +1,7 @@
+#
+# Here for legacy reasons - prefer to source gen3setup.sh directly
+#
+g3kScriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE:-$0}")")
+export GEN3_HOME="${GEN3_HOME:-$(dirname "$g3kScriptDir")}"
+
+source "${GEN3_HOME}/gen3/gen3setup.sh"
