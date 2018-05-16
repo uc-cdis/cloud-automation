b5f0d529bc43390ea911836a9cfe9aea811635aa
Fauzi <fauzigo@gmail.com>
2018-05-16 08:45:40 -0500

    add(nginx config): AWS ES cluster ready

--- flavors/nginx/es_revproxy.sh
+++ flavors/nginx/es_revproxy.sh
@@ -20,6 +20,36 @@ time_zone = LOCAL
 log_group_name = ${HOSTNAME}
 EOM
 
+cat > /etc/nginx/sites-enabled/default  <<EOF
+
+server {
+        listen 80;
+        listen [::]:80;
+        server_name _;
+        auth_basic "Restricted Content";
+        auth_basic_user_file /etc/nginx/.htpasswd;
+        location / {
+                proxy_http_version      1.1;
+                proxy_set_header        Host https://search-commons-logs-lqi5sot65fryjwvgp6ipyb65my.us-east-1.es.amazonaws.com:443/; #$host;
+                proxy_set_header        Connection "Keep-Alive";
+                proxy_set_header        Proxy-Connection "Keep-Alive";
+                proxy_set_header        Authorization "";
+                proxy_set_header        X-Real-IP 35.174.124.219;
+                proxy_pass              https://search-commons-logs-lqi5sot65fryjwvgp6ipyb65my.us-east-1.es.amazonaws.com:443/;
+                proxy_redirect          https://search-commons-logs-lqi5sot65fryjwvgp6ipyb65my.us-east-1.es.amazonaws.com:443/ https://35.174.124.219/_plugin/kibana/;
+        }
+        location ~ (/app/kibana|/app/timelion|/bundles|/es_admin|/plugins|/api|/ui|/elasticsearch) {
+                proxy_pass              https://search-commons-logs-lqi5sot65fryjwvgp6ipyb65my.us-east-1.es.amazonaws.com;
+                proxy_set_header        Host $host;
+                proxy_set_header        X-Real-IP $remote_addr;
+                proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header        X-Forwarded-Proto $scheme;
+                proxy_set_header        X-Forwarded-Host $http_host;
+                proxy_set_header        Authorization  "";
+        }
+}
+EOF
+
 chmod 755 /etc/init.d/awslogs
 systemctl enable awslogs
 systemctl restart awslogs
