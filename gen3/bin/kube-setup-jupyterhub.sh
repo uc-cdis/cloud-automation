1b5b4faeaa664dd7750bf8164ce6d047b9cca315
Zac Flamig <Zac@WeatherWary.com>
2018-05-20 20:56:15 -0500

    feat(jupyter): More tweaks...

--- gen3/bin/kube-setup-jupyterhub.sh
+++ gen3/bin/kube-setup-jupyterhub.sh
@@ -29,6 +29,7 @@ g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/serviceaccount.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/role-jupyter.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/rolebinding-jupyter.yaml"
 
+g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-prepuller.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-config.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-service.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-storage.yaml"
