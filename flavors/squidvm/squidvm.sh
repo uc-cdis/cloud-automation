15594908da3ded672d4dcd12ba506553a973cd95
Renuka Arya <renukarya@MacBook-Pro-53.local>
2018-05-25 14:12:27 -0500

    chore(squidvm): minor fix to squidvm.sh

--- flavors/squidvm/squidvm.sh
+++ flavors/squidvm/squidvm.sh
@@ -4,16 +4,17 @@
 sudo sed -i '/proxy/ d' /etc/environment
 sudo rm /etc/apt/apt.conf.d/01proxy
 
-SUB_FOLDER="/home/ubuntu/cloud-automation/"
-PUBLIC_IP="35.174.124.219"
-MAGIC_URL="http://169.254.169.254/latest/meta-data/"
+#SUB_FOLDER="/home/ubuntu/cloud-automation/"
+#PUBLIC_IP="35.174.124.219"
+#MAGIC_URL="http://169.254.169.254/latest/meta-data/"
 
 #CSOC-ACCOUNT-ID=$(${AWS} sts get-caller-identity --output text --query 'Account')
-sudo apt install -y curl jq python-pip apt-transport-https ca-certificates software-properties-common fail2ban libyaml-dev
-sudo pip install --upgrade pip
-ACCOUNT_ID=$(curl -s ${MAGIC_URL}iam/info | jq '.InstanceProfileArn' |sed -e 's/.*:://' -e 's/:.*//')
+#sudo apt install -y curl jq python-pip apt-transport-https ca-certificates software-properties-common fail2ban libyaml-dev
+#sudo pip install --upgrade pip
+#ACCOUNT_ID=$(curl -s ${MAGIC_URL}iam/info | jq '.InstanceProfileArn' |sed -e 's/.*:://' -e 's/:.*//')
 
 ## squid proxy set-up
+cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y build-essential wget libssl-dev
 wget http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.26.tar.xz
