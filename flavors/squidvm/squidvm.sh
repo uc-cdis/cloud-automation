2203589622a3a6c356e9b23db98d54339d1d9c58
Renuka Arya <renukarya@MacBook-Pro-53.local>
2018-05-25 13:32:34 -0500

    chore(squidutility): some changes to the squid utility VM script

--- flavors/squidvm/squidvm.sh
+++ flavors/squidvm/squidvm.sh
@@ -19,4 +19,42 @@ wget http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.26.tar.xz
 tar -xJf squid-3.5.26.tar.xz
 mkdir squid-build
 
+git clone https://github.com/uc-cdis/images.git
 
+cp /home/ubuntu/images/configs/ftp_whitelist /tmp/ftp_whitelist
+cp /home/ubuntu/images/configs/web_whitelist /tmp/web_whitelist
+cp /home/ubuntu/images/configs/web_wildcard_whitelist /tmp/web_wildcard_whitelist
+cp /home/ubuntu/images/configs/squid.conf /tmp/squid.conf
+cp /home/ubuntu/images/configs/squid-build.sh /home/ubuntu/squid-build/squid-build.sh
+cp /home/ubuntu/images/configs/iptables.conf /tmp/iptables.conf
+cp /home/ubuntu/images/configs/iptables-rules /tmp/iptables-rules
+cp /home/ubuntu/images/configs/squid.service /tmp/squid.service
+
+cd /home/ubuntu/squid-build/
+bash squid-build.sh
+sudo make install
+
+sudo mv /tmp/ftp_whitelist /etc/squid/ftp_whitelist
+sudo mv /tmp/web_whitelist /etc/squid/web_whitelist
+sudo mv /tmp/web_wildcard_whitelist /etc/squid/web_wildcard_whitelist
+sudo mv /tmp/squid.conf /etc/squid/squid.conf
+sudo mv /tmp/iptables.conf /etc/iptables.conf
+sudo mv /tmp/iptables-rules /etc/network/if-up.d/iptables-rules
+
+sudo chown root: /etc/network/if-up.d/iptables-rules
+sudo chmod 0755 /etc/network/if-up.d/iptables-rules
+
+sudo mkdir /etc/squid/ssl
+sudo openssl genrsa -out /etc/squid/ssl/squid.key 2048
+sudo openssl req -new -key /etc/squid/ssl/squid.key -out /etc/squid/ssl/squid.csr -subj '/C=XX/ST=XX/L=squid/O=squid/CN=squid'
+sudo openssl x509 -req -days 3650 -in /etc/squid/ssl/squid.csr -signkey /etc/squid/ssl/squid.key -out /etc/squid/ssl/squid.crt
+sudo cat /etc/squid/ssl/squid.key /etc/squid/ssl/squid.crt | sudo tee /etc/squid/ssl/squid.pem
+sudo mv /tmp/squid.service /etc/systemd/system/
+sudo chmod 0755 /etc/systemd/system/squid.service
+sudo mkdir -p /var/log/squid /var/cache/squid
+sudo chown -R proxy:proxy /var/log/squid /var/cache/squid
+
+echo 'enabling squid with systemd'
+sudo systemctl enable squid
+sudo service squid stop
+sudo service squid start
