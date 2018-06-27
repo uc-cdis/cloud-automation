4c9bf9a4223278a8b9fc8f179aa699debfcac5c7
Renuka Arya <renukarya@MacBook-Pro-53.local>
2018-06-27 12:03:01 -0500

    chore(squidnlb: changes to the updatewhitelish.sh and commons code  to add the vpc endpoint config.tvars variable

--- gen3/lib/aws.sh
+++ gen3/lib/aws.sh
@@ -345,6 +345,7 @@ vpc_name="$GEN3_WORKSPACE"
 #
 vpc_octet2=GET_A_UNIQUE_VPC_172_OCTET2
 vpc_octet3=GET_A_UNIQUE_VPC_172_OCTET3
+squid-nlb-endpointservice-name=CSOC_SQUID_NLB_SERVICE_NAME
 dictionary_url="https://s3.amazonaws.com/dictionary-artifacts/YOUR/DICTIONARY/schema.json"
 portal_app="dev"
 
