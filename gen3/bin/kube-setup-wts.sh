68d8b49b5424e4a604af8f8ec91593e233465b1a
philloooo <phillis.tt@gmail.com>
2018-06-28 23:21:57 -0500

    feat(wts): fix role binding

--- gen3/bin/kube-setup-wts.sh
+++ gen3/bin/kube-setup-wts.sh
@@ -22,14 +22,18 @@ if ! g3kubectl get secret wts-secret > /dev/null 2>&1; then
     encryption_key="$(random_alphanumeric 32 | base64)"
     secret_key="$(random_alphanumeric 32 | base64)"
     echo "create wts-secret"
-    kubectl create secret generic wts-secret --type=string --from-literal=OIDC_CLIENT_ID='$client_id' --from-literal=OIDC_CLIENT_SECRET='$client_secret' --from-literal=ENCRYPTION_KEY='$encryption_key' --from-literal=SECRET_KEY='$secret_key' --from-literal=FENCE_BASE_URL='${hostname}/user/' --from-literal=WTS_BASE_URL='${hostname}/wts/' --from-literal=SQLALCHEMY_DATABASE_URI='sqlite:////mnt/data/wts.db'
+    kubectl create secret generic wts-secret --type=string --from-literal=OIDC_CLIENT_ID="$client_id" --from-literal=OIDC_CLIENT_SECRET="$client_secret" --from-literal=ENCRYPTION_KEY="$encryption_key" --from-literal=SECRET_KEY="$secret_key" --from-literal=FENCE_BASE_URL="https://${hostname}/user/" --from-literal=WTS_BASE_URL="https://${hostname}/wts/" --from-literal=SQLALCHEMY_DATABASE_URI="sqlite:////mnt/data/wts.db"
 
 fi
 # deploy wts
+
 g3kubectl apply -f "${GEN3_HOME}/kube/services/wts/storageclass.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/wts/serviceaccount.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/wts/role-wts.yaml"
-g3kubectl apply -f "${GEN3_HOME}/kube/services/wts/rolebinding-wts.yaml"
+context=$(kubectl config view -o template --template='{{ index . "current-context" }}')
+namespace=$(kubectl config view -o jsonpath="{.contexts[?(@.name == \"$context\")].context.namespace}")
+g3k_kv_filter ${GEN3_HOME}/kube/services/wts/rolebinding-wts.yaml CURRENT_NAMESPACE "namespace: $namespace" | g3kubectl apply -f -
+
 gen3 roll wts
 g3kubectl apply -f "${GEN3_HOME}/kube/services/wts/wts-service.yaml"
 
