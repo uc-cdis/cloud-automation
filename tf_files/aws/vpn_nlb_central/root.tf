e0fbb5a4250b4860ca9fdac7f5cbd49ccfc5286b
Renuka Arya <renukarya@MacBook-Pro-53.local>
2018-07-13 14:39:49 -0500

    chore(vpnnlb): variables fixed

--- tf_files/aws/vpn_nlb_central/root.tf
+++ tf_files/aws/vpn_nlb_central/root.tf
@@ -10,11 +10,11 @@ provider "aws" {
   version = "1.18.0"
 }
 
-module "squid_nlb" {
+module "vpn_nlb" {
   source           = "../modules/vpn_nlb_central_csoc"
   env_vpc_octet3   = "${var.env_vpc_octet3}"
   env_vpc_id       = "${var.env_vpc_id}"
-  env_nlb_name     = "${var.env_vpn_nlb_name}"
+  env_vpn_nlb_name     = "${var.env_vpn_nlb_name}"
   ami_account_id   = "${var.ami_account_id}"
   csoc_cidr        = "${var.csoc_cidr}"
   env_priv_subnet_routetable_id = "${var.env_priv_subnet_routetable_id}"
