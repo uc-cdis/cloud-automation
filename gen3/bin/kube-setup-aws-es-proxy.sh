094c184c6038b85e2a323f0d7faca8a4ddf5b7ae
Fauzi <fauzigo@gmail.com>
2018-07-16 13:41:07 -0500

    chore(corrections): following Reubens suggestions

--- gen3/bin/kube-setup-aws-es-proxy.sh
+++ gen3/bin/kube-setup-aws-es-proxy.sh
@@ -4,17 +4,18 @@
 # https://github.com/abutaha/aws-es-proxy
 # 
 
-if [ $# -ne 1 ]; then
-    echo "USAGE: $0 name_of_child_vpc"
-    exit 1
-fi
+#if [ $# -ne 1 ]; then
+#    echo "USAGE: $0 name_of_child_vpc"
+#    exit 1
+#fi
 
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 
 gen3 kube-setup-secrets
-#gen3 roll esproxy
-sed "s/ES_ENDPOINT/https:\/\/${1}/" "${GEN3_HOME}/kube/services/aws-es-proxy/aws-es-proxy-deploy.yaml" | g3kubectl apply -f -
+
+ES_ENDPOINT=$(aws es describe-elasticsearch-domains --domain-names ${vpc_name}-gen3-metadata --query "DomainStatusList[*].Endpoints" --output text)
+sed "s/ES_ENDPOINT/https:\/\/${ES_ENDPOINT}/" "${GEN3_HOME}/kube/services/aws-es-proxy/aws-es-proxy-deploy.yaml" | g3kubectl apply -f -
 g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_aws_es_proxy.yaml"
 
 cat <<EOM
