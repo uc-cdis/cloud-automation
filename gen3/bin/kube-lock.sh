f7eef17152e1c9c9a61a138e8b70aa0e126ccbdb
Steven Cui <stevencui729@gmail.com>
2018-07-27 15:20:24 -0500

    feat(kube-lock): implemented lock and unlock

--- gen3/bin/kube-lock.sh
+++ gen3/bin/kube-lock.sh
@@ -1,8 +1,53 @@
 #!/bin/bash
 #
-# Little helper derives terraform and k8s configuration
-# from kubernetes secrets if possible.  
-# Generates
-#    $XDG_RUNTIME_DIR/kube-extract-config/creds.json, config.tfvars, and 00configmap.yaml
-# Requries g3kubectl to be on the path and configured.
-#
+# Script to lock namespace using labels on a lock ConfigMap (make sure 
+# KUBECTL_NAMESPACE is set so that g3kubectl works properly)
+
+set -i
+# load bashrc so that the script is treated like it was launched on the remote machine
+source ~/.bashrc
+
+# load gen3 tools
+if [[ -n "$GEN3_HOME" ]]; then  # load gen3 tools from cloud-automation
+  source "${GEN3_HOME}/gen3/lib/utils.sh"
+  gen3_load "gen3/gen3setup"
+else
+  echo "GEN3_HOME is not set"
+  exit 1
+fi
+
+echo $KUBECTL_NAMESPACE
+echo $1
+
+# create locks ConfigMap if it does not already exist, and set the lock we are 
+# currently trying to lock to unlocked with no owner
+if ! g3kubectl get configmaps locks; then
+  echo "locks configmap not detected, creating one"
+  g3kubectl create configmap locks
+  g3kubectl label configmap locks $1=false $1_owner=none
+else 
+  if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$1}") = '' ]]; then
+    echo "lock $1 doesn't exist, creating it"
+    g3kubectl label configmap locks $1=false $1_owner=none
+  fi
+fi
+
+# check if the lock we are currently trying to lock is unlocked. If it is, lock 
+# lock and wait, then check again if we have the lock before proceeding
+if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$1}") = "false" ]]; then
+  echo "lock is unlocked, locking"
+  g3kubectl label --overwrite configmap locks $1=true $1_owner=$2
+  sleep $(shuf -i 1-5 -n 1)
+
+  if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$1}") = "true" 
+    && $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$1_owner}") = $2 ]]; then 
+    echo "still have lock after sleeping"
+    exit 0
+  else
+    echo "no longer have lock after sleeping"
+    exit 1
+  fi
+else 
+  echo "lock is locked"
+  exit 1
+fi
---
+++ gen3/bin/kube-unlock.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Script to unlock namespace using labels on a lock ConfigMap (make sure 
+# KUBECTL_NAMESPACE is set so that g3kubectl works properly)
+
+set -i
+# load bashrc so that the script is treated like it was launched on the remote machine
+source ~/.bashrc
+
+# load gen3 tools
+if [[ -n "$GEN3_HOME" ]]; then  # load gen3 tools from cloud-automation
+  source "${GEN3_HOME}/gen3/lib/utils.sh"
+  gen3_load "gen3/gen3setup"
+else
+  echo "GEN3_HOME is not set"
+  exit 1
+fi
+
+# create locks ConfigMap if it does not already exist, and set the lock we are 
+# currently trying to lock to unlocked with no owner
+if ! g3kubectl get configmaps locks; then
+  echo "locks configmap not detected, can't unlock"
+  exit 1
+else 
+  if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$1}") != 'true' ]]; then
+    echo "lock $1 doesn't exist or isn't locked, can't unlock"
+    exit 1
+  else 
+    if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$1_owner}") != $2 ]]; then
+      echo "you don't own this lock, can't unlock"
+      exit 1
+    else 
+      g3kubectl label --overwrite configmap locks $1=false $1_owner=none
+      exit 0
+    fi
+  fi
+fi
