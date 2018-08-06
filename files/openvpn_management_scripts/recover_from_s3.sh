52a4e219df4533557f6ecd9adcc8cb23be8f4a51
Kyle Schmitt <schmitt@uchicago.edu>
2018-08-06 16:19:28 -0500

    forgot a line

--- files/openvpn_management_scripts/recover_from_s3.sh
+++ files/openvpn_management_scripts/recover_from_s3.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 aws s3 sync --exclude "csoc-vpn-bucket*" --exclude "user_passwd.csv" s3://vpn-certs-and-files/ /etc/openvpn/easy-rsa/keys/
+aws s3 cp s3://vpn-certs-and-files/user_passwd.csv /etc/openvpn/user_passwd.csv
