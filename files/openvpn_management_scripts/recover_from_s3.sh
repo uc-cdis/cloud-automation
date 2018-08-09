6e3a008d8982660ea154a2b8a8eae62f9d4dea5c
Kyle Schmitt <schmitt@uchicago.edu>
2018-08-09 13:12:52 -0500

    changed keys to easy-rsa for fuller coverage

--- files/openvpn_management_scripts/recover_from_s3.sh
+++ files/openvpn_management_scripts/recover_from_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws s3 sync s3://vpn-certs-and-files/WHICHVPN/keys/ /etc/openvpn/easy-rsa/keys/
+aws s3 sync s3://vpn-certs-and-files/WHICHVPN/easy-rsa/ /etc/openvpn/easy-rsa/
 aws s3 cp s3://vpn-certs-and-files/WHICHVPN/user_passwd.csv /etc/openvpn/user_passwd.csv
 aws s3 cp s3://vpn-certs-and-files/WHICHVPN/server.pem /etc/lighttpd/certs/server.pem
