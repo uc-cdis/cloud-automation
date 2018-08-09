80272390c2f526322dbc06a724e33ff2be4987e4
Kyle Schmitt <schmitt@uchicago.edu>
2018-08-09 13:46:59 -0500

    backup ipp.txt

--- files/openvpn_management_scripts/recover_from_s3.sh
+++ files/openvpn_management_scripts/recover_from_s3.sh
@@ -2,3 +2,4 @@
 aws s3 sync s3://vpn-certs-and-files/WHICHVPN/easy-rsa/ /etc/openvpn/easy-rsa/
 aws s3 cp s3://vpn-certs-and-files/WHICHVPN/user_passwd.csv /etc/openvpn/user_passwd.csv
 aws s3 cp s3://vpn-certs-and-files/WHICHVPN/server.pem /etc/lighttpd/certs/server.pem
+aws s3 cp s3://vpn-certs-and-files/WHICHVPN/ipp.txt /etc/openvpn/ipp.txt
