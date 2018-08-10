d6a35b2772c4e6db5e6940a8e02770c211edb33c
Kyle Schmitt <schmitt@uchicago.edu>
2018-08-10 12:08:43 -0500

    fixed a path and added some checks

--- files/openvpn_management_scripts/push_to_s3.sh
+++ files/openvpn_management_scripts/push_to_s3.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-aws s3 sync /etc/openvpn/easy-rsa/ s3://vpn-certs-and-files/WHICHVPN/easy-rsa/
-aws s3 cp /etc/openvpn/user_passwd.csv s3://vpn-certs-and-files/WHICHVPN/
-aws s3 cp /root/certs/server.pem s3://vpn-certs-and-files/WHICHVPN/
-aws s3 cp /etc/openvpn/ipp.txt s3://vpn-certs-and-files/WHICHVPN/
+if [ -d /etc/openvpn/easy-rsa/ ]
+then
+	aws s3 sync /etc/openvpn/easy-rsa/ s3://vpn-certs-and-files/WHICHVPN/easy-rsa/
+else
+	echo directory /etc/openvpn/easy-rsa/ does not exist
+fi
+
+for F in /etc/openvpn/user_passwd.csv /etc/lighttpd/certs/server.pem /etc/openvpn/ipp.txt
+do
+	if [ -e $F ]
+	then
+		aws s3 cp $F s3://vpn-certs-and-files/WHICHVPN/
+	else
+		echo file $F does not exist
+	fi
+done
