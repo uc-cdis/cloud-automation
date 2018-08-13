755c870294db3600fe3fb2f5202f7834a0ab4d86
Kyle Schmitt <schmitt@uchicago.edu>
2018-08-13 10:13:55 -0500

    updated recover_from_s3 to fix perms on download

--- files/openvpn_management_scripts/recover_from_s3.sh
+++ files/openvpn_management_scripts/recover_from_s3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 aws s3 sync s3://vpn-certs-and-files/WHICHVPN/easy-rsa/ /etc/openvpn/easy-rsa/ || true
+#fix perms
+for i in /etc/openvpn/easy-rsa/*;do [[ $i = *".cnf" || $i = *"/vars" ]] || chmod a+x $i;done
 aws s3 cp s3://vpn-certs-and-files/WHICHVPN/user_passwd.csv /etc/openvpn/user_passwd.csv || true
 aws s3 cp s3://vpn-certs-and-files/WHICHVPN/server.pem /root/server.pem || true
 aws s3 cp s3://vpn-certs-and-files/WHICHVPN/ipp.txt /etc/openvpn/ipp.txt || true
