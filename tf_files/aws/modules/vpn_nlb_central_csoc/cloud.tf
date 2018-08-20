08fb443b3de4817bea24224fa10217cd4eb9a321
Renuka Arya <renukarya@MacBook-Pro-53.local>
2018-08-20 12:05:16 -0500

    chore(openvpn):test

--- tf_files/aws/modules/vpn_nlb_central_csoc/cloud.tf
+++ tf_files/aws/modules/vpn_nlb_central_csoc/cloud.tf
@@ -325,7 +325,8 @@ sed -i "s/WHICHVPN/${var.env_vpn_nlb_name}/" /root/openvpn_management_scripts/re
 sed -i "s/WHICHVPN/${var.env_vpn_nlb_name}/" /root/openvpn_management_scripts/install_ovpn.sh
 
 # Replace the User variable for hostname, VPN subnet and VM subnet 
-sudo sed -i "s/HOSTNAME/${var.env_vpn_nlb_name}/" /root/openvpn_management_scripts/csoc_vpn_user_variable
+#sudo sed -i "s/HOSTNAME/${var.env_vpn_nlb_name}/" /root/openvpn_management_scripts/csoc_vpn_user_variable
+sudo sed -i "s/HOSTNAME/RARYATEST/" /root/openvpn_management_scripts/csoc_vpn_user_variable
 sudo sed -i "s/VPN_SUBNET/${var.csoc_vpn_subnet}/" /root/openvpn_management_scripts/csoc_vpn_user_variable
 sudo sed -i "s/VM_SUBNET/${var.csoc_vm_subnet}/" /root/openvpn_management_scripts/csoc_vpn_user_variable
 #sudo sed -i "s/VM_ROUTE_PUSH/10.128.2.0\/24/" /root/openvpn_management_scripts/csoc_vpn_user_variable
