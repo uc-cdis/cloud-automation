b4d8c87e5a6b69feafb4708983880f79e48288af
Reuben Pasquini <reuben@frickjack.com>
2018-08-28 17:22:10 -0500

    chore(manifest): manifest images and index name

--- apis_configs/tube_settings.py
+++ apis_configs/tube_settings.py
@@ -34,13 +34,16 @@ PARALLEL_JOBS = 1
 ES = {
     "es.nodes": ES_URL,
     "es.port": '9200',
-    "es.resource": 'etl',
+    "es.resource": os.getenv('ES_INDEX_NAME', 'null'),
     "es.input.json": 'yes',
     "es.nodes.client.only": 'false',
     "es.nodes.discovery": 'false',
     "es.nodes.data.only": 'false',
     "es.nodes.wan.only": 'true'
 }
+
+if 'null' == ES['es.resource']:
+  raise Exception('ES_INDEX_NAME environment not defined')
 
 HADOOP_HOME = os.getenv('HADOOP_HOME', '/usr/local/Cellar/hadoop/3.1.0/libexec/')
 JAVA_HOME = os.getenv('JAVA_HOME', '/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home')
