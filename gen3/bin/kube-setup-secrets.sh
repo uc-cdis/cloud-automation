ea6fb3d258f88d81c7d0e270eec406da054f2374
Di Wu <diwu@uchicago.edu>
2018-08-27 21:15:31 -0500

    feat(test transfer data ditionary)

--- gen3/bin/kube-setup-secrets.sh
+++ gen3/bin/kube-setup-secrets.sh
@@ -47,10 +47,10 @@ fi
 
 
 if ! g3kubectl get configmaps global > /dev/null 2>&1; then
-  if [[ -f "${WORKSPACE}/${vpc_name}/00configmap.yaml" ]]; then
-    g3kubectl apply -f "${WORKSPACE}/${vpc_name}/00configmap.yaml"
+  if [[ -f "${GEN3_MANIFEST_HOME}/00configmap.yaml" ]]; then
+    g3kubectl apply -f "${GEN3_MANIFEST_HOME}/00configmap.yaml"
   else
-    echo "ERROR: unable to configure global configmap - missing ${WORKSPACE}/${vpc_name}/00configmap.yaml"
+    echo "ERROR: unable to configure global configmap - missing ${GEN3_MANIFEST_HOME}/00configmap.yaml"
     exit 1
   fi
 fi
