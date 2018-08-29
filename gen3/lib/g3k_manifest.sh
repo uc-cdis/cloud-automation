752733de148b828aebebbedaba5aa6004c1683db
Di Wu <diwu@uchicago.edu>
2018-08-29 12:30:24 -0500

    chore(g3k_manifest:)

--- gen3/lib/g3k_manifest.sh
+++ gen3/lib/g3k_manifest.sh
@@ -3,20 +3,21 @@
 # Little library of manifest related functions usually accessed
 # via the `g3k/gen3` cli
 #
-GEN3_HOST_NAME=${1:-$(g3kubectl get configmaps global -ojsonpath='{ .data.hostname }')}
+#GEN3_HOST_NAME=${1:-$(g3kubectl get configmaps global -ojsonpath='{ .data.hostname }')}
+
+CONFIGMAP_HOME=$(cd "${GEN3_HOME}/.." && pwd)/"${vpc_name}"
+GEN3_HOST_NAME="$(yq .data.hostname ${CONFIGMAP_HOME}/00configmap.yaml | sed "s/\"//g")"
   if [[ -z "$GEN3_HOST_NAME" ]]; then
     echo -e $(red_color "g3k_manifest_path could not establish commons hostname") 1>&2
     return 1
   fi
-CONFIGMAP_HOME=$(cd "${GEN3_HOME}/.." && pwd)/"${vpc_name}"
-#GEN3_HOST_NAME="$(yq .data.hostname ${CONFIGMAP_HOME}/00configmap.yaml | sed "s/\"//g")"
 #new path for all gitops files 
 GEN3_COMMON_HOME=$(cd "${GEN3_HOME}/.." && pwd)/"${GEN3_HOST_NAME}"
 #old manifest path
 GEN3_MANIFEST_HOME="${GEN3_MANIFEST_HOME:-"$(cd "${GEN3_HOME}/.." && pwd)/cdis-manifest"}"
 export GEN3_HOST_NAME=${GEN3_HOST_NAME}
 export GEN3_MANIFEST_HOME=${GEN3_MANIFEST_HOME}
-export GEN3_COMMON_HOME=${GEN3_COMMON_HOME}
+export GEN3_COMMON_HOME=${GEN3_COMMON_HOME}ß
 
 ##########comment out
 #GEN3_MANIFEST_HOME="${GEN3_MANIFEST_HOME:-"$(cd "${GEN3_HOME}/.." && pwd)/cdis-manifest"}"
@@ -65,10 +66,10 @@ g3k_manifest_init() {
     return 0
   fi
 
-  git ls-remote "https://github.com/uc-cdis/${GEN3_HOST_NAME}.git" > /dev/null 2>&1
+  NEW_MANIFEST_EXIST=$(curl -s https://api.github.com/repos/uc-cdis/${GEN3_COMMON_HOME} |jq .message
   #if the separate common didn't setup, we will use the exist cdis-manifest logic 
   #we should set up two separate variable one is 
-  if [[ "$?" -ne 0 ]]; then
+  if [[ "${NEW_MANIFEST_EXIST}" -eq "Not Found" ]]; then
     if [[ ! -d "${GEN3_MANIFEST_HOME}" ]]; then
       echo -e $(red_color "ERROR: GEN3_MANIFEST_HOME does not exist: ${GEN3_MANIFEST_HOME}") 1>&2
       echo "git clone https://github.com/uc-cdis/cdis-manifest.git ${GEN3_MANIFEST_HOME}" 1>&2
@@ -79,10 +80,10 @@ g3k_manifest_init() {
       # Don't do this when running tests in Jenkins ...
       echo "INFO: git fetch in $GEN3_MANIFEST_HOME" 1>&2
       (cd "$GEN3_MANIFEST_HOME" && git pull; git status) 1>&2
-    fi
+    fiß
   fi
   
-  if [["$?" -eq 0]]; then
+  if [[ "${NEW_MANIFEST_EXIST}" -ne "Not Found" ]]; then
     if [[ ! -d "${GEN3_COMMON_HOME}" ]]; then
       echo -e $(red_color "ERROR: GEN3_COMMON_HOME does not exist: ${GEN3_COMMON_HOME}") 1>&2
       echo "git clone https://github.com/uc-cdis/${GEN3_HOST_NAME}.git ${GEN3_COMMON_HOME}" 1>&2
