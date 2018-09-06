bbe2a8db9d38651563b6ceb148fc9a05896c635e
Reuben Pasquini <reuben@frickjack.com>
2018-09-06 12:41:56 -0500

    feat(gen3 es): add subcommands
    gen3 es port-forward
    gen3 es alias
    gen3 es export
    gen3 es import

--- gen3/bin/es.sh
+++ gen3/bin/es.sh
@@ -8,12 +8,156 @@ gen3_load "gen3/gen3setup"
 
 help() {
   cat - <<EOM
-gen3 es indices
-  list the elastic search indices
-gen3 es dump index-name
-  dump the contents of an ES index (ex: arranger-projects)
+gen3 es 
+  alias index-name [alias-name]
+    (re)create an alias for the given index if alias-name is given - 
+    otherwise just lists the aliases associated with the given index
+  dump index-name
+    dump the contents of an ES index (ex: arranger-projects)
+  export project-name
+  import project-name
+  indices
+    list the elastic search indices
+  mapping index-name
+    fetch the type-mapping for the given index
+  port-forward
+    forward the es-proxy to localhost
+  
 EOM
 }
+
+
+#
+# Forward the esproxy-deployment port to localhost in a background process.
+# Guard with a call to `ps uxwww` to check if port is already forwarded.
+#
+# @return echo the forwarded port
+#
+es_port_forward() {
+  local portNum
+  portNum="$(ps uxwwww | grep port-forward | grep 9200 | grep -v grep | sed 's/.* \([0-9]*\):9200/\1/')"
+  if [[ -n "$portNum" ]]; then
+    echo "It looks like a port-forward process is already running" 1>&2
+  else
+    local OFFSET
+    OFFSET=$((RANDOM % 1000))
+    portNum=$((OFFSET+9200))
+    g3kubectl port-forward deployment/aws-es-proxy-deployment ${portNum}:9200 1>&2 &
+  fi
+  export ESHOST="localhost:$portNum"
+  echo "$portNum"
+}
+
+
+#
+# Dump the contents of a given index
+#
+# @parma indexName
+#
+function es_dump() {
+  local indexName
+  indexName=$1
+
+curl -X GET "${ESHOST}/${indexName}/_search?pretty=true&size=100" \
+-H 'Content-Type: application/json' -d'
+{
+  "query": { "match_all": {} }
+}
+'
+
+}
+
+#
+# Get the list of indexes
+#
+function es_indices() {
+  curl -X GET "${ESHOST}/_cat/indices?v"
+}
+
+#
+# Dump the arranger config indexes to the given destination folder
+# @param destFolder
+# @param projectName name of the arranger project
+#
+function es_export() {
+  local destFolder
+  local projectName
+  local indexList
+
+  if [[ $# -lt 2 ]]; then
+    echo 'USE: es_export destFolderPath arrangerProjectName'
+    return 1
+  fi
+  destFolder="$1"
+  shift
+  projectName="$1"
+  shift
+  mkdir -p "$destFolder"
+  indexList=$(es_indices 2> /dev/null | grep "arranger-$projectName" | awk '{ print $3 }')
+  for name in $indexList; do
+    echo $name
+    npx elasticdump --input http://$ESHOST/$name --output ${destFolder}/${name}__data.json --type data
+    npx elasticdump --input http://$ESHOST/$name --output ${destFolder}/${name}__mapping.json --type mapping
+  done
+}
+
+#
+# Point the given alias at the given index
+#
+# @param indexName
+# @param aliasName
+#
+function es_alias() {
+  local indexName
+  local aliasName
+  if [[ $# -lt 2 ]]; then
+    local getPath
+    getPath=$ESHOST/_aliases?pretty=true
+    indexName="$1"
+    if [[ -n "$indexName" ]]; then
+      getPath="$ESHOST/$indexName/_alias?pretty=true"
+    fi
+    curl -X GET $getPath
+    return $?
+  fi
+  indexName="$1"
+  aliasName="$2"
+  
+  # Check if the alias already exists - if so, then remove it
+  local existingAliases
+  local oldName
+  local removeExistingAliases
+  removeExistingAliases=""
+  existingKeys="$(curl -s -X GET $ESHOST/_aliases | jq -r "to_entries[] | select(.value.aliases[\"$aliasName\"]) | .key")"
+  for oldName in $existingKeys; do
+    removeExistingAliases="$removeExistingAliases
+    { \"remove\" : { \"index\" : \"$oldName\", \"alias\" : \"$aliasName\" } },
+    "
+  done
+  curl -X POST $ESHOST/_aliases \
+   -H 'Content-Type: application/json' \
+   -d"
+{
+    \"actions\" : [
+        $removeExistingAliases
+        { \"add\" : { \"index\" : \"$indexName\", \"alias\" : \"$aliasName\" } }
+    ]
+}
+"
+}
+
+
+#
+# Get the mapping of a given index
+#
+# @param indexName
+#
+function es_mapping() {
+  local indexName
+  indexName=$1
+  curl -X GET $ESHOST/${indexName}/_mapping?pretty=true
+}
+
 
 
 if [[ -z "$1" || "$1" =~ ^-*help$ ]]; then
@@ -25,8 +169,11 @@ command="$1"
 shift
 
 case "$command" in
+"alias")
+  es_alias "$@"
+  ;;
 "indices")
-  gen3 devterm 'source gen3-arranger/Docker/Stacks/esearch/indexSetup.sh; export ESHOST=esproxy-service:9200; es_indices'
+  es_indices
   ;;
 "dump")
   indexName="$1"
@@ -34,9 +181,21 @@ case "$command" in
     help
     exit 1
   fi
-  gen3 devterm "source gen3-arranger/Docker/Stacks/esearch/indexSetup.sh; export ESHOST=esproxy-service:9200; es_dump '$indexName'"
+  es_dump "$@"
   ;;
-"*")
+"export")
+  es_export "$@"
+  ;;
+"import")
+  es_import "$@"
+  ;;
+"mapping")
+  es_mapping "$@"
+  ;;
+"port-forward")
+  es_port_forward
+  ;;
+*)
   help
   exit 1
   ;;
