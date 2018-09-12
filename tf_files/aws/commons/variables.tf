de9a22276f57b693e5dc23666e7828c3a4332c12
lookcrabs <6787558+lookcrabs@users.noreply.github.com>
2018-09-12 10:44:14 -0500

    feat(mailgun-creds): updated templates/secrets sh with mailgun section

--- tf_files/aws/commons/variables.tf
+++ tf_files/aws/commons/variables.tf
@@ -168,4 +168,13 @@ variable "csoc_managed" {
 
 variable "organization_name" {
   default = "Basic Service"
+
+
+## Mailgun variable defaults/definitions.
+variable "mailgun_api_key" {}
+variable "mailgun_smtp_host" {
+    default = "smtp.mailgun.org"
+}
+variable "mailgun_api_url" {
+    default = "https://api.mailgun.net/v3/"
 }
