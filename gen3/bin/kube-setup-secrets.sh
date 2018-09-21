5b792b332cb4ca125e4bc725d838d1e98ec64712
Reuben Pasquini <reuben@frickjack.com>
2018-09-21 12:28:43 -0500

    fix(configmaps): update-guard logic tweak
    probably broke in merge

--- gen3/bin/kube-setup-secrets.sh
+++ gen3/bin/kube-setup-secrets.sh
@@ -46,7 +46,6 @@ if [[ -f "${WORKSPACE}/${vpc_name}/creds.json" ]]; then # update secrets
   fi
 fi
 
-
 if ! g3kubectl get configmaps global > /dev/null 2>&1; then
   if [[ -f "${WORKSPACE}/${vpc_name}/00configmap.yaml" ]]; then
     g3kubectl apply -f "${WORKSPACE}/${vpc_name}/00configmap.yaml"
@@ -59,11 +58,12 @@ if ! g3kubectl get configmap config-helper > /dev/null 2>&1; then
   gen3 update_config config-helper "${GEN3_HOME}/apis_configs/config_helper.py"
 fi
 
-
+# only update configmaps here if they have not updated in last 2 minutes
+# (gen3 roll all calls this over and over)
 let configmapsExpireTime="$(date +%s) - 120"
 configmapsFlagFile="${WORKSPACE}/${vpc_name}/.configmapsFlagFile"
 # Avoid creating configmaps more than once every two minutes
-if [[ ! -f "configmapsFlagFile" || $(stat -c %Y "$configmapsFlagFile") -lt "$configmapsExpireTime" ]]; then
+if [[ (! -f "$configmapsFlagFile") || $(stat -c %Y "$configmapsFlagFile") -lt "$configmapsExpireTime" ]]; then
   # call g3k configmaps to create manifest configmaps
   echo "creating manifest configmaps"
   g3k configmaps
