c83b4b48e8092672d3bed753f92005afbd640b50
Steven Cui <stevencui729@gmail.com>
2018-10-16 15:40:26 -0500

    fix(klock-tests): removed text checking tests

--- gen3/bin/testsuite.sh
+++ gen3/bin/testsuite.sh
@@ -189,7 +189,6 @@ test_kube_lock() {
   gen3 klock lock testlock2 testuser 60; because $? "klock lock should be able to handle multiple locks"
   gen3 klock lock testlock3 testuser2 60; because $? "klock lock should be able to handle multiple users"
   gen3 klock lock testlock testuser2 60; because !$? "attempting to lock an already locked lock with a different user should fail"
-  kubectl get configmaps locks -o=yaml | grep -e 'testlock: "true"'; because $? "klock lock success should write to the configmap properly"
   gen3 klock lock testlock4 testuser 10
   sleep 11
   gen3 klock lock testlock4 testuser 15; because $? "attempting to lock an expired lock should succeed"
@@ -209,7 +208,6 @@ test_kube_unlock() {
   gen3 klock unlock testlock testuser2; because !$? "calling klock unlock for the first time on a lock the user does not own should fail"
   gen3 klock unlock testlock testuser; because $? "calling klock unlock for the first time on a lock the user owns should succeed"
   gen3 klock unlock testlock testuser; because !$? "calling klock unlock for the second time on a lock the user owns should fail because the lock is already unlocked"
-  kubectl get configmaps locks -o=yaml | grep -e 'testlock: "false"'; because $? "klock unlock success should write to the configmap properly"
   kubectl delete configmap locks
 }
 
