1e5960f9f4a797c903851eb10678e0cd55030581
Steven Cui <stevencui729@gmail.com>
2018-10-16 15:36:06 -0500

    fix(klock-tests): reordered tests

--- gen3/bin/testsuite.sh
+++ gen3/bin/testsuite.sh
@@ -185,11 +185,11 @@ test_kube_lock() {
   gen3 klock lock testlock testuser 60 -w not-a-number | grep -e "ERROR: wait-time is not-a-number, must be an integer"; because $? "calling klock lock without a number for wait-time should show this error message"
   kubectl delete configmap locks
   gen3 klock lock testlock testuser 60; because $? "calling klock lock for the first time for a lock should successfully lock it, and it should create the configmap locks if it does not exist already"
-  kubectl get configmaps locks -o=yaml | grep -e 'testlock: "true"'; because $? "klock lock success should write to the configmap properly"
   gen3 klock lock testlock testuser 60; because !$? "calling klock lock for the second time in a row for a lock should fail to lock it"
   gen3 klock lock testlock2 testuser 60; because $? "klock lock should be able to handle multiple locks"
   gen3 klock lock testlock3 testuser2 60; because $? "klock lock should be able to handle multiple users"
   gen3 klock lock testlock testuser2 60; because !$? "attempting to lock an already locked lock with a different user should fail"
+  kubectl get configmaps locks -o=yaml | grep -e 'testlock: "true"'; because $? "klock lock success should write to the configmap properly"
   gen3 klock lock testlock4 testuser 10
   sleep 11
   gen3 klock lock testlock4 testuser 15; because $? "attempting to lock an expired lock should succeed"
@@ -208,8 +208,8 @@ test_kube_unlock() {
   gen3 klock unlock testlock2 testuser; because !$? "calling klock unlock for the first time on a lock that does not exist should fail"
   gen3 klock unlock testlock testuser2; because !$? "calling klock unlock for the first time on a lock the user does not own should fail"
   gen3 klock unlock testlock testuser; because $? "calling klock unlock for the first time on a lock the user owns should succeed"
-  kubectl get configmaps locks -o=yaml | grep -e 'testlock: "false"'; because $? "klock unlock success should write to the configmap properly"
   gen3 klock unlock testlock testuser; because !$? "calling klock unlock for the second time on a lock the user owns should fail because the lock is already unlocked"
+  kubectl get configmaps locks -o=yaml | grep -e 'testlock: "false"'; because $? "klock unlock success should write to the configmap properly"
   kubectl delete configmap locks
 }
 
