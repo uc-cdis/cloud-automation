822c224683408240ef523ef15ba4ea9b28da188e
Steven Cui <stevencui729@gmail.com>
2018-10-30 14:38:55 -0500

    feat(gen3-reset): testing full script

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
+#
+# script to reset kubernetes namespace gen3 objects/services
+#
 
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 
-echo $KUBECTL_NAMESPACE
-
+# make sure KUBECTL_NAMESPACE env var is set (defaults to current namespace)
 if [[ -z $KUBECTL_NAMESPACE ]]; then
-    echo "-z pass"
     KUBECTL_NAMESPACE=$(g3kubectl get configmap manifest-global -o=jsonpath='{.metadata.namespace}')
-else
-    echo "-z fail"
+    echo "KUBECTL_NAMESPACE set to $KUBECTL_NAMESPACE"
 fi
-
-echo $KUBECTL_NAMESPACE
 
 gen3 klock lock reset-lock gen3-reset 3600 -w 60
 
+g3kubectl delete --all deployments --namespace=$KUBECTL_NAMESPACE
 
-# g3kubectl delete --all deployments --namespace=$KUBECTL_NAMESPACE
+services=( fence sheepdog indexd)
+for service in ${services[@]}; do
+    echo $service
+    echo '\c template1; DROP DATABASE $KUBECTL_NAMESPACE; CREATE DATABASE $KUBBECTL_NAMESPACE' | gen3 psql $service
+done
 
-
-
+gen3 roll all
+gen3 kube-wait4-pods
+gen3 runjob gdcdb-create; gen3 runjob indexd-userdb; gen3 runjob usersync;
+gen3 roll all
+gen3 kube-wait4-pods
 
 gen3 klock unlock reset-lock gen3-reset
