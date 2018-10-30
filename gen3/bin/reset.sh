e955c4f9751278db0edaa6d2f99821244ca15c7b
Steven Cui <stevencui729@gmail.com>
2018-10-30 16:08:05 -0500

    feat(gen3-reset): fixed multicommand postgres echo

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -19,7 +19,7 @@ g3kubectl delete --all deployments --namespace=$KUBECTL_NAMESPACE
 services=( fence sheepdog indexd)
 for service in ${services[@]}; do
     echo $service
-    echo "\c template1; DROP DATABASE $KUBECTL_NAMESPACE; CREATE DATABASE $KUBBECTL_NAMESPACE" | gen3 psql $service
+    echo "\c template1 \\ DROP DATABASE $KUBECTL_NAMESPACE \\ CREATE DATABASE $KUBBECTL_NAMESPACE" | gen3 psql $service
 done
 
 # gen3 roll all
