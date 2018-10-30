6327f3244827a76938b9b17d247f695f20efe843
Steven Cui <stevencui729@gmail.com>
2018-10-30 13:45:53 -0500

    feat(gen3-reset): testing kubectl_namespac

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -5,8 +5,11 @@ gen3_load "gen3/gen3setup"
 
 echo $KUBECTL_NAMESPACE
 
-if [[ -z KUBECTL_NAMESPACE ]]; then
+if [[ -z $KUBECTL_NAMESPACE ]]; then
+    echo "-z pass"
     KUBECTL_NAMESPACE=$(g3kubectl get configmap manifest-global -o=jsonpath='{.metadata.namespace}')
+else
+    echo "-z fail"
 fi
 
 echo $KUBECTL_NAMESPACE
