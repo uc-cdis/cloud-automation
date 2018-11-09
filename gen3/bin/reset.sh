1d6286aa20d6c0440fcecff6cb0f2654e728ebdc
Steven Cui <stevencui729@gmail.com>
2018-11-09 14:43:34 -0600

    feat(gen3-reset): added a function to wait for pods that access database to be down

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -3,6 +3,7 @@
 # script to reset kubernetes namespace gen3 objects/services
 #
 
+# load gen3 tools and set up for resetting namespace
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 gen3_load "gen3/lib/kube-setup-init"
@@ -13,13 +14,28 @@ if [[ -z $KUBECTL_NAMESPACE ]]; then
     echo "KUBECTL_NAMESPACE set to $KUBECTL_NAMESPACE"
 fi
 
+wait_for_pods_down() {
+    echo "entered wait"
+    podsDownFlag = 1
+    while [[ podsDownFlag == 0 ]]; do
+        g3kubectl get pods
+        if [[ 0 == "$(g3kubectl get pods -o json | jq -r '[.items[] | { name: .metadata.labels.app } ] | map(select(.name=="fence" or .name=="sheepdog" or .name=="peregrine" or .name=="indexd")) | length')" ]]; then
+            let podsDownFlag=0
+        fi
+    done
+    return 0
+}
+
+
 gen3 klock lock reset-lock gen3-reset 3600 -w 60
 
 if [[ -f "${WORKSPACE}/${vpc_name}/.rendered_gdcapi_db" ]]; then
     echo "deleting .rendered_gdcapi_db flag file"
     rm "${WORKSPACE}/${vpc_name}/.rendered_gdcapi_db"
 fi
+
 g3kubectl delete --all deployments --namespace=$KUBECTL_NAMESPACE
+wait_for_pods_down
 
 # # drop and recreate all the postgres databases
 serviceCreds=( fence-creds sheepdog-creds indexd-creds )
@@ -27,6 +43,7 @@ for serviceCred in ${serviceCreds[@]}; do
     dbName=$(g3kubectl get secrets $serviceCred -o json | jq -r '.data["creds.json"]' | base64 --decode | jq -r  .db_database)
     service=${serviceCred%-creds}
 
+    # check for user consent before deleting and recreating tables
     echo -e "$(red_color "WARNING: about to drop the $dbName database from the $service postgres server - proceed? (y/n)")"
     read -r yesno
     if [[ $yesno = "n" ]]; then
@@ -37,10 +54,12 @@ for serviceCred in ${serviceCreds[@]}; do
     echo "\c template1 \\\ DROP DATABASE $KUBECTL_NAMESPACE; CREATE DATABASE $KUBECTL_NAMESPACE;" | gen3 psql $service
 done
 
-# gen3 roll all
-# gen3 kube-wait4-pods
-gen3 job run gdcdb-create; gen3 job run indexd-userdb; gen3 job run usersync
-gen3 kube-wait4-pods
 gen3 roll all
+gen3 kube-wait4-pods
+# gen3 job run gdcdb-create; gen3 job run indexd-userdb; gen3 job run usersync
+# gen3 kube-wait4-pods
+# gen3 roll all
 
 gen3 klock unlock reset-lock gen3-reset
+
+
