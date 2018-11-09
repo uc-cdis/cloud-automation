1b17635300a60c2f57f9dc13082d1e360588c121
Steven Cui <stevencui729@gmail.com>
2018-11-09 11:49:40 -0600

    feat(gen3-reset): putting rolls back in, testing

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -17,35 +17,30 @@ gen3 klock lock reset-lock gen3-reset 3600 -w 60
 
 if [[ -f "${WORKSPACE}/${vpc_name}/.rendered_gdcapi_db" ]]; then
     echo "deleting .rendered_gdcapi_db flag file"
-    # rm "${WORKSPACE}/${vpc_name}/.rendered_gdcapi_db"
-else 
-    echo "something wrong with flag file detection"
+    rm "${WORKSPACE}/${vpc_name}/.rendered_gdcapi_db"
 fi
-# g3kubectl delete --all deployments --namespace=$KUBECTL_NAMESPACE
+g3kubectl delete --all deployments --namespace=$KUBECTL_NAMESPACE
 
 # # drop and recreate all the postgres databases
 serviceCreds=( fence-creds sheepdog-creds indexd-creds )
 for serviceCred in ${serviceCreds[@]}; do
-    dbname=$(g3kubectl get secrets $serviceCred -o json | jq -r '.data["creds.json"]' | base64 --decode | jq -r  .db_database)
-    # echo $service
-    echo "$KUBECTL_NAMESPACE"
-    stripped=${serviceCred%-creds}
-    echo $stripped
-#     echo "\c template1 \\\ DROP DATABASE $KUBECTL_NAMESPACE; CREATE DATABASE $KUBECTL_NAMESPACE;" | gen3 psql $service
+    dbName=$(g3kubectl get secrets $serviceCred -o json | jq -r '.data["creds.json"]' | base64 --decode | jq -r  .db_database)
+    service=${serviceCred%-creds}
 
-    echo -e "$(red_color "WARNING: about to drop the $db_name database from the $service postgres server - proceed? (y/n)")"
+    echo -e "$(red_color "WARNING: about to drop the $dbName database from the $service postgres server - proceed? (y/n)")"
     read -r yesno
     if [[ $yesno = "n" ]]; then
         echo "'n' detected, unlocking klock and aborting"
         gen3 klock unlock reset-lock gen3-reset
         exit 1
     fi
+    echo "\c template1 \\\ DROP DATABASE $KUBECTL_NAMESPACE; CREATE DATABASE $KUBECTL_NAMESPACE;" | gen3 psql $service
 done
 
 # gen3 roll all
 # gen3 kube-wait4-pods
-# gen3 job run gdcdb-create; gen3 job run indexd-userdb; gen3 job run usersync
-# gen3 kube-wait4-pods
-# gen3 roll all
+gen3 job run gdcdb-create; gen3 job run indexd-userdb; gen3 job run usersync
+gen3 kube-wait4-pods
+gen3 roll all
 
 gen3 klock unlock reset-lock gen3-reset
