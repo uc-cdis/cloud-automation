05f5cc24e403090130a420c6c61558a737c964e2
Steven Cui <stevencui729@gmail.com>
2018-11-09 14:44:42 -0600

    feat(gen3-reset): forgot about bash whitespace

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -16,7 +16,7 @@ fi
 
 wait_for_pods_down() {
     echo "entered wait"
-    podsDownFlag = 1
+    podsDownFlag=1
     while [[ podsDownFlag == 0 ]]; do
         g3kubectl get pods
         if [[ 0 == "$(g3kubectl get pods -o json | jq -r '[.items[] | { name: .metadata.labels.app } ] | map(select(.name=="fence" or .name=="sheepdog" or .name=="peregrine" or .name=="indexd")) | length')" ]]; then
