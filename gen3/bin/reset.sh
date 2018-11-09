f97fea5401bcdf0f44739ca8a8d9b423a0cbb885
Steven Cui <stevencui729@gmail.com>
2018-11-09 10:15:10 -0600

    feat(gen3-reset): fixed bailout functionality and service name finding

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -15,9 +15,9 @@ fi
 
 gen3 klock lock reset-lock gen3-reset 3600 -w 60
 
-echo -e "$(red_color "WARNING: about to drop the $db_name database from the $service postgres server - proceed?(Y/N)")"
+echo -e "$(red_color "WARNING: about to drop the $db_name database from the $service postgres server - proceed? (y/n)")"
 read -r yesno
-if [[ yesno == "N" ]]; then
+if [[ yesno = "n" ]]; then
     exit 1
 fi
 
@@ -34,7 +34,8 @@ fi
 serviceCreds=( fence-creds sheepdog-creds indexd-creds )
 for serviceCred in ${serviceCreds[@]}; do
     echo $serviceCred
-    kubectl get secrets $serviceCred -o json | jq -r '.data["creds.json"]' | base64 --decode | jq -r  .db_database
+    service=$(kubectl get secrets $serviceCred -o json | jq -r '.data["creds.json"]' | base64 --decode | jq -r  .db_database)
+    echo $service
     echo "$KUBECTL_NAMESPACE"
 #     echo "\c template1 \\\ DROP DATABASE $KUBECTL_NAMESPACE; CREATE DATABASE $KUBECTL_NAMESPACE;" | gen3 psql $service
 done
