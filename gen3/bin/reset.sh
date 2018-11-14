671197bf1a41ce31ee3691f6ffd1ae2659eb33b1
Steven Cui <stevencui729@gmail.com>
2018-11-14 17:13:12 -0600

    feat(gen3-reset): added README and wait for pods to be down logging

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -15,17 +15,17 @@ if [[ -z $KUBECTL_NAMESPACE ]]; then
 fi
 
 wait_for_pods_down() {
-    echo "entered wait"
     podsDownFlag=1
     while [[ podsDownFlag -ne 0 ]]; do
         g3kubectl get pods
         if [[ 0 == "$(g3kubectl get pods -o json | jq -r '[.items[] | { name: .metadata.labels.app } ] | map(select(.name=="fence" or .name=="sheepdog" or .name=="peregrine" or .name=="indexd")) | length')" ]]; then
+            echo "pods are down, ready to drop databases"
             let podsDownFlag=0
         else
             sleep 10
+            echo "pods not done terminating, waiting"
         fi
     done
-    return 0
 }
 
 
