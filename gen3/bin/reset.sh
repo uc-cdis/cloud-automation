6ffd51bd977ea17951adb15f18af0b5ed638cad2
Alexander VanTol <Avantol13@users.noreply.github.com>
2018-12-19 15:33:27 -0600

    fix(db-names): replace - with _ for db names in environments (#631)
    * fix(db-names): replace - with _ for db names in environments
    
    * fix(gen3 reset): get dbname from secrets

--- gen3/bin/reset.sh
+++ gen3/bin/reset.sh
@@ -8,11 +8,6 @@ source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 gen3_load "gen3/lib/kube-setup-init"
 
-# make sure KUBECTL_NAMESPACE env var is set (defaults to current namespace)
-if [[ -z $KUBECTL_NAMESPACE ]]; then
-    export KUBECTL_NAMESPACE=$(g3kubectl get configmap manifest-global -o=jsonpath='{.metadata.namespace}')
-    echo "KUBECTL_NAMESPACE set to $KUBECTL_NAMESPACE"
-fi
 
 wait_for_pods_down() {
     podsDownFlag=1
@@ -32,13 +27,13 @@ wait_for_pods_down() {
 
 gen3 klock lock reset-lock gen3-reset 3600 -w 60
 
-g3kubectl delete --all deployments --namespace=$KUBECTL_NAMESPACE --now
+g3kubectl delete --all deployments --now
 wait_for_pods_down
 
 # drop and recreate all the postgres databases
 serviceCreds=( fence-creds sheepdog-creds indexd-creds )
 for serviceCred in ${serviceCreds[@]}; do
-    dbName=$(g3kubectl get secrets $serviceCred -o json | jq -r '.data["creds.json"]' | base64 --decode | jq -r  .db_database)
+    dbName="$(g3kubectl get secrets $serviceCred -o json | jq -r '.data["creds.json"]' | base64 --decode | jq -r  .db_database)"
     service=${serviceCred%-creds}
 
     # check for user consent before deleting and recreating tables
@@ -49,7 +44,7 @@ for serviceCred in ${serviceCreds[@]}; do
         gen3 klock unlock reset-lock gen3-reset
         exit 1
     fi
-    echo "\c template1 \\\ DROP DATABASE \"$KUBECTL_NAMESPACE\"; CREATE DATABASE \"$KUBECTL_NAMESPACE\";" | gen3 psql $service
+    echo "\c template1 \\\ DROP DATABASE \"${dbName}\"; CREATE DATABASE \"${dbName}\";" | gen3 psql $service
 done
 
 gen3 roll all
