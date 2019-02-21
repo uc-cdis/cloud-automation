309642d9e3d795903cddce5955a29adf0fae9c6a
Renuka Arya <renukarya@MacBook-Pro-53.local>
2019-02-21 13:33:57 -0600

    chore(squid_auto):fix the default route script

--- flavors/squid_auto/default_ip_route_config.sh
+++ flavors/squid_auto/default_ip_route_config.sh
@@ -5,4 +5,7 @@ route_table_id=$(sed -n -e '/VAR2/ s/.*\= *//p' /home/ubuntu/squid_auto_user_var
 squid_auto_interface_id=$(aws ec2 describe-instances --profile cdistest --filters "Name=network-interface.addresses.private-ip-address,Values=$instance_ip" --query 'Reservations[*].Instances[*].{ID:NetworkInterfaces[0].NetworkInterfaceId}' --output text)
 
 
+echo "Creating the default route....."
 aws ec2 create-route --profile cdistest --route-table-id $route_table_id --destination-cidr-block 1.1.1.1/32 --network-interface-id $squid_auto_interface_id
+echo "Updating the default route ...."
+aws ec2 replace-route --profile cdistest --route-table-id $route_table_id --destination-cidr-block 1.1.1.1/32 --network-interface-id $squid_auto_interface_id
