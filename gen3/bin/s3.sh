6ae42562b1b45397caf7e58afe30fc8edf43479e
Ted Summer <ted.summer2@gmail.com>
2019-02-27 18:32:46 +0000

    refactor(s3): refactoring

--- gen3/bin/s3.sh
+++ gen3/bin/s3.sh
@@ -30,45 +30,47 @@ gen3_s3() {
 }
 
 list() {
-  gen3_aws_run aws s3 ls
+  if [[ -z "$1" ]]; then
+    gen3_aws_run aws s3 ls
+  else
+    gen3_aws_run aws s3 ls s3://$1
+  fi
 }
 
 create() {
-  echo $@
   local bucketName=$1
-  # check if bucket exists
-  # if bucket exists, do nothing
-  # don't think this is right, need to check for a full name...
-  # would be nice if there were a function (e.g. in aws.sh) that could determine the name
-  #  otherwise I have to do the whole routine of making the tfplan, then it giving me an error
-  #  before I know that it already exists...
-  #  could at least adhere to a naming convention - I think this already may be setup
-  #  <specialname>-databucket-gen3
-  if [[ -z "$bucketName" ]]; then echo "ERROR: no bucket name provided"; help; exit 1; fi
-  if [[ ! -z "$(aws s3 ls | grep $1)"  ]]; then
+  # do simple validation of bucket name
+  local regexp="^[a-z][a-z0-9\-]*$"
+  if [[ ! $bucketName =~ $regexp ]];then
+    cat << EOF
+ERROR: Bucket name does not meet the following requirements:
+  - starts with a-z
+  - contains only a-z, 0-9, and dashes, "-"
+EOF
+    exit 1
+  fi
+  # if bucket already exists do nothing and exit
+  if aws s3 ls "s3://$bucketName" > /dev/null 2>&1; then
     echo "INFO: Bucket already exists"
     exit 0
   fi
-  # if bucket doesn't exist, make and apply tfplan
-  # setup name
-  # local environment=${vpc_name:-$(g3kubectl get configmap -o jsonpath="{.data.environment}")}
-  # if [[ -z "${environment}" ]]; then
-  #   echo "ERROR: could not determine environment"
-  #   exit 1
-  # fi
-  local workspace_name="${bucketName}_databucket"
-  # TODO: Should each databucket create it's own cloudwatch logs as well?
-  # Currently its configured so that all are directed towards the vpc_name (also for the log-group-name I think??)
-  gen3 workon default $workspace_name
+  # if bucket doesn't exist make and apply tfplan
+  gen3 workon default "${bucketName}_databucket"
   gen3 cd
+  cat << EOF > config.tfvars
+bucket_name="$bucketName"
+environment="${vpc_name:-$(g3kubectl get configmap global -o jsonpath="{.data.environment}")}"
+EOF
   gen3 tfplan
   # gen3 tfapply
+  # gen3 trash
 }
 
 get() {
   # TODO: determine what this function should do
   # also, could add a way to just determine what the bucket name should be based on service...
-  gen3_aws_run aws s3api list-objects --bucket $1
+  # gen3_aws_run aws s3 ls s3://$1
+  echo "Not implemented..."
 }
 
 gen3_s3 "$@"
