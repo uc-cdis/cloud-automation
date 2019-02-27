1584b41979dcdfc758775a442aa843059aaa3f3a
Reuben Pasquini <reuben@frickjack.com>
2019-02-27 12:11:41 -0600

    test(shunitTest): cleanup test output

--- gen3/test/shunitTest.sh
+++ gen3/test/shunitTest.sh
@@ -1,9 +1,9 @@
 
 test_shunit_assert() {
   [[ $SHUNIT_ASSERT_COUNT -eq 0 && $SHUNIT_ASSERT_FAIL -eq 0 ]]; because $? "sshunit_runtest clears ASSERT counters at beginning of test"
-  ([[ 1 -eq 1 ]]; because $? "1 -eq 1"); because $? "because 0($?) returns 0"
+  ([[ 1 -eq 1 ]]; because $? "1 -eq 1"); because $? "valid assertion 0($?) should succeed"
   [[ $SHUNIT_ASSERT_COUNT = 2 ]]; because $? "SHUNIT_ASSERT_COUNT increments after each in-process assertion"
-  ([[ 1 -eq 2 ]]; because $? "should fail 1 -eq 2 - \$? is $?"); [[ $? -eq 1 ]]; because $? "because 1 calls 'exit 1'"
+  ! ([[ 1 -eq 2 ]]; because $? "should fail 1 -eq 2 - \$? is $?") > /dev/null 2>&1; because $? "invalid assertion should fail"
 }
 
 test_fail() {
@@ -13,10 +13,11 @@ test_fail() {
 test_shunit_runtest() {
   # Note - test runs in a sub-process courtesy of test_shunit_runtest
   [[ $SHUNIT_CURRENT_TEST = "test_shunit_runtest" ]]; because $? "shunit_runtest sets the SHUNIT_CURRENT_TEST variable: $SHUNIT_CURRENT_TEST"
+  # send output to /dev/null to avoid confusion
   shunit_clear  # clear counters in sub-process
-  shunit_runtest "test_shunit_assert"
-  shunit_runtest "test_fail"
-  shunit_runtest "test_fail"
+  shunit_runtest "test_shunit_assert"  > /dev/null 2>&1
+  shunit_runtest "test_fail" > /dev/null 2>&1
+  shunit_runtest "test_fail" > /dev/null 2>&1
   echo "SHUNIT_TEST_COUNT is $SHUNIT_TEST_COUNT"
   if [[ $SHUNIT_TEST_COUNT -eq 3 ]]; then
     echo "=3!"
@@ -26,21 +27,22 @@ test_shunit_runtest() {
   [[ $SHUNIT_TEST_COUNT -eq 3 ]]; because $? "3 tests ran: $SHUNIT_TEST_COUNT"
   [[ $SHUNIT_TEST_FAIL -eq 2 ]]; because $? "2 tests failed: $SHUNIT_TEST_FAIL"
   [[ $SHUNIT_FAILED_TESTS = " test_fail test_fail" ]]; because $? "tracked failed tests: $SHUNIT_FAILED_TESTS"
-  shunit_summary
+  shunit_summary  > /dev/null 2>&1
   [[ $SHUNIT_TEST_COUNT -eq 0 && $SHUNIT_TEST_FAIL -eq 0 && $SHUNIT_FAILED_TESTS = "" ]]; because $? "shunit_summary clears counters"
 }
 
 test_shunit_summary() {
   # drop into a subshell, so shunit_summary doesn't clear the counters
+  # send output to /dev/null to avoid confusion
   ! (
     shunit_clear
-    shunit_runtest "test_fail"
-    shunit_summary
+    shunit_runtest "test_fail" > /dev/null 2>&1
+    shunit_summary > /dev/null 2>&1
   ); because $? "shunit_summary should have non-zero result if any tests failed"
   (
     shunit_clear
-    shunit_runtest "test_shunit_assert"
-    shunit_summary
+    shunit_runtest "test_shunit_assert" > /dev/null 2>&1
+    shunit_summary > /dev/null 2>&1
   ); because $? "shunit_summary should have zero result if all tests pass" 
 }
 
