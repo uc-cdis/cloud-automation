2b84ddc04f3d17a469859e92913e9940f0e64d99
Renuka Arya <renukarya@MacBook-Pro-53.local>
2019-03-12 15:34:47 -0500

    chore(squid_auto):reduced health check logging

--- flavors/squid_auto/healthcheck_status.sh
+++ flavors/squid_auto/healthcheck_status.sh
@@ -7,11 +7,12 @@ count_stat1=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-
 count_stat2=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names commons_squid_auto_autoscaling_grp --region us-east-1  --query AutoScalingGroups[].Instances[].HealthStatus --output text | grep -w Unhealthy | awk '{print NF}')
 
 
-echo "The number of Healthy hosts  at $timestamp  is $count_stat1" >> /var/log/squid_health.log
-echo "The number of Unhealthy hosts  at $timestamp is $count_stat2" >> /var/log/squid_health.log
+
 
 
 if [[ "$count_stat1" -lt '2'  ||   "$count_stat2" -ge '1' ]] ; then
+echo "The number of Healthy hosts  at $timestamp  is $count_stat1" >> /var/log/squid_health.log
+echo "The number of Unhealthy hosts  at $timestamp is $count_stat2" >> /var/log/squid_health.log
 echo "Running the private subnets route table script at $timestamp" >> /var/log/squid_health.log
 sudo bash /home/ubuntu/default_ip_route_and_instance_check_config.sh
 echo "Running the route53 DNS update script at $timestamp" >> /var/log/squid_health.log
