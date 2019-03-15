edd3727eb4955f9e1dc6f174a2f8c4f4a06d26cc
Fauzi Gomez <fauzigo@gmail.com>
2019-03-15 15:19:07 -0500

    Chore/es ha PXP-2554 (#734)
    * add(ha and logging): 3 instance cluster and errors log
    
    * del(subnet): using private_kube now
    
    * chore(remove comments):
    
    * rem(shuffle resource): not needed anymore

--- tf_files/aws/modules/commons-vpc-es/cloud.tf
+++ tf_files/aws/modules/commons-vpc-es/cloud.tf
@@ -3,29 +3,11 @@ resource "aws_iam_service_linked_role" "es" {
   aws_service_name = "es.amazonaws.com"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-
-resource "random_shuffle" "az" {
-  input = ["${data.aws_availability_zones.available.names}"] #["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
-  result_count = 1
-  count = 1
-}
-
-data "aws_vpcs" "vpcs" {
-  tags {
-    Name = "${var.vpc_name}"
-  }
-}
-
-# Assuming that there is only one VPC with the vpc_name
-data "aws_vpc" "the_vpc" {
-  id = "${element(data.aws_vpcs.vpcs.ids, count.index)}"
-}
-
-
+#resource "random_shuffle" "az" {
+#  input = ["${data.aws_availability_zones.available.names}"]
+#  result_count = 1
+#  count = 1
+#}
 
 
 resource "aws_security_group" "private_es" {
@@ -54,37 +36,44 @@ resource "aws_security_group" "private_es" {
 }
 
 
-resource "aws_subnet" "private_sn_es" {
-  vpc_id                  = "${element(data.aws_vpcs.vpcs.ids, count.index)}"
-  cidr_block              = "${cidrhost(data.aws_vpc.the_vpc.cidr_block, 256 * 6 )}/24"
-  availability_zone       = "${element(random_shuffle.az.result, count.index)}"
-  map_public_ip_on_launch = false
+#resource "aws_subnet" "private_sn_es" {
+#  vpc_id                  = "${element(data.aws_vpcs.vpcs.ids, count.index)}"
+#  cidr_block              = "${cidrhost(data.aws_vpc.the_vpc.cidr_block, 256 * 6 )}/24"
+#  availability_zone       = "${element(random_shuffle.az.result, count.index)}"
+#  map_public_ip_on_launch = false
 
-  tags {
-    Name         = "private_es"
-    Environment  = "${var.vpc_name}"
-    Organization = "Basic Service"
-  }
-}
+#  tags {
+#    Name         = "private_es"
+#    Environment  = "${var.vpc_name}"
+#    Organization = "Basic Service"
+#  }
+#}
 
 
 resource "aws_elasticsearch_domain" "gen3_metadata" {
   domain_name           = "${var.vpc_name}-gen3-metadata"
-  elasticsearch_version = "6.2"
+  elasticsearch_version = "6.3"
   encrypt_at_rest {
     enabled = "true"
   }
   vpc_options {
     security_group_ids = ["${aws_security_group.private_es.id}"]
-    subnet_ids = ["${aws_subnet.private_sn_es.id}"]
+    subnet_ids = ["${data.aws_subnet_ids.private.ids}"]
   }
   cluster_config {
     instance_type = "m4.large.elasticsearch"
+    instance_count = 3
   }
   ebs_options {
     ebs_enabled = "true"
     volume_size = 20
   }
+
+#  log_publishing_options {
+#    log_type = "ES_APPLICATION_LOGS"
+#    cloudwatch_log_group_arn = "${data.aws_cloudwatch_log_group.logs_group.arn}"
+#    enabled = "true"
+#  }
 
   advanced_options {
     "rest.action.multi.allow_explicit_index" = "true"
@@ -119,7 +108,3 @@ resource "aws_elasticsearch_domain" "gen3_metadata" {
 }
 CONFIG
 }
-
-data "aws_iam_user" "es_user" {
-  user_name = "${var.vpc_name}_es_user"
-} 
