53f9071cafe3ee1372c36eee95b4e77ae6d43ca1
Zakir Gowani <zakirgowani@gmail.com>
2019-03-21 14:43:32 -0500

    squash

--- gen3/bin/kube-setup-manifestservice.sh
+++ gen3/bin/kube-setup-manifestservice.sh
@@ -9,6 +9,27 @@ gen3_load "gen3/gen3setup"
 
 gen3 kube-setup-secrets
 
+
+hostname="$(g3kubectl get configmap global -o json | jq -r .data.hostname)"
+bucketname="manifest-${hostname//./-}"
+gen3 s3 create "$bucketname"
+# this will fail if manifest-user does not exist ... I think -Reuben
+gen3 s3 attach-bucket-policy "$bucketname" --read-write --user-name manifest_bot || true
+
+
+credsFile="$(gen3_secrets_folder)/g3auto/manifestservice/creds.json"
+if [[ (! -f "$credsFile") && -z "$JENKINS_HOME" ]]; then
+  gen3_log_err "manifestservice creds.json not initialized - add AWS creds"
+  cat - > "$credsFile" <<EOM
+{
+  "manifest_bucket_name": "$bucketName",
+  "hostname": "$hostname"
+}
+EOM
+  gen3 secrets sync "initialize manifestservice/creds.json"
+fi
+
+
 # deploy manifest-service
 gen3 roll manifestservice
 g3kubectl apply -f "${GEN3_HOME}/kube/services/manifestservice/manifestservice-service.yaml"
