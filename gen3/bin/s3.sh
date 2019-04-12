dad5cab1139d59b5cf6727950aef89e5f4aca673
Ted Summer <ted.summer2@gmail.com>
2019-04-12 20:18:01 +0000

    fix(gen3_s3): echo result of bucket exists

--- gen3/bin/s3.sh
+++ gen3/bin/s3.sh
@@ -94,9 +94,9 @@ _add_bucket_to_cloudtrail() {
 _bucket_exists() {
   local bucketName=$1
   if [[ -z "$(gen3_aws_run aws s3api head-bucket --bucket $bucketName 2>&1)" ]]; then
-    return "true"
+    echo "true"
   else
-    return "false"
+    echo "false"
   fi
 }
 
