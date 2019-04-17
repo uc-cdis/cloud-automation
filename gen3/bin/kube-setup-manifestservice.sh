3fcee0d94fb7a8a6a6f546d5542ab2d6f1db1bd5
philloooo <phillis.tt@gmail.com>
2019-04-17 12:30:34 -0500

    chore(manifest): fix secrets

--- gen3/bin/kube-setup-manifestservice.sh
+++ gen3/bin/kube-setup-manifestservice.sh
@@ -16,17 +16,19 @@ gen3 s3 create "$bucketname"
 # this will fail if manifest-user does not exist ... I think -Reuben
 gen3 s3 attach-bucket-policy "$bucketname" --read-write --user-name manifest_bot || true
 
-
-credsFile="$(gen3_secrets_folder)/g3auto/manifestservice/creds.json"
+mkdir -p $(gen3_secrets_folder)/g3auto/manifestservice
+credsFile="$(gen3_secrets_folder)/g3auto/manifestservice/config.json"
 if [[ (! -f "$credsFile") && -z "$JENKINS_HOME" ]]; then
-  gen3_log_err "manifestservice creds.json not initialized - add AWS creds"
+  gen3_log_err "initializing manifestservice config.json - need to add AWS creds afterwards"
   cat - > "$credsFile" <<EOM
 {
-  "manifest_bucket_name": "$bucketName",
-  "hostname": "$hostname"
+  "manifest_bucket_name": "$bucketname",
+  "hostname": "$hostname",
+  "aws_access_key_id": "",
+  "aws_secret_access_key", ""
 }
 EOM
-  gen3 secrets sync "initialize manifestservice/creds.json"
+  gen3 secrets sync "initialize manifestservice/config.json"
 fi
 
 
--- kube/services/manifestservice/README.md
+++ kube/services/manifestservice/README.md
@@ -3,3 +3,21 @@ https://github.com/uc-cdis/manifestservice
 
 Microservice that's responsible for creating and retrieving files from a dedicated s3 bucket.
 See repo for more information.
+
+## Deployment
+
+Create an IAM user named "manifest_bot" manually first.
+
+Run
+```
+gen3 kube-setup-manifestservice
+gen3 kube-setup-certs
+gen3 kube-setup-networkpolicy
+gen3 kube-setup-revproxy
+```
+Or just `gen3 roll all`
+
+Copy aws key pair for the manifest_bot user to `$VPC_FOLDER/g3auto/manifestservice/config.json` then run `gen3 secrets sync; gen3 roll manfiestservice`
+
+TODO:
+Will update `kube-setup-manifestservice` after iam automation is added
