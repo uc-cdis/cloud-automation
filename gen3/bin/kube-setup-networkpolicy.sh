233965dbaf9a28e043f282fa6842854efc12ac4b
Reuben Pasquini <reuben@frickjack.com>
2019-04-23 14:50:53 -0500

    chore(netpolicy): checkpoint work

--- gen3/bin/kube-setup-networkpolicy.sh
+++ gen3/bin/kube-setup-networkpolicy.sh
@@ -28,16 +28,80 @@ name2IP() {
   echo "$ip"
 }
 
+
+#
+# Allow communication to all CIDR's except 10/8 and 172.16/12
+#
+externalIPs() {
+  local basePolicy
+  basePolicy="$(mktemp "$XDG_RUNTIME_DIR/netpolicy.json_XXXXXX")"
+  cidrList="$(mktemp "$XDG_RUNTIME_DIR/cidrList.ndjson_XXXXXX")"
+  cat - > "$basePolicy" <<EOM
+{
+    "apiVersion": "extensions/v1beta1",
+    "kind": "NetworkPolicy",
+    "metadata": {
+        "name": "networkpolicy-reuben",
+    },
+    "spec": {
+        "egress": [
+            {
+                "to": [
+                    {
+                        "ipBlock": {
+                            "cidr": "0.0.0.0/0"
+                        }
+                    }
+                ]
+            }
+        ],
+        "podSelector": {
+            "matchLabels": {
+                "app": "reuben"
+            }
+        },
+        "policyTypes": [
+            "Ingress",
+            "Egress"
+        ]
+    }
+}
+EOM
+
+  for ip in {1..254}; do
+    if [[ "$ip" -ne 10 && "$ip" -ne 172 ]]; then
+      cat - >> "$cidrList" <<EOM
+{
+  "ipBlock": {
+    "cidr": "${ip}.0.0.0/8"
+  }
+}
+EOM
+    fi
+  done
+  rm "$basePolicy"
+}
+
 credsPath="$(gen3_secrets_folder)/creds.json"
 if [[ -f "$credsPath" ]]; then # setup netpolicy
-  # google config this is already an IP
-  gdcapi_db_host=$(jq -r .gdcapi.db_host < "$credsPath")
-  indexd_db_host=$(jq -r .indexd.db_host < "$credsPath")
-  fence_db_host=$(jq -r .fence.db_host < "$credsPath")
-
-  GDCAPIDB_IP="$(name2IP "$gdcapi_db_host")"
-  INDEXDDB_IP="$(name2IP "$indexd_db_host")"
-  FENCEDB_IP="$(name2IP "$fence_db_host")"
+  # figure out the hostname associated with each service's database
+  dbServices=(fence indexd sheepdog)
+  tempSecrets="$(mktemp "$XDG_RUNTIME_DIR/secrets.json_XXXXXX")"
+  g3kubectl get secrets -o json | jq -r '.items | map(select( .data["dbcreds.json"] and (.metadata.name|test("-g3auto$")))) | map( { "name": .metadata.name })' > "$tempSecrets"
+  numSecrets="$(jq -r '. | length' < "$tempSecrets")"
+  for ((i=0; i < numSecrets; i++)); do
+    service="$(jq -r ".[${i}].name" < "$tempSecrets")"
+    service="${service%-g3auto}"
+    dbServices+=("$service")
+  done
+  /bin/rm "$tempSecrets"
+  
+  dbArgs=()
+  for serviceName in "${dbServices[@]}"; do
+    host="$(gen3 db creds "$serviceName" | jq -r .db_host)"
+    varName="GEN3_${serviceName^^}_DBIP"
+    dbArgs+=("$varName" "$(name2IP "$host")")
+  done
 
   #
   # Replace this with something better later ...
@@ -45,28 +109,7 @@ if [[ -f "$credsPath" ]]; then # setup netpolicy
   #
   CLOUDPROXY_CIDR="172.0.0.0/8"
 
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_fence_templ.yaml" GEN3_FENCEDB_IP "$FENCEDB_IP" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_fenceshib_templ.yaml" GEN3_FENCEDB_IP "$FENCEDB_IP" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_google-sa-validation_templ.yaml" GEN3_FENCEDB_IP "$FENCEDB_IP" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_indexd_templ.yaml" GEN3_INDEXDDB_IP "$INDEXDDB_IP" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_peregrine_templ.yaml" GEN3_GDCAPIDB_IP "$GDCAPIDB_IP" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_wts_templ.yaml" GEN3_GDCAPIDB_IP "$GDCAPIDB_IP" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_pidgin_templ.yaml" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_arborist_templ.yaml" GEN3_FENCEDB_IP "$FENCEDB_IP" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_ssjdispatcher_templ.yaml" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_ssjdispatcherjob_templ.yaml" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_sheepdog_templ.yaml" GEN3_GDCAPIDB_IP "$GDCAPIDB_IP" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_portal_templ.yaml" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_revproxy_templ.yaml" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" | g3kubectl apply -f -
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_jenkins_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_allowdns_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_gen3job_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_arranger_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_aws_es_proxy.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_arranger_dashboard_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_tube_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_spark_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_manifestservice_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_guppy_templ.yaml"
-  g3kubectl apply -f "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_sower_templ.yaml"
+  for name in "${GEN3_HOME}/kube/services/netpolicy/networkpolicy"*.yaml; do
+    (g3k_kv_filter "${GEN3_HOME}/kube/services/netpolicy/networkpolicy_fence_templ.yaml" GEN3_CLOUDPROXY_CIDR "$CLOUDPROXY_CIDR" "${dbArgs[@]}" | g3kubectl apply -f -) || true
+  done
 fi
