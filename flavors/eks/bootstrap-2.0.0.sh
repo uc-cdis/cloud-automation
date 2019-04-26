d581e2dc9d97f5d23f92dec8d83fc5c910b75949
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-04-26 12:07:03 -0500

    fix(move-ssh-copy): Moved the ssh key copy to the top of the bootsrap script so that users can login and troubleshoot if script fails

--- flavors/eks/bootstrap-2.0.0.sh
+++ flavors/eks/bootstrap-2.0.0.sh
@@ -3,6 +3,8 @@
 # User data for our EKS worker nodes basic arguments to call the bootstrap script for EKS images 
 # More info https://docs.aws.amazon.com/eks/latest/userguide/getting-started.html
  
+sudo cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/authorized_keys
+
 sudo sysctl fs.inotify.max_user_watches=12000
 
 KUBELET_EXTRA_ARGUMENTS="--node-labels=role=${nodepool}"
@@ -16,5 +18,3 @@ fi
 cat > /home/ec2-user/.ssh/authorized_keys <<EFO
 ${ssh_keys}
 EFO
-
-sudo cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/authorized_keys
