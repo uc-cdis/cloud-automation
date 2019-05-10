4c30f14da8b3927f2ab54c6c747c3822036e32d5
Alexander VanTol <avantol13@gmail.com>
2019-05-10 12:44:01 -0500

    fix(indexd): pull cfg from secrets folder (not cloud auto), for now, default to old deployment without arborist

--- apis_configs/indexd_settings.py
+++ apis_configs/indexd_settings.py
@@ -5,53 +5,55 @@ import config_helper
 from os import environ
 import json
 
-APP_NAME='indexd'
+APP_NAME = "indexd"
+
+
 def load_json(file_name):
-  return config_helper.load_json(file_name, APP_NAME)
+    return config_helper.load_json(file_name, APP_NAME)
 
-conf_data = load_json('creds.json')
 
-usr = conf_data.get('db_username', '{{db_username}}')
-db = conf_data.get('db_database', '{{db_database}}')
-psw = conf_data.get('db_password', '{{db_password}}')
-pghost = conf_data.get('db_host', '{{db_host}}')
+conf_data = load_json("creds.json")
+
+usr = conf_data.get("db_username", "{{db_username}}")
+db = conf_data.get("db_database", "{{db_database}}")
+psw = conf_data.get("db_password", "{{db_password}}")
+pghost = conf_data.get("db_host", "{{db_host}}")
 pgport = 5432
-index_config = conf_data.get('index_config')
+index_config = conf_data.get("index_config")
 CONFIG = {}
 
-CONFIG['JSONIFY_PRETTYPRINT_REGULAR'] = False
+CONFIG["JSONIFY_PRETTYPRINT_REGULAR"] = False
 
-dist = environ.get('DIST', None)
+dist = environ.get("DIST", None)
 if dist:
-  CONFIG['DIST'] = json.loads(dist)
+    CONFIG["DIST"] = json.loads(dist)
 
-CONFIG['INDEX'] = {
-'driver': SQLAlchemyIndexDriver('postgresql+psycopg2://{usr}:{psw}@{pghost}:{pgport}/{db}'.format(
-    usr=usr,
-    psw=psw,
-    pghost=pghost,
-    pgport=pgport,
-    db=db,
-), index_config=index_config),
+CONFIG["INDEX"] = {
+    "driver": SQLAlchemyIndexDriver(
+        "postgresql+psycopg2://{usr}:{psw}@{pghost}:{pgport}/{db}".format(
+            usr=usr, psw=psw, pghost=pghost, pgport=pgport, db=db
+        ),
+        index_config=index_config,
+    )
 }
 
-CONFIG['ALIAS'] = {
-'driver': SQLAlchemyAliasDriver('postgresql+psycopg2://{usr}:{psw}@{pghost}:{pgport}/{db}'.format(
-    usr=usr,
-    psw=psw,
-    pghost=pghost,
-    pgport=pgport,
-    db=db,
-)),
+CONFIG["ALIAS"] = {
+    "driver": SQLAlchemyAliasDriver(
+        "postgresql+psycopg2://{usr}:{psw}@{pghost}:{pgport}/{db}".format(
+            usr=usr, psw=psw, pghost=pghost, pgport=pgport, db=db
+        )
+    )
 }
 
-AUTH = SQLAlchemyAuthDriver('postgresql+psycopg2://{usr}:{psw}@{pghost}:{pgport}/{db}'.format(
-    usr=usr,
-    psw=psw,
-    pghost=pghost,
-    pgport=pgport,
-    db=db,
-    arborist="http://arborist-service/",
-))
+AUTH = SQLAlchemyAuthDriver(
+    "postgresql+psycopg2://{usr}:{psw}@{pghost}:{pgport}/{db}".format(
+        usr=usr,
+        psw=psw,
+        pghost=pghost,
+        pgport=pgport,
+        db=db,
+        # arborist="http://arborist-service/",  # uncomment if you're using arborist
+    )
+)
 
-settings = {'config': CONFIG, 'auth': AUTH}
+settings = {"config": CONFIG, "auth": AUTH}
