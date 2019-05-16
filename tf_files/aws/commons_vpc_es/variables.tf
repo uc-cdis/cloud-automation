7a6f0c0322889c44d79c142f5ef26e65f7d095cf
emalinowski <emalinowski@uchicago.edu>
2019-05-16 10:37:25 -0500

    fix(es alarms): fixed slack webhook variables to have default value (#800)

--- tf_files/aws/commons_vpc_es/variables.tf
+++ tf_files/aws/commons_vpc_es/variables.tf
@@ -1,7 +1,12 @@
 
 variable "vpc_name" {}
-variable "slack_webhook" {}
-variable "secondary_slack_webhook" {}
+
+variable "slack_webhook" {
+  default = ""
+}
+variable "secondary_slack_webhook" {
+  default  = ""
+}
 
 variable "instance_type" {
   default = "m4.large.elasticsearch"
