84a2aa1bb2250119ad283b5c49fe5fd59299198d
Reuben Pasquini <reuben@frickjack.com>
2019-06-25 09:36:04 -0500

    chore(peregrine): hpa (#903)
    horizontal pod autoscaler

--- gen3/bin/kube-setup-metrics.sh
+++ gen3/bin/kube-setup-metrics.sh
@@ -18,17 +18,40 @@ source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 
 
-if [[ "$(gen3 db namespace)" == "default" ]] && (! g3kubectl get deployment metrics-server --namespace kube-system > /dev/null 2>&1); then
-  (
-    cd "$XDG_RUNTIME_DIR"
-    #DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/kubernetes-incubator/metrics-server/releases/latest" | jq -r .tarball_url)
-    DOWNLOAD_URL="https://github.com/kubernetes-incubator/metrics-server/archive/v0.3.3.tar.gz"
-    DOWNLOAD_VERSION=$(grep -o '[^/v]*$' <<< $DOWNLOAD_URL)
-    curl -Ls "$DOWNLOAD_URL" -o metrics-server-$DOWNLOAD_VERSION.tar.gz
-    mkdir metrics-server-$DOWNLOAD_VERSION
-    tar -xzf metrics-server-$DOWNLOAD_VERSION.tar.gz --directory metrics-server-$DOWNLOAD_VERSION --strip-components 1
-    export KUBECTL_NAMESPACE=kube-system
-    g3kubectl apply -f metrics-server-$DOWNLOAD_VERSION/deploy/1.8+/
-    /bin/rm -rf "metrics-server-$DOWNLOAD_VERSION"
-  )
-fi
+gen3_metrics_deploy() {
+  if [[ "$(gen3 db namespace)" == "default" ]] && (! g3kubectl get deployment metrics-server --namespace kube-system > /dev/null 2>&1); then
+    (
+      cd "$XDG_RUNTIME_DIR"
+      #DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/kubernetes-incubator/metrics-server/releases/latest" | jq -r .tarball_url)
+      DOWNLOAD_URL="https://github.com/kubernetes-incubator/metrics-server/archive/v0.3.3.tar.gz"
+      DOWNLOAD_VERSION=$(grep -o '[^/v]*$' <<< $DOWNLOAD_URL)
+      curl -Ls "$DOWNLOAD_URL" -o metrics-server-$DOWNLOAD_VERSION.tar.gz
+      mkdir metrics-server-$DOWNLOAD_VERSION
+      tar -xzf metrics-server-$DOWNLOAD_VERSION.tar.gz --directory metrics-server-$DOWNLOAD_VERSION --strip-components 1
+      export KUBECTL_NAMESPACE=kube-system
+      g3kubectl apply -f metrics-server-$DOWNLOAD_VERSION/deploy/1.8+/
+      /bin/rm -rf "metrics-server-$DOWNLOAD_VERSION"
+    )
+  else
+    gen3_log_info "not deploying metrics outside default namespace"
+  fi
+}
+
+gen3_metrics_check() {
+  [[ "True" == "$(g3kubectl get apiservices.apiregistration.k8s.io v1beta1.metrics.k8s.io -o json 2> /dev/null | jq -e -r '.status.conditions | map(.key=.type | .value=.status) | from_entries | .Available' 2> /dev/null)" ]]
+}
+
+command="$1"
+shift
+case "$command" in
+  "deploy"):
+    gen3_metrics_deploy "$@"
+    ;;
+  "check"):
+    gen3_metrics_check "$@"
+    ;; 
+  *)
+    gen3_log_err "unknown option: $command"
+    gen3 help kube-setup-metrics
+    ;;
+esac
