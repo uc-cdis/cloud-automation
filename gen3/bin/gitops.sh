cadb142ebe1d25ff58b0bedc8ee6415066c287b5
Fauzi <fauzigo@gmail.com>
2019-07-12 16:45:43 -0500

    add(debugger)

--- gen3/bin/gitops.sh
+++ gen3/bin/gitops.sh
@@ -92,20 +92,22 @@ gen3_run_tfplan() {
     # for whatever reson 
     case "$module" in
       "vpc")
+        echo "by VPC"
         message=$(_gen3_run_tfplan_vpc)
         ;;
       "eks")
+        echo "by EKS"
         message=$(_gen3_run_tfplan_eks)
         ;;
     esac
   fi
   echo ${message}
   echo $XDG_RUNTIME_DIR
-  ls $XDG_RUNTIME_DIR
+  ls -la $XDG_RUNTIME_DIR
 
   if [ -n "${message}" ];
   then
-    aws sns publish --target-arn ${sns_topic} --message file://${message} > /dev/null 2>&1
+    aws sns publish --target-arn ${sns_topic} --message file://${message} #> /dev/null 2>&1
     rm ${message}
   fi
 
@@ -121,7 +123,7 @@ _gen3_run_tfplan_vpc() {
   local slack_hook
   local tempFile
 
-  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1) ${vpc_name} > /dev/null 2>&1
+  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1) ${vpc_name} #> /dev/null 2>&1
   plan=$(gen3 tfplan | grep "Plan")
 
   if [ -n "${plan}" ];
