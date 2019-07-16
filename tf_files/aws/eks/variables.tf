77fa74c34c3b30865a886af476b9e8da52e5df3e
Fauzi <fauzigo@gmail.com>
2019-07-16 11:17:01 -0500

    chore(eks version): bumped to 1.12 for default value

--- tf_files/aws/eks/variables.tf
+++ tf_files/aws/eks/variables.tf
@@ -13,7 +13,6 @@ variable "jupyter_instance_type"{
   default = "t3.medium"
 }
 
-#variable "csoc_cidr" {
 variable "peering_cidr" {
   default = "10.128.0.0/20"
 }
@@ -30,12 +29,9 @@ variable "worker_drive_size" {
 }
 
 variable "eks_version" {
-  default = "1.11"
+  default = "1.12"
 }
 
-#variable "deploy_jupyter_pool" {
-#  default = "no"
-#}
 
 variable "workers_subnet_size" {
   default = 24
