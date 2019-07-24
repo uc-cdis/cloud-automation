92d14ea55124df74550df97f6aaaf85f99aa9793
Reuben Pasquini <reuben@frickjack.com>
2019-07-24 17:19:59 -0500

    chore(gen3-ambassador): setup new revproxy

--- gen3/bin/kube-setup-ambassador.sh
+++ gen3/bin/kube-setup-ambassador.sh
@@ -8,9 +8,15 @@ source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 namespace="$(gen3 db namespace)"
 
+
 g3k_kv_filter ${GEN3_HOME}/kube/services/ambassador/ambassador-rbac.yaml AMBASSADOR_BINDING "name: ambassador-binding-$namespace" CURRENT_NAMESPACE "namespace: $namespace" | g3kubectl apply -f -
 gen3 roll ambassador
 g3kubectl apply -f "${GEN3_HOME}/kube/services/ambassador/ambassador-service.yaml"
+
+if g3k_manifest_lookup '.versions["ambassador-gen3"]' 2> /dev/null; then
+  gen3 roll ambassador-gen3
+  g3k_kv_filter "${GEN3_HOME}/kube/services/ambassador-gen3/ambassador-gen3-service.yaml" GEN3_ARN "$(g3kubectl get configmap global --output=jsonpath='{.data.revproxy_arn}')" | g3kubectl apply -f - 
+fi
 
 cat <<EOM
 The ambassador services has been deployed onto the k8s cluster.
---
+++ kube/services/ambassador-gen3/README.md
@@ -0,0 +1,23 @@
+# TL;DR
+
+Ambassador proxy for gen3 services.
+The other ambassador deployment manages user notebooks.
+
+## Gen3 Customization
+
+### CSRF
+
+### Cookie to JWT
+
+### Authz
+
+### Logging
+
+
+## References
+
+* global configuration customiztion: https://www.getambassador.io/reference/core/ambassador/#lua-scripts-lua_scripts
+* statsd integration: https://www.getambassador.io/reference/statistics/
+* running multiple ambassador instances in same namespace: https://www.getambassador.io/reference/running/
+* ambassador mappings: https://www.getambassador.io/reference/mappings/
+
