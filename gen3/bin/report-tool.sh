7ca3cb732f1d6d8285fdd441bc5d612212004655
Fauzi <fauzigo@gmail.com>
2019-08-29 13:53:32 -0500

    add(formats): json and plain text

--- gen3/bin/report-tool.sh
+++ gen3/bin/report-tool.sh
@@ -4,6 +4,7 @@
 #
 
 export GEN3_HOME="${GEN3_HOME:-"$HOME/cloud-automation"}"
+SCRIPT=$(basename ${BASH_SOURCE[0]})
 
 if ! [[ -d "$GEN3_HOME" ]]; then
   echo "ERROR: this does not look like a gen3 environment - check $GEN3_HOME and $KUBECONFIG"
@@ -26,50 +27,93 @@ PATH="${PATH}:/usr/local/bin"
 
 
 function kube_facts() {
+  local k8s_version=$(g3kubectl version  -o json)
+  echo "${k8s_version}"
+}
 
-  eks_information="$( aws eks describe-cluster --name ${vpc_name} )"
-  eks_version=$(echo -e ${eks_information} | jq -r .cluster.version)
+function eks_facts() {
 
-  k8s_version=$(g3kubectl version  -o json)
-
+  local eks_information="$( aws eks describe-cluster --name ${vpc_name} )"
+  local eks_version="$(echo -e ${eks_information} | jq -r .cluster.version)"
+  echo "${eks_information}"
 }
 
 function instances_facts() {
 
-  local instances_name="eks-${vpc_name}"
-  instance_list="$(aws ec2 describe-instances --filter "Name=tag:Name,Values=${instances_name}*")"
-  instance_names="$(echo -e ${instance_list}| jq -r '.Reservations[].Instances[].Tags[] | select( .Key=="Name" ) | .Value')"
+  local blob='{"pool": [] }'
+  local cluster_instances_name="eks-${vpc_name}"
+  local instance_list="$(aws ec2 describe-instances --filter "Name=tag:Name,Values=${cluster_instances_name}*")"
+  local instance_names="$(echo ${instance_list}| jq -r '.Reservations[].Instances[].Tags[] | select( .Key=="Name" ) | .Value')"
+  local total_instance_count="$(echo "${instance_names}" | wc -l)"
+  local pools=$(printf "%s\n" ${instance_names} | sort | uniq)
 
-  total_instance_count="$(echo -e "${instance_names}" | wc -l)"
+  for pool in ${pools};
+  do
+    local pool_instance_count=$(echo "${instance_names}" | grep -o ${pool} | wc -l)
+    local pool_instances="$(echo ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${pool}'" )')"
+    local pool_instance_type="$(echo "${pool_instances}" |jq .InstanceType | sort | uniq |tr "\n" ",")"
+    local pool_ami_id=$(echo ${instance_list} | jq '.Reservations[].Instances[] | select( .Tags[].Value=="'${pool}'" ) |.ImageId' | sort |uniq |tr "\n" ",")
+#    pool_ami
+    blob=$(echo ${blob} | jq '.pool |= (.+ [{"'${pool}'":{count:'${pool_instance_count}',type:['${pool_instance_type::-1}'],AMIs:['${pool_ami_id::-1}']}}])')
+  done
+  echo "${blob}"
+#  exit
 
-  jupyter_instances_count=$(echo -e "${instance_names}" | grep jupyter | wc -l)
-  default_instances_count=$(echo -e "${instance_names}" | grep -v jupyter | wc -l)
 
-  # echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}-jupyter'") | .Tags[] |select( .Key=="Name" )| .Value' |wc -l
-  jupyter_instances="$(echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}-jupyter'")')"
-  default_instances="$(echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}'")')"
+# local jupyter_instances_count=$(echo "${instance_names}" | grep jupyter | wc -l)
+# local default_instances_count=$(echo "${instance_names}" | grep -v jupyter | wc -l)
 
-  #jupyter_instance_type="$(echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}-jupyter'") | .InstanceType' |sort |uniq)"
-  jupyter_instance_type="$(echo -e "${jupyter_instances}" |jq .InstanceType | sort | uniq)"
-  #default_instance_type="$(echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}'") | .InstanceType' |sort |uniq)"
-  default_instance_type="$(echo -e "${default_instances}" |jq .InstanceType | sort | uniq)"
+# # echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}-jupyter'") | .Tags[] |select( .Key=="Name" )| .Value' |wc -l
+# local jupyter_instances="$(echo ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}-jupyter'")')"
+# local default_instances="$(echo ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}'")')"
 
-  images_id=$(echo -e ${instance_list} | jq -r .Reservations[].Instances[].ImageId | sort |uniq)
+# #jupyter_instance_type="$(echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}-jupyter'") | .InstanceType' |sort |uniq)"
+# local jupyter_instance_type="$(echo "${jupyter_instances}" |jq .InstanceType | sort | uniq)"
+# #default_instance_type="$(echo -e ${instance_list} |jq -r '.Reservations[].Instances[] | select( .Tags[].Value=="'${instances_name}'") | .InstanceType' |sort |uniq)"
+# local default_instance_type="$(echo "${default_instances}" |jq .InstanceType | sort | uniq)"
+
+# local images_id=$(echo ${instance_list} | jq -r .Reservations[].Instances[].ImageId | sort |uniq)
+
+# blob=$(echo ${blobl} | jq '.pool | ')
 }
 
+function print_report_full_json() {
+
+  echo -e "$(eks_facts)" "$(kube_facts)" "$(instances_facts)" |jq -s '.[0] * .[1] * .[2]'
+  #echo $(instances_facts) | jq .
+}
 
 function print_report_full() {
 
+  local eks_facts=$(eks_facts)
+  local kube_facts=$(kube_facts)
+  local instances_facts=$(instances_facts)
+
+  local pool_size=$(echo ${instances_facts} |jq -r '.pool| length')
+
   echo
   echo ${vpc_name}
-  echo -e "EKS version: ${eks_version}"
-  echo -e "K8s version: Client-> $(echo -e ${k8s_version} | jq -r '.clientVersion| "\(.major).\(.minor)"') -- Server-> $(echo -e ${k8s_version} | jq -r '.serverVersion | "\(.major).\(.minor)"')"
+  echo -e "EKS version: $(echo -e ${eks_facts} | jq -r .cluster.version)"
+  echo -e "K8s version: Client-> $(echo -e ${kube_facts} | jq -r '.clientVersion| "\(.major).\(.minor)"') -- Server-> $(echo -e ${kube_facts} | jq -r '.serverVersion | "\(.major).\(.minor)"')"
   echo
-  echo -e "Pool\t\tCount\t\tFlavor"
-  echo -e "default\t\t${default_instances_count}\t\t$(echo ${default_instance_type})"
-  echo -e "jupyter\t\t${jupyter_instances_count}\t\t$(echo ${jupyter_instance_type})"
-  echo
-  echo -e "AMIs: "${images_id}
+  #echo -e "Pool\t\t\t\tCount\t\tFlavor"
+  local table_header="Pool Count Flavor"
+  local table_content=""
+  for (( i = 0; i < ${pool_size}; i++ ))
+  do
+    local pool_key="$(echo ${instances_facts} | jq -r '.pool['${i}']| keys[]')"
+    local pool_count="$(echo ${instances_facts} | jq -r '.pool['${i}']."'${pool_key}'".count')"
+    local pool_types="$(echo ${instances_facts} | jq -r '.pool['${i}']."'${pool_key}'".type[]')"
+    table_content="${pool_key} ${pool_count} ${pool_types} ${table_content}"
+  done
+  local IFS=' '
+  read -ra TABLE <<< "${table_header} ${table_content}"
+  printf "%-30s%-10s%s\n" "${TABLE[@]}" 
+#  echo -e "default\t\t$(echo ${instances_facts} | jq -r '.pool[0]."eks-'${vpc_name}'".count')\t\t$(echo ${instances_facts} | jq -r '.pool[0]."eks-'${vpc_name}'".type[]')"
+#  echo -e "jupyter\t\t$(echo ${instances_facts} | jq -r '.pool[1]."eks-'${vpc_name}'-jupyter".count')\t\t$(echo ${instances_facts} | jq -r '.pool[1]."eks-'${vpc_name}'-jupyter".type[]')"
+#  echo -e "jupyter\t\t${jupyter_instances_count}\t\t$(echo ${jupyter_instance_type})"
+#  echo
+#  echo -e "AMIs: "${images_id}
 
 }
 
@@ -78,12 +122,12 @@ BOLD='\e[1;31m'         # Bold Red
 REV='\e[1;32m'       # Bold Green
 
 #Help function
-function HELP {
+function HELP() {
   gen3 help report-tool
   echo -e "${REV}Basic usage:${OFF} ${BOLD}$SCRIPT -r full ${OFF}"\\n
   echo -e "${REV}The following switches are recognized. $OFF "
   echo -e "${REV}-r --report ${OFF}  Print a report, valid types are 'full' only ${OFF}."
-#  echo -e "Example: ${BOLD}$SCRIPT -d helloworld -p /opt/py27env/bin -v 2.7 ${OFF}"\\n
+  echo -e "Example: ${BOLD}$SCRIPT -r full ${OFF}"\\n
   exit 1
 }
 
@@ -92,9 +136,18 @@ function main() {
 
   case "${1}" in
     "full")
-      kube_facts
-      instances_facts
-      print_report_full
+      case "${2}" in
+        "plain")
+          print_report_full
+          ;;
+        "json")
+          print_report_full_json
+          ;;
+      esac
+      #kube_facts
+      #instances_facts
+      #print_report_full_json
+      #print_report_full
       ;;
     *)
       echo "Unknown value for the report"
@@ -103,24 +156,31 @@ function main() {
   esac
 }
 
-OPTSPEC="hr:-:"
+OPTSPEC="ho:r:-:"
 while getopts "$OPTSPEC" optchar; do
   case "${optchar}" in
     -)
       case "${OPTARG}" in
         report)
-          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          main ${val}
+          REPORT_TYPE="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          #main ${val}
           #echo "Parsing option any: '--${OPTARG}', value: '${val}'" >&2;
           ;;
         report=*)
-          val=${OPTARG#*=}
-          opt=${OPTARG%=$val}
+          REPORT_TYPE=${OPTARG#*=}
+          #opt=${OPTARG%=$val}
           #echo "Parsing option eq: '--${opt}', value: '${val}'" >&2
-          main ${val}
+          #main ${val}
+          ;;
+        output)
+          OUTPUT="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        output=*)
+          OUTPUT=${OPTARG#*=}
           ;;
         help)
           HELP
+          exit
           ;;
         *)
           if [ "$OPTERR" = 1 ] && [ "${OPTSPEC:0:1}" != ":" ]; then
@@ -132,7 +192,11 @@ while getopts "$OPTSPEC" optchar; do
       esac;;
     r)
       #echo "Parsing option: '${OPTARG}'" >&2
-      main ${OPTARG}
+      REPORT_TYPE=${OPTARG}
+      #main ${OPTARG}
+      ;;
+    o)
+      OUTPUT=${OPTARG}
       ;;
     h)
       HELP
@@ -146,3 +210,12 @@ while getopts "$OPTSPEC" optchar; do
       ;;
     esac
 done
+
+if [ -z ${OUTPUT} ];
+then 
+  OUTPUT="plain"
+fi
+
+#echo "$OUTPUT - $REPORT_TYPE"
+#exit
+main ${REPORT_TYPE} ${OUTPUT}
