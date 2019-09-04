dc2289850f4e1bc1ee778ea6557ef1aba485a1aa
Reuben Pasquini <reuben@frickjack.com>
2019-09-04 17:25:33 -0500

    chore(dashboard): gen3 dashboard setup

--- gen3/lib/bash-completions.sh
+++ gen3/lib/bash-completions.sh
@@ -7,6 +7,8 @@ gen3_completions() {
     COMPREPLY=($(compgen -W "roll update_config $(/bin/ls -1 "$GEN3_HOME/gen3/bin" | sed -e 's/.sh$//')" "${COMP_WORDS[1]}"))
   elif [[ ${COMP_CWORD} -eq 2 && "${COMP_WORDS[1]}" == "es" ]]; then
     COMPREPLY=($(compgen -W "filter configmaps alias indices delete dump export import mapping port-forward" "${COMP_WORDS[2]}"))
+  elif [[ ${COMP_CWORD} -eq 2 && "${COMP_WORDS[1]}" == "dashboard" ]]; then
+    COMPREPLY=($(compgen -W "prefix publish gitops-sync" "${COMP_WORDS[2]}"))
   elif [[ ${COMP_CWORD} -eq 2 && "${COMP_WORDS[1]}" == "gitops" ]]; then
     COMPREPLY=($(compgen -W "filter configmaps rsync sshlist sync repolist taglist dotag tfplan tfapply" "${COMP_WORDS[2]}"))
   elif [[ ${COMP_CWORD} -eq 2 && "${COMP_WORDS[1]}" == "job" ]]; then
@@ -19,6 +21,9 @@ gen3_completions() {
     COMPREPLY=($(compgen -W "$(/bin/ls "${GEN3_HOME}/kube/services" | grep -v -e job -e netpolicy)" "${COMP_WORDS[2]}"))
   elif [[ ${COMP_CWORD} -eq 3 && "${COMP_WORDS[1]}" == "job" && ("${COMP_WORDS[2]}" == "run" || "${COMP_WORDS[2]}" == "logs") ]]; then
     COMPREPLY=($(compgen -W "$(/bin/ls "${GEN3_HOME}/kube/services/jobs" | grep -e yaml | sed -e 's/\.yaml$//' | grep -e '-job' -e '-cronjob' | sed -e 's/-job.*$//')" "${COMP_WORDS[3]}"))
+  elif [[ ${COMP_CWORD} -eq 3 && "${COMP_WORDS[1]}" == "update_config" ]]; then
+    # see https://unix.stackexchange.com/questions/151118/understand-compgen-builtin-command
+    COMPREPLY=($(compgen -f "${COMP_WORDS[3]}"))
   else
     COMPREPLY=("${COMP_CWORD}" "${COMP_WORDS[2]}")
   fi
