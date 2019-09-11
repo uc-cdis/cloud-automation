9fae8d8e5614f273f4c6262e0f8643ce14add1db
Fauzi <fauzigo@gmail.com>
2019-09-11 10:43:37 -0500

    rem(prints): leftovers from debugging

--- files/lambda/security_alerts.py
+++ files/lambda/security_alerts.py
@@ -13,17 +13,14 @@ def lambda_handler(event, context):
         client = boto3.client('cloudtrail')
         if event['detail']['eventName'] == 'StopLogging':
             response = client.start_logging(Name=event['detail']['requestParameters']['name'])
-            print(response)
             client2 = boto3.client('sns')
             response = client2.publish(
                 TopicArn=os.environ['topic'],
                 Message=json.dumps({'default': json.dumps(event['detail'])}),
                 MessageStructure='json'
                 )
-            print(response)
         else:
             print(event['detail']['eventName'])
     except Exception as e:
         print(e)
         #sys.exit();
-
