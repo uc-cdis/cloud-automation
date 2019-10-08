6aed9c5ba9818a8136343250b259c6168807d3dd
Zakir Gowani <zakirgowani@gmail.com>
2019-10-08 13:51:27 -0500

    feat: data ingestion pipeline job

--- gen3/bin/kube-setup-data-ingestion-job.sh
+++ gen3/bin/kube-setup-data-ingestion-job.sh
@@ -4,45 +4,64 @@
 # This is an optional service that's not part of gen3 core services
 # It only needs to be deployed to DataSTAGE
 
+
+
+
+
+
+
 source "${GEN3_HOME}/gen3/lib/utils.sh"
-gen3_load "gen3/gen3setup"
+gen3_load "gen3/lib/kube-setup-init"
 
 gen3 kube-setup-secrets
 
-# hostname="$(g3kubectl get configmap global -o json | jq -r .data.hostname)"
-# bucketname="manifest-${hostname//./-}"
+gen3 kube-setup-roles
 
-mkdir -p $(gen3_secrets_folder)/g3auto/data-ingestion-job
-credsFile="$(gen3_secrets_folder)/g3auto/data-ingestion-job/config.json"
-echo "kube-setup-data-ingestion-job line 17"
-if (! (g3kubectl describe secret data-ingestion-job-secret 2> /dev/null | grep config.js > /dev/null 2>&1)) \
-  && [[ (! -f "$credsFile") && -z "$JENKINS_HOME" ]]; 
-then
-  echo "kube-setup-data-ingestion-job line 21"
-  gen3_log_info "kube-setup-data-ingestion-job" "setting up data-ingestion-job resources"
-  # gen3 s3 create "$bucketname"
-  gen3 awsuser create data-ingestion-bot
-  # gen3 s3 attach-bucket-policy "$bucketname" --read-write --user-name data-ingestion-bot
-  gen3_log_info "initializing data-ingestion-job config.json"
-  user=$(gen3 secrets decode data-ingestion-job-secret awsusercreds.json)
-  key_id=$(jq -r .id <<< $user)
-  access_key=$(jq -r .secret <<< $user)
-  echo "kube-setup-data-ingestion-job line 30"
-  echo "$key_id"
-  cat - > "$credsFile" <<EOM
-{
-  "aws_access_key_id": "$key_id",
-  "aws_secret_access_key": "$access_key"
-}
-EOM
-  gen3 secrets sync "initialize data-ingestion-job/config.json"
-fi
+g3kubectl apply -f "${GEN3_HOME}/kube/services/jobs/data-ingestion-job.yaml"
 
-echo "kube-setup-data-ingestion-job line 38"
-# gen3 roll data-ingestion-job
-# g3kubectl apply -f "${GEN3_HOME}/kube/services/data-ingestion-job/data-ingestion-job-service.yaml"
-gen3 runjob data-ingestion-job
 
-# cat <<EOM
-# The data ingestion job has been deployed onto the k8s cluster.
+
+
+
+# source "${GEN3_HOME}/gen3/lib/utils.sh"
+# gen3_load "gen3/gen3setup"
+
+# gen3 kube-setup-secrets
+
+# # hostname="$(g3kubectl get configmap global -o json | jq -r .data.hostname)"
+# # bucketname="manifest-${hostname//./-}"
+
+# mkdir -p $(gen3_secrets_folder)/g3auto/data-ingestion-job
+# credsFile="$(gen3_secrets_folder)/g3auto/data-ingestion-job/config.json"
+# echo "kube-setup-data-ingestion-job line 17"
+# if (! (g3kubectl describe secret data-ingestion-job-secret 2> /dev/null | grep config.js > /dev/null 2>&1)) \
+#   && [[ (! -f "$credsFile") && -z "$JENKINS_HOME" ]]; 
+# then
+#   echo "kube-setup-data-ingestion-job line 21"
+#   gen3_log_info "kube-setup-data-ingestion-job" "setting up data-ingestion-job resources"
+#   # gen3 s3 create "$bucketname"
+#   gen3 awsuser create data-ingestion-bot
+#   # gen3 s3 attach-bucket-policy "$bucketname" --read-write --user-name data-ingestion-bot
+#   gen3_log_info "initializing data-ingestion-job config.json"
+#   user=$(gen3 secrets decode data-ingestion-job-secret awsusercreds.json)
+#   key_id=$(jq -r .id <<< $user)
+#   access_key=$(jq -r .secret <<< $user)
+#   echo "kube-setup-data-ingestion-job line 30"
+#   echo "$key_id"
+#   cat - > "$credsFile" <<EOM
+# {
+#   "aws_access_key_id": "$key_id",
+#   "aws_secret_access_key": "$access_key"
+# }
 # EOM
+#   gen3 secrets sync "initialize data-ingestion-job/config.json"
+# fi
+
+# echo "kube-setup-data-ingestion-job line 38"
+# # gen3 roll data-ingestion-job
+# # g3kubectl apply -f "${GEN3_HOME}/kube/services/data-ingestion-job/data-ingestion-job-service.yaml"
+# gen3 runjob data-ingestion-job
+
+# # cat <<EOM
+# # The data ingestion job has been deployed onto the k8s cluster.
+# # EOM
