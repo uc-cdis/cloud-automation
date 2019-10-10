a65b2b7b6e34cb573ef6f2a45346ca83793efdfa
Zakir Gowani <zakirgowani@gmail.com>
2019-10-10 15:12:44 -0500

    feat: data ingestion pipeline job

--- gen3/bin/kube-setup-data-ingestion-job.sh
+++ gen3/bin/kube-setup-data-ingestion-job.sh
@@ -19,10 +19,9 @@ gen3_load "gen3/gen3setup"
 
 gen3 kube-setup-secrets
 
-PHS_ID_LIST_PATH=apis_configs/data-ingestion-job-phs-id-list.txt
+PHS_ID_LIST_PATH=devplanetv1/apis_configs/data-ingestion-job-phs-id-list.txt
 if [ $# -eq 1 ]
   then PHS_ID_LIST_PATH=$1
-    
 fi
 
 g3kubectl create configmap phs-id-list --from-file=$PHS_ID_LIST_PATH
