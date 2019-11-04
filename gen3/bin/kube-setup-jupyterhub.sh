e95990ca60e2de61df2ca703914c38eaf0699056
Reuben Pasquini <reuben@frickjack.com>
2019-11-04 14:56:40 -0600

    fix(qa-cronjob): remove setup-jupyter

--- gen3/bin/kube-setup-jupyterhub.sh
+++ gen3/bin/kube-setup-jupyterhub.sh
@@ -10,18 +10,22 @@ gen3_load "gen3/gen3setup"
 # Jenkins friendly
 export WORKSPACE="${WORKSPACE:-$HOME}"
 
-namespace="$(gen3 db namespace)"
-notebookNamespace="$(gen3 jupyter j-namespace)"
+if g3k_manifest_lookup .versions.jupyterhub > /dev/null 2>&1; then
+  namespace="$(gen3 db namespace)"
+  notebookNamespace="$(gen3 jupyter j-namespace)"
 
-gen3 jupyter j-namespace setup
+  gen3 jupyter j-namespace setup
 
-g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/serviceaccount.yaml"
-g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/role-jupyter.yaml"
-g3k_kv_filter ${GEN3_HOME}/kube/services/jupyterhub/rolebinding-jupyter.yaml JUPYTER_BINDING "name: jupyter-binding-$namespace" CURRENT_NAMESPACE "namespace: $namespace" NOTEBOOK_NAMESPACE "namespace: $notebookNamespace" | g3kubectl apply -f -
+  g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/serviceaccount.yaml"
+  g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/role-jupyter.yaml"
+  g3k_kv_filter ${GEN3_HOME}/kube/services/jupyterhub/rolebinding-jupyter.yaml JUPYTER_BINDING "name: jupyter-binding-$namespace" CURRENT_NAMESPACE "namespace: $namespace" NOTEBOOK_NAMESPACE "namespace: $notebookNamespace" | g3kubectl apply -f -
 
-g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-service.yaml"
-g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-storage.yaml"
+  g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-service.yaml"
+  g3kubectl apply -f "${GEN3_HOME}/kube/services/jupyterhub/jupyterhub-storage.yaml"
 
-# disable network policy for now
-#gen3 kube-setup-networkpolicy jupyter
-gen3 jupyter upgrade
+  # disable network policy for now
+  #gen3 kube-setup-networkpolicy jupyter
+  gen3 jupyter upgrade
+else
+  gen3_log_info "jupyterhub not enabled in the manifest"
+fi
