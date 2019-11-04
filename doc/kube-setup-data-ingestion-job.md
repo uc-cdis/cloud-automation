b8d10c09c0b50fc934c2a7a76e0c58488b26e69d
Zakir Gowani <zakirgowani@gmail.com>
2019-11-04 11:15:52 -0600

    giving user option to provide genome_file_manifest

--- doc/kube-setup-data-ingestion-job.md
+++ doc/kube-setup-data-ingestion-job.md
@@ -1,11 +1,12 @@
 The data-ingestion-job is specific to DataSTAGE.
 
 Usage:
-`gen3 kube-setup-data-ingestion-job [<phs_id_list_filepath>] [<data_requiring_manual_review_filepath>] CREATE_GOOGLE_GROUPS <bool>`
+`gen3 kube-setup-data-ingestion-job [<phs_id_list_filepath>] [<data_requiring_manual_review_filepath>] [<genome_file_manifest_path>]
+CREATE_GOOGLE_GROUPS <bool>`
 
-These arguments are optional with default filepaths, and only the phs id file needs to actually exist. 
+These arguments are optional with default filepaths, and only the phs id file needs to actually exist.
 It is by default expected to live here:
-`g3auto/data-ingestion-job/data-ingestion-job-phs-id-list.txt`
+`g3auto/data-ingestion-job/phsids.txt`
 
 This job also requires a config file with creds to be filled out in advance, here:
 `g3auto/data-ingestion-job/data_ingestion_job_config.json`
--- gen3/bin/kube-setup-data-ingestion-job.sh
+++ gen3/bin/kube-setup-data-ingestion-job.sh
@@ -7,7 +7,7 @@
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 
-gen3 kube-setup-secrets
+gen3 kube-setup-secrets > /dev/null
 
 mkdir -p $(gen3_secrets_folder)/g3auto/data-ingestion-job
 credsFile="$(gen3_secrets_folder)/g3auto/data-ingestion-job/data_ingestion_job_config.json"
@@ -15,8 +15,6 @@ credsFile="$(gen3_secrets_folder)/g3auto/data-ingestion-job/data_ingestion_job_c
 if (! (g3kubectl describe secret data-ingestion-job-secret 2> /dev/null | grep config.js > /dev/null 2>&1)) \
   && [[ (! -f "$credsFile") && -z "$JENKINS_HOME" ]]; 
 then
-  gen3_log_info "kube-setup-data-ingestion-job" "setting up data-ingestion-job resources"
-  gen3_log_info "initializing data-ingestion-job config.json"
   cat - > "$credsFile" <<EOM
 {
   "gs_creds": {
@@ -43,23 +41,14 @@ then
   "git_repo_to_pr_to": ""
 }
 EOM
-  gen3 secrets sync "initialize data-ingestion-job/data_ingestion_job_config.json"
+  gen3 secrets sync "initialize data-ingestion-job/data_ingestion_job_config.json" > /dev/null
 fi
 
+# Prep inputs to job
 
-PHS_ID_LIST_PATH=$(gen3_secrets_folder)/g3auto/data-ingestion-job/data-ingestion-job-phs-id-list.txt
-if [ $# -ge 1 ] && [ -f "$1" ]
-  then PHS_ID_LIST_PATH=$1
-fi
-if [ ! -f $PHS_ID_LIST_PATH ] 
-  then echo "A file containing a list of study accessions was not found at $PHS_ID_LIST_PATH. Please provide one! Exiting."
-  exit
-fi
-
+PHS_ID_LIST_PATH=$(gen3_secrets_folder)/g3auto/data-ingestion-job/phsids.txt
 DATA_REQUIRING_MANUAL_REVIEW_PATH=$(gen3_secrets_folder)/g3auto/data-ingestion-job/data_requiring_manual_review.tsv
-if [ $# -ge 2 ] && [ -f "$2" ]
-  then DATA_REQUIRING_MANUAL_REVIEW_PATH=$2
-fi
+GENOME_FILE_MANIFEST_PATH=$(gen3_secrets_folder)/g3auto/data-ingestion-job/genome_file_manifest.csv
 
 argc=$#
 argv=("$@")
@@ -67,17 +56,25 @@ for (( j=0; j < argc - 1; j++ )); do
   if [ "${argv[j]}" == "CREATE_GOOGLE_GROUPS" ]; then
     CREATE_GOOGLE_GROUPS="${argv[j+1]}"
   fi
-  if [ "${argv[j]}" == "CREATE_GENOME_MANIFEST" ]; then
-    CREATE_GENOME_MANIFEST="${argv[j+1]}"
-  fi
 done
 
-g3kubectl delete configmap phs-id-list
-g3kubectl delete configmap data-requiring-manual-review
+g3kubectl delete configmap phs-id-list > /dev/null
+g3kubectl delete configmap data-requiring-manual-review > /dev/null
 
+if [ ! -f $PHS_ID_LIST_PATH ]; then
+  echo "A file containing a list of study accessions was not found at $PHS_ID_LIST_PATH. Please provide one! Exiting."
+  exit
+fi
 g3kubectl create configmap phs-id-list --from-file=$PHS_ID_LIST_PATH
+
 if [ -f "$DATA_REQUIRING_MANUAL_REVIEW_PATH" ]; then
+  echo "Found a data_requiring_manual_review file at $DATA_REQUIRING_MANUAL_REVIEW_PATH; will incorporate these PHS IDs in extract creation."
   g3kubectl create configmap data-requiring-manual-review --from-file=$DATA_REQUIRING_MANUAL_REVIEW_PATH
 fi
 
-gen3 runjob data-ingestion CREATE_GOOGLE_GROUPS $CREATE_GOOGLE_GROUPS CREATE_GENOME_MANIFEST $CREATE_GENOME_MANIFEST
+if [ -f "$GENOME_FILE_MANIFEST_PATH" ]; then
+  echo "Found a genome file manifest at $GENOME_FILE_MANIFEST_PATH; will use this file to skip manifest creation step."
+  g3kubectl create configmap genome-file-manifest --from-file=$GENOME_FILE_MANIFEST_PATH
+fi
+
+gen3 runjob data-ingestion CREATE_GOOGLE_GROUPS $CREATE_GOOGLE_GROUPS
