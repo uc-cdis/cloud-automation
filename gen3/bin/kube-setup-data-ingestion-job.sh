a08f4b797c1d9bef8578a8143736ff90e83fcc5d
Zakir Gowani <zakirgowani@gmail.com>
2019-11-04 15:08:46 -0600

    fix typo

--- gen3/bin/kube-setup-data-ingestion-job.sh
+++ gen3/bin/kube-setup-data-ingestion-job.sh
@@ -9,7 +9,7 @@ gen3_load "gen3/gen3setup"
 
 gen3 kube-setup-secrets > /dev/null
 
-mkdir -p $(gen3_secrets_folder)/g3auto/data-ingestion-job
+mkdir -p "$(gen3_secrets_folder)/g3auto/data-ingestion-job"
 credsFile="$(gen3_secrets_folder)/g3auto/data-ingestion-job/data_ingestion_job_config.json"
 
 if (! (g3kubectl describe secret data-ingestion-job-secret 2> /dev/null | grep config.js > /dev/null 2>&1)) \
@@ -65,15 +65,15 @@ done
 g3kubectl delete configmap phs-id-list > /dev/null
 g3kubectl delete configmap data-requiring-manual-review > /dev/null
 
-if [ ! -f $PHS_ID_LIST_PATH ]; then
+if [ ! -f "$PHS_ID_LIST_PATH" ]; then
   echo "A file containing a list of study accessions was not found at $PHS_ID_LIST_PATH. Please provide one! Exiting."
   exit
 fi
-g3kubectl create configmap phs-id-list --from-file=$PHS_ID_LIST_PATH
+g3kubectl create configmap phs-id-list --from-file="$PHS_ID_LIST_PATH"
 
 if [ -f "$DATA_REQUIRING_MANUAL_REVIEW_PATH" ]; then
   echo "Found a data_requiring_manual_review file at $DATA_REQUIRING_MANUAL_REVIEW_PATH; will incorporate these PHS IDs in extract creation."
-  g3kubectl create configmap data-requiring-manual-review --from-file=$DATA_REQUIRING_MANUAL_REVIEW_PATH
+  g3kubectl create configmap data-requiring-manual-review --from-file="$DATA_REQUIRING_MANUAL_REVIEW_PATH"
 fi
 
 
@@ -81,8 +81,8 @@ if [ -f "$GENOME_FILE_MANIFEST_PATH" ]; then
   echo "Found a genome file manifest at $GENOME_FILE_MANIFEST_PATH; will use this file to skip manifest creation step."
   hostname="$(g3kubectl get configmap global -o json | jq -r .data.hostname)"
   bucketname="data-ingestion-${hostname//./-}"
-  aws s3 cp $GENOME_FILE_MANIFEST_PATH "s3://$bucketname/genome_file_manifest.csv"
-  GENOME_FILE_MAMIFEST_PATH="s3://$bucketname/genome_file_manifest.csv"
+  aws s3 cp "$GENOME_FILE_MANIFEST_PATH" "s3://$bucketname/genome_file_manifest.csv"
+  GENOME_FILE_MANIFEST_PATH="s3://$bucketname/genome_file_manifest.csv"
 fi
 
 gen3 runjob data-ingestion CREATE_GOOGLE_GROUPS $CREATE_GOOGLE_GROUPS
