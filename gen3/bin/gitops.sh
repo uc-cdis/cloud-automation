5f7d18a3bdd1463606aa8803dfca4ec93a7299d3
Fauzi <fauzigo@gmail.com>
2019-11-11 13:43:34 -0600

    fix(bugs)

--- gen3/bin/gitops.sh
+++ gen3/bin/gitops.sh
@@ -83,11 +83,32 @@ gen3_run_tfplan() {
   local apply
   
 
-  module=$1
-  quiet=$2
-  apply=$3
-  sns_topic="arn:aws:sns:us-east-1:433568766270:planx-csoc-alerts-topic"
-  #sns_topic="arn:aws:sns:us-east-1:433568766270:fauzi-alert-channel"
+  args=("$@")
+  ELEMENTS=${#args[@]}
+  for (( i=0;i<$ELEMENTS;i++)); do 
+    #echo ${args[${i}]} 
+    if [ "${args[${i}]}" == "vpc" ] || [ "${args[${i}]}" == "eks" ] || [ "${args[${i}]}" == "management-logs" ];
+    then
+      module="${args[${i}]}"
+    elif [ "${args[${i}]}" == "apply" ];
+    then
+      apply="${args[${i}]}"
+    elif [ "${args[${i}]}" == "quiet" ];
+    then
+      quiet="${args[${i}]}"
+    fi
+  done
+
+  #exit
+  #module=$1
+  #quiet=$2
+  #apply=$3
+  #sns_topic="arn:aws:sns:us-east-1:433568766270:planx-csoc-alerts-topic"
+  sns_topic="arn:aws:sns:us-east-1:433568766270:fauzi-alert-channel"
+
+  #gen3_log_info ${module}
+  #gen3_log_info ${quiet}
+  #gen3_log_info ${apply}
 
   (
     cd ~/cloud-automation
@@ -116,15 +137,15 @@ gen3_run_tfplan() {
         case "$module" in
           "vpc")
             #message=$(_gen3_run_tfplan_vpc ${apply})
-            message=$(_gen3_run_tfplan_x ${apply} ${module})
+            message=$(_gen3_run_tfplan_x ${module} ${apply})
             ;;
           "eks")
             #message=$(_gen3_run_tfplan_eks ${apply})
-            message=$(_gen3_run_tfplan_x ${apply} ${module})
+            message=$(_gen3_run_tfplan_x ${module} ${apply})
             ;;
           "management-logs")
             #message=$(_gen3_run_tfplan_management-logs ${apply})
-            message=$(_gen3_run_tfplan_x ${apply} ${module})
+            message=$(_gen3_run_tfplan_x ${module} ${apply})
             ;;
         esac
       else
@@ -151,25 +172,25 @@ gen3_run_tfplan() {
 #
 # Apply changes picked up by tfplan
 #
-_gen3_run_tfapply_eks() {
-  gen3_run_tfplan "$@" "quiet" "apply"
-}
+#_gen3_run_tfapply_eks() {
+#  gen3_run_tfplan "$@" "quiet" "apply"
+#}
 
 
 #
 # Apply changes picked up by tfplan
 #
-_gen3_run_tfapply_vpc() {
+#_gen3_run_tfapply_vpc() {
   #echo "$@"
-  gen3_run_tfplan "$@" "quiet" "apply"
-}
+#  gen3_run_tfplan "$@" "quiet" "apply"
+#}
 
 #
 # Apply changes picked up by tfplan
 #
-_gen3_run_tfapply_management-logs() {
-  gen3_run_tfplan "$@" "quiet" "apply"
-}
+#_gen3_run_tfapply_management-logs() {
+#  gen3_run_tfplan "$@" "quiet" "apply"
+#}
 
 #
 # Public function to start tfapply, only for eks, for the VPC it might not be a good idea
@@ -193,10 +214,9 @@ gen3_run_tfapply() {
 }
 
 #
+# Run a terraform plan and output a short version of the plan
+# 
 #
-#
-#
-
 _gen3_run_tfplan_x(){
 
   local plan
@@ -208,13 +228,13 @@ _gen3_run_tfplan_x(){
   local profile
   local vpc_module
 
-  apply=$1
-  module=$2
+  apply=$2
+  module=$1
   profile=$(grep profile ~/.aws/config | awk '{print $2}' | cut -d] -f1 |head -n1)
 
 
-  
-  if [ -n ${module} ] && [ "${module}" != "vpc" ];
+ 
+  if [ -n ${module} ] && [ "${module}" == "vpc" ];
   then
     vpc_module=${vpc_name}
     #gen3_log_info "Entering gen3 workon ${profile} ${vpc_name}"
@@ -229,12 +249,17 @@ _gen3_run_tfplan_x(){
     exit 2
   fi
     
+  #gen3_log_info ${vpc_module}
+  #gen3_log_info "${apply}"
+  #gen3_log_info ${module}
+  #gen3_log_info "${profile}"
   gen3_log_info "Entering gen3 workon ${profile} ${vpc_module}"
   gen3 workon ${profile} ${vpc_module} > /dev/null 2>&1
 
   output="$(gen3 tfplan)"
   plan=$(echo -e "${output}" |grep "Plan")
 
+  gen3_log_info ${plan}
   if [ -n "${plan}" ];
   then
     tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_plan.XXXXXX")
@@ -248,6 +273,7 @@ _gen3_run_tfplan_x(){
       echo "No apply this time" >> ${tempFile}
     fi
   fi
+  gen3_log_info "${tmpFile}"
   echo "${tempFile}"
 }
 
@@ -255,104 +281,104 @@ _gen3_run_tfplan_x(){
 # Function that checks for uncomitted changes to cloud-automation
 # and also if there are unapplied changes to the vpc module
 #
-_gen3_run_tfplan_vpc() {
+#_gen3_run_tfplan_vpc() {
 
-  local plan
-  local slack_hook
-  local tempFile
-  local output
-  local apply
+#  local plan
+#  local slack_hook
+#  local tempFile
+#  local output
+#  local apply
 
-  apply=$1
+#  apply=$1
 
-  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1 |head -n1) ${vpc_name} > /dev/null 2>&1
+#  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1 |head -n1) ${vpc_name} > /dev/null 2>&1
   #plan=$(gen3 tfplan | grep "Plan")
-  output="$(gen3 tfplan)"
-  plan=$(echo -e "${output}" | grep "Plan")
+#  output="$(gen3 tfplan)"
+#  plan=$(echo -e "${output}" | grep "Plan")
 
-  if [ -n "${plan}" ];
-  then
-    tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_plan.XXXXXX")
-    echo "${vpc_name} has unapplied plan:" > ${tempFile}
-    echo -e "${plan}"| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" >> ${tempFile}
-    if [ -n "$apply" -a "$apply" == "apply" ];
-    then
-      echo -e "${output}" >> ${tempFile}
-      gen3 tfapply >> ${tempFile} 2>&1
-    else
-      echo "No apply this time" >> ${tempFile}
-    fi
-  fi
-  echo "${tempFile}"
-}
-
-#
-# Function that checks for uncomitted changes to cloud-automation
-# and also if there are unapplied changes to the eks module
-#
-_gen3_run_tfplan_eks() {
-
-  local plan
-  local slack_hook
-  local tempFile
-  local apply
-  local output
-
-  apply=$1
-
-  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1|head -n1) ${vpc_name}_eks > /dev/null 2>&1
-  output="$(gen3 tfplan)"
-  plan=$(echo -e "${output}" | grep "Plan")
-
-  if [ -n "${plan}" ];
-  then
-    tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_plan.XXXXXX")
-    echo "${vpc_name}_eks has unapplied plan:" > ${tempFile}
-    echo -e "${plan}"| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" >> ${tempFile}
-    if [ -n "$apply" -a "$apply" == "apply" ];
-    then
-      echo -e "${output}" >> ${tempFile}
-      gen3 tfapply >> ${tempFile} 2>&1
-    else
-      echo "No apply this time" >> ${tempFile}
-    fi
-  fi
-  echo "${tempFile}"
-}
+#  if [ -n "${plan}" ];
+#  then
+#    tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_plan.XXXXXX")
+#    echo "${vpc_name} has unapplied plan:" > ${tempFile}
+#    echo -e "${plan}"| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" >> ${tempFile}
+#    if [ -n "$apply" -a "$apply" == "apply" ];
+#    then
+#      echo -e "${output}" >> ${tempFile}
+#      gen3 tfapply >> ${tempFile} 2>&1
+#    else
+#      echo "No apply this time" >> ${tempFile}
+#    fi
+#  fi
+#  echo "${tempFile}"
+#}
 
 #
 # Function that checks for uncomitted changes to cloud-automation
 # and also if there are unapplied changes to the eks module
 #
-_gen3_run_tfplan_management-logs() {
+#_gen3_run_tfplan_eks() {
+#
+#  local plan
+#  local slack_hook
+#  local tempFile
+#  local apply
+#  local output
+#
+#  apply=$1
+#
+#  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1|head -n1) ${vpc_name}_eks > /dev/null 2>&1
+#  output="$(gen3 tfplan)"
+#  plan=$(echo -e "${output}" | grep "Plan")
+#
+#  if [ -n "${plan}" ];
+#  then
+#    tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_plan.XXXXXX")
+#    echo "${vpc_name}_eks has unapplied plan:" > ${tempFile}
+#    echo -e "${plan}"| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" >> ${tempFile}
+#    if [ -n "$apply" -a "$apply" == "apply" ];
+#    then
+#      echo -e "${output}" >> ${tempFile}
+#      gen3 tfapply >> ${tempFile} 2>&1
+#    else
+#      echo "No apply this time" >> ${tempFile}
+#    fi
+#  fi
+#  echo "${tempFile}"
+#}
 
-  local plan
-  local slack_hook
-  local tempFile
-  local apply
-  local output
-
-  apply=$1
-
-  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1|head -n1) $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1|head -n1)_management-logs > /dev/null 2>&1
-  output="$(gen3 tfplan)"
-  plan=$(echo -e "${output}" | grep "Plan")
-
-  if [ -n "${plan}" ];
-  then
-    tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_plan.XXXXXX")
-    echo "${vpc_name}_management-logs has unapplied plan:" > ${tempFile}
-    echo -e "${plan}"| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" >> ${tempFile}
-    if [ -n "$apply" -a "$apply" == "apply" ];
-    then
-      echo -e "${output}" >> ${tempFile}
-      gen3 tfapply >> ${tempFile} 2>&1
-    else
-      echo "No apply this time" >> ${tempFile}
-    fi
-  fi
-  echo "${tempFile}"
-}
+#
+# Function that checks for uncomitted changes to cloud-automation
+# and also if there are unapplied changes to the eks module
+#
+#_gen3_run_tfplan_management-logs() {
+#
+#  local plan
+#  local slack_hook
+#  local tempFile
+#  local apply
+#  local output
+#
+#  apply=$1
+#
+#  gen3 workon $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1|head -n1) $(grep profile ~/.aws/config  |awk '{print $2}'| cut -d] -f1|head -n1)_management-logs > /dev/null 2>&1
+#  output="$(gen3 tfplan)"
+#  plan=$(echo -e "${output}" | grep "Plan")
+#
+#  if [ -n "${plan}" ];
+#  then
+#    tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_plan.XXXXXX")
+#    echo "${vpc_name}_management-logs has unapplied plan:" > ${tempFile}
+#    echo -e "${plan}"| sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" >> ${tempFile}
+#    if [ -n "$apply" -a "$apply" == "apply" ];
+#    then
+#      echo -e "${output}" >> ${tempFile}
+#      gen3 tfapply >> ${tempFile} 2>&1
+#    else
+#      echo "No apply this time" >> ${tempFile}
+#    fi
+#  fi
+#  echo "${tempFile}"
+#}
 
 # command to update dictionary URL and image versions
 #
