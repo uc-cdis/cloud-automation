84b6e3743bc1fdc052c9a74fd22cc6d3f7b45bbf
Reuben Pasquini <reuben@frickjack.com>
2019-12-11 11:25:30 -0600

    Fix/smallstuff (#1044)
    * fix(setup): easier slim commons
    
    make it easier to bring up a commons without indexd or sheepdog
    
    * fix(setup-workvm): install npm b4 using it

--- gen3/bin/kube-setup-sheepdog.sh
+++ gen3/bin/kube-setup-sheepdog.sh
@@ -10,6 +10,16 @@ gen3_load "gen3/gen3setup"
 
 [[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
 
+if [[ ! -f "$(gen3_secrets_folder)/.rendered_gdcapi_db" ]]; then
+    # job runs asynchronously ...
+    gen3 job run gdcdb-create
+    echo "Sleep 10 seconds for gdcdb-create job"
+    sleep 10
+    gen3 job logs gdcb-create || true
+    echo "Leaving the jobs running in the background if not already done"
+    touch "$(gen3_secrets_folder)/.rendered_gdcapi_db"
+fi
+
 # deploy sheepdog 
 gen3 roll sheepdog
 g3kubectl apply -f "${GEN3_HOME}/kube/services/sheepdog/sheepdog-service.yaml"
