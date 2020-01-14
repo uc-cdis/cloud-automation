e214179d767833ae65dd9a8fa0eee31eaa3bb1aa
Fauzi <fauzigo@gmail.com>
2020-01-14 16:45:47 -0600

    chore(entrypoint): code optimization

--- Docker/squid/entrypoint.sh
+++ Docker/squid/entrypoint.sh
@@ -13,9 +13,9 @@ create_cache_dir() {
 }
 
 apply_backward_compatibility_fixes() {
-    if [[ -f /etc/squid/squid.user.conf ]]; then
-        rm -rf /etc/squid/squid.conf
-        ln -sf /etc/squid/squid.user.conf /etc/squid/squid.conf
+    if [[ -f ${SQUID_SYSCONFIG_DIR}/squid.user.conf ]]; then
+        rm -rf ${SQUID_SYSCONFIG_DIR}/squid.conf
+        ln -sf ${SQUID_SYSCONFIG_DIR}/squid.user.conf ${SQUID_SYSCONFIG_DIR}/squid.conf
     fi
 }
 
@@ -36,10 +36,10 @@ fi
 if [[ -z ${1} ]]; then
     if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
         echo "Initializing cache..."
-        $(which squid) -N -f /etc/squid/squid.conf -z
+        $(which squid) -N -f ${SQUID_SYSCONFIG_DIR}/squid.conf -z
     fi
     echo "Starting squid..."
-    exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+    exec $(which squid) -f ${SQUID_SYSCONFIG_DIR}/squid.conf -NYCd 1 ${EXTRA_ARGS}
 else
     exec "$@"
 fi
