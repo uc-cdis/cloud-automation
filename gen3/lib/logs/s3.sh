6d841b869c9f4a2ffb0bc5b7a9fa59c684d165b0
Reuben Pasquini <reuben@frickjack.com>
2020-01-28 17:56:28 -0600

    chore(gen3 logs s3): process s3 logs

--- gen3/lib/logs/s3.sh
+++ gen3/lib/logs/s3.sh
@@ -8,71 +8,58 @@ source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 
 
-s3LogsReport() {
+#
+# Generate access reports from S3 logs collected
+# over some date range
+#
+# @param start
+# @param end
+# @param prefix... s3 logs prefixes
+#
+gen3_logs_s3() {
   local s3Path
-  local minDate
-  local maxDate
-}
-
-
-bhcReport() {
-  local logFolders
+  local startDate
+  local endDate
+  local prefix
   local inputName
   local outputName
-  
-  # 
-  logFolders=(
-    s3://bhc-bucket-logs # / - for bhc-data
-    s3://bhcprodv2-data-bucket-logs/log/
-    s3://s3logs-s3logs-mjff-databucket-gen3/log/
-  )
-  lsFiles=(
+  local lsFiles=()
 
-  )
+  startDate="$(date -d "$(gen3_logs_get_arg start 'yesterday' "$@")" "+%Y-%m-%d")"
+  endDate="$(date -d "$(gen3_logs_get_arg end 'tomorrow' "$@")" "+%Y-%m-%d")"
+  prefix="$(gen3_logs_get_arg prefix '' "$@")"
+  if [[ -z "$prefix" ]]; then
+    gen3_log_err "must specify logs bucket prefix"
+    return 1
+  fi
+  if [[ ! $prefix =~ ^s3:// ]]; then
+    gen3_log_err "only support s3 logs bucket currently: $prefix"
+    return 1
+  fi
 
-  for inputName in "${logFolders[@]}"; do 
-      echo $inputName; outputName="${inputName#s3://}";
-      outputName="ls.${outputName%%/*}.txt"; 
-      echo $outputName; 
-      aws s3 ls "$inputName" | tee "${outputName}"
-      echo -------;
-      lsFiles+=($outputName) 
+  local lsTemp="$(mktemp "$XDG_RUNTIME_DIR/s3Ls_XXXXXX")"
+  local downloadTemp="$(mktemp "$XDG_RUNTIME_DIR/s3Download_XXXXXX")"
+  local resultTemp="$(mktemp "$XDG_RUNTIME_DIR/s3Result_XXXXXX")"
+
+  gen3_log_info "scanning $prefix"
+  aws s3 ls "${prefix}${startDate}" | awk 'BEGIN { done="false"; }; (done == "false" && $4 ~ /'$endDate'/){ done="true"; }; (done == "false") { print $4 };' | tee "${lsTemp}" 1>&2
+  # download matching files
+  local logPath
+  local logFolder
+  logFolder="$(dirname "${prefix}")"
+  cat "${lsTemp}" | while read -r logPath; do
+    gen3_log_info "collecting $logFolder/$logPath"
+    if aws s3 cp "${logFolder}/${logPath}" "$downloadTemp" 1>&2; then
+      cat "$downloadTemp" >> "$resultTemp"
+    fi
   done
-  for inputName in ls.*.txt; do
-      outputName="${inputName//ls./2019.}"
-      echo "$outputName"
-      cat "$inputName" | awk '{ print $4 }' | grep -E '2019-0[678]-' | tee "$outputName"
-      echo ---------
-  done
-  rm tempfile 2> /dev/null || true
-  for inputName in 2019.*.txt; do
-      bucketName="${inputName#2019.}"
-      bucketName="${bucketName%.txt}"
-      outputName="raw.${bucketName}.txt"
-      prefix="/log"
-      if [[ "$bucketName" == bhc-bucket-logs ]]; then
-        prefix=""
-      fi
-      echo "$outputName gets s3://$bucketName$prefix/"
-      cat "$inputName" | while read -r path; do 
-        fullPath="s3://$bucketName$prefix/$path"
-        echo "Downloading $fullPath"
-        if aws s3 cp "$fullPath" tempfile; then
-          cat tempfile >> "$outputName"
-          rm tempfile
-        else
-          echo "Failed to download $fullPath"
-          rm tempfile 2> /dev/null || true
-        fi
-      done
-      echo ---------
-    done
-    for inputName in raw.*.txt; do
-      bucketName="${inputName#raw.}"
-      bucketName="${bucketName%.txt}"
-      outputName="ips.${bucketName}.txt"
-      echo "Scanning $inputName for $outputName"
-      cat "$inputName" | awk '{ print $5 " " $8 " " $13 }' | grep GET.OBJECT | grep 200 | awk '{ print $1 }' | sort -u | tee "$outputName"
-    done
+  cat "$resultTemp"
+  /bin/rm "$resultTemp" "$downloadTemp" "$lsTemp"
+  return 0
 }
 
+
+# s3://qaplanetv1-data-bucket-logs/log/qaplanetv1-data-bucket2020
+# s3://bhc-bucket-logs s3://bhcprodv2-data-bucket-logs/log/ s3://s3logs-s3logs-mjff-databucket-gen3/log/
+# s3://jenkinsv1-data-bucket-logs/log/jenkinsv1-data-bucket2020
+# $ cat $XDG_RUNTIME_DIR/s3Result_aRSoKE | grep 'username' | grep GET | awk '{ print $9 }' | sort | uniq -c
