edff24fb37edf9404e9cc761c0aa9c74fa835236
Fauzi <fauzigo@gmail.com>
2020-03-10 14:15:49 -0500

    chore(cleanup):

--- tf_files/aws/modules/rds/main.tf
+++ tf_files/aws/modules/rds/main.tf
@@ -99,68 +99,6 @@ resource "aws_iam_role" "rds_backup_role" {
 POLICY
 }
 
-/*
-resource "aws_iam_policy" "backup_bucket_access" {
-  count       = "${var.rds_instance_backup_kms_key == "" ? 1 : 0}"
-  name        = "${var.rds_instance_indentifier}_backup_bucket_access"
-  description = "Allow access to the backup bucket"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement":
-    [
-        {
-        "Effect": "Allow",
-        "Action":
-            [
-                "s3:ListBucket",
-                "s3:GetBucketLocation"
-            ],
-        "Resource": "arn:aws:s3:::${var.rds_instance_backup_bucket_name}"
-        },
-        {
-        "Effect": "Allow",
-        "Action":
-            [
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:ListMultipartUploadParts",
-                "s3:AbortMultipartUpload"
-            ],
-        "Resource": "arn:aws:s3:::${var.rds_instance_backup_bucket_name}/*"
-        }
-    ]
-}
-EOF
-}
-*/
-
-/*
-data "aws_iam_policy_document" "backup_bucket_access" {
-  count       = "${var.rds_instance_backup_kms_key == "" ? 1 : 0}"
-
-  statement {
-    actions = [
-        "s3:ListBucket",
-        "s3:GetBucketLocation"
-      ],
-    resources = ["arn:aws:s3:::${var.rds_instance_backup_bucket_name}"]
-    effect = "Allow"
-  }
-
-  statement {
-    actions = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:ListMultipartUploadParts",
-        "s3:AbortMultipartUpload"
-      ],
-    resources = ["arn:aws:s3:::${var.rds_instance_backup_bucket_name}"]
-    effect = "Allow"
-  }
-}
-*/
-
 
 data "aws_iam_policy_document" "backup_bucket_access_kms" {
   count       = "${var.rds_instance_backup_enabled ? 1 : 0}"
@@ -196,83 +134,14 @@ data "aws_iam_policy_document" "backup_bucket_access_kms" {
   }
 }
 
+
 resource "aws_iam_role_policy" "backup_bucket_access" {
   count  = "${var.rds_instance_backup_enabled ? 1 : 0}"
   name   = "${var.rds_instance_identifier}_backup_bucket_access"
-#  policy = "${var.rds_instance_backup_kms_key == "" ? data.aws_iam_policy_document.backup_bucket_access.json : data.aws_iam_policy_document.backup_bucket_access_kms.json}"
   policy = "${data.aws_iam_policy_document.backup_bucket_access_kms.json}"
   role   = "${aws_iam_role.rds_backup_role.id}"
 }
 
-/*
-resource "aws_iam_policy" "backup_bucket_access_kms" {
-  count       = "${var.rds_instance_backup_kms_key == "" ? 0 : 1}"
-  name        = "${var.rds_instace_indentifier}_backup_bucket_access"
-  description = "Allow access to the backup bucket"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement":
-    [
-        {
-        "Effect": "Allow",
-        "Action":
-            [
-                "kms:DescribeKey",
-                "kms:GenerateDataKey",
-                "kms:Encrypt",
-                "kms:Decrypt"
-            ],
-        "Resource": "arn:aws:kms:region:${data.aws_caller_identity.current.account_id}:key/${var.rds_instance_backup_kms_key}"
-        },
-        {
-        "Effect": "Allow",
-        "Action":
-            [
-                "s3:ListBucket",
-                "s3:GetBucketLocation"
-            ],
-        "Resource": "arn:aws:s3:::${var.rds_instance_backup_bucket_name}"
-        },
-        {
-        "Effect": "Allow",
-        "Action":
-            [
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:ListMultipartUploadParts",
-                "s3:AbortMultipartUpload"
-            ],
-        "Resource": "arn:aws:s3:::${var.rds_instance_backup_bucket_name}/*"
-        }
-    ]
-}
-EOF
-}
-*/
-
-/*
-resouce "aws_iam_policy" "backup_bucket_access" {
-  count  = "${var.rds_instance_backup_bucket_name == "" ? 0 : 1}"
-  name   = "${var.rds_instance_identifier}"
-}
-*/
-
-/*
-resource "aws_iam_role_policy_attachment" "rds_access_backup_bucket" {
-  policy_arn = ""
-  role       = "${aws_iam_role.eks_node_role.name}"
-}
-*/
-
-/*
-resource "aws_db_instance_role_association" "backup_association" {
-  count                  = "${var.rds_instance_create ? 1 : 0}"
-  db_instance_identifier = "${local.is_mssql ? aws_db_instance.this_mssql.id : aws_db_instance.this.id}"
-  feature_name           = "S3_INTEGRATION"
-  role_arn               = "${aws_iam_role.rds_backup_role.id}"
-}
-*/
 
 resource "aws_db_instance" "this" {
   count = "${var.rds_instance_create && false == local.is_mssql ? 1 : 0}"
@@ -301,14 +170,14 @@ resource "aws_db_instance" "this" {
   vpc_security_group_ids                = "${var.rds_instance_vpc_security_group_ids}"
   db_subnet_group_name                  = "${var.rds_instance_db_subnet_group_name}"
   parameter_group_name                  = "${var.rds_instance_parameter_group_name}"
-  option_group_name                     = "${var.rds_instance_option_group_name}"
+  #option_group_name                     = "${var.rds_instance_option_group_name}"
+  option_group_name                     = "${var.rds_instance_option_group_name == "" && var.rds_instance_backup_enabled ? aws_db_option_group.rds_instance_new_option_group.name : var.rds_instance_option_group_name}"
 
   availability_zone                     = "${var.rds_instance_availability_zone}"
   multi_az                              = "${var.rds_instance_multi_az}"
   iops                                  = "${var.rds_instance_iops}"
   publicly_accessible                   = "${var.rds_instance_publicly_accessible}"
   monitoring_interval                   = "${var.rds_instance_monitoring_interval}"
-  #monitoring_role_arn                   = "${coalesce(var.rds_instance_monitoring_role_arn, aws_iam_role.enhanced_monitoring.*.arn, "")}"
   monitoring_role_arn                   = "${coalesce(var.rds_instance_monitoring_role_arn, join("",aws_iam_role.enhanced_monitoring.*.arn))}"
 
   allow_major_version_upgrade           = "${var.rds_instance_allow_major_version_upgrade}"
@@ -368,7 +237,7 @@ resource "aws_db_instance" "this_mssql" {
   vpc_security_group_ids                = "${var.rds_instance_vpc_security_group_ids}"
   db_subnet_group_name                  = "${var.rds_instance_db_subnet_group_name}"
   parameter_group_name                  = "${var.rds_instance_parameter_group_name}"
-  option_group_name                     = "${var.rds_instance_option_group_name}"
+  #option_group_name                     = "${var.rds_instance_option_group_name}"
   option_group_name                     = "${var.rds_instance_option_group_name == "" && var.rds_instance_backup_enabled ? aws_db_option_group.rds_instance_new_option_group.name : var.rds_instance_option_group_name}"
 
   availability_zone                     = "${var.rds_instance_availability_zone}"
@@ -376,7 +245,6 @@ resource "aws_db_instance" "this_mssql" {
   iops                                  = "${var.rds_instance_iops}"
   publicly_accessible                   = "${var.rds_instance_publicly_accessible}"
   monitoring_interval                   = "${var.rds_instance_monitoring_interval}"
-  #monitoring_role_arn                   = "${coalesce(var.rds_instance_monitoring_role_arn, length(aws_iam_role.enhanced_monitoring.*.arn) > 0 ? aws_iam_role.enhanced_monitoring.arn : "", "")}"
   monitoring_role_arn                   = "${coalesce(var.rds_instance_monitoring_role_arn, join("",aws_iam_role.enhanced_monitoring.*.arn))}"
 
   allow_major_version_upgrade           = "${var.rds_instance_allow_major_version_upgrade}"
