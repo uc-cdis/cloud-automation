54a31614a1aa17955563d2b244871058bdc8b345
Fauzi Gomez <fauzigo@gmail.com>
2020-03-12 15:39:01 -0500

    Feat/squid instance switch (#1125)
    * chore(first): commit including the script to be tested
    
    * chore(bash): bash option
    
    * chore(bash): optinal to run the swap using bash

---
+++ gen3/bin/squid.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# In order to facilitate the swithing of the active squid instance
+# we came up with a script that would force it for us.
+#
+# This would be helpful for those environments where downtime should
+# be close to none when migrating to the HA model. Also helpful when
+# new feature/updates/critical-changes are intruduced to the HA-squid
+# module, you could apply the changes and then switch the active proxy
+# after the new one comes up.
+
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/lib/kube-setup-init"
+
+SCRIPT=$(basename ${BASH_SOURCE[0]})
+ACOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+
+##
+# function to call the proxy switch script
+#
+##
+
+function gen3_gitops_swap_proxy() {
+
+  local squidLibDir="${GEN3_HOME}/gen3/lib/squid/"
+  
+  gen3_log_info "Executing Proxy swap at ${squidLibDir}proxy_switch.py"
+  if [ ${1} == "bash" ];
+  then
+    bash ${squidLibDir}proxy_switch.sh
+  else
+    bash ${squidLibDir}proxy_switch.py
+  fi
+
+  if [ $? -gt 0 ]; 
+  then
+    gen3_log_error "There has been an error during the swap, please review the output"
+  else
+    gen3_log_info "all good"
+  fi
+}
+
+
+help() {
+  gen3 help squid
+}
+
+if [[ -z "$GEN3_SOURCE_ONLY" ]]; then
+  # Support sourcing this file for test suite
+  command="$1"
+  shift
+  case "$command" in
+    "swap")
+      gen3_gitops_swap_proxy "$@"
+      ;;
+    *)
+      help
+      ;;
+  esac
+fi
