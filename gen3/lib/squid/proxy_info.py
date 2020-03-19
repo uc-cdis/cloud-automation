342cd25f6882a1a5aefbb8a529e75b96d6d3f5db
Fauzi <fauzigo@gmail.com>
2020-03-19 11:38:11 -0500

    chore(report): information about the proxies

--- gen3/lib/squid/proxy_info.py
+++ gen3/lib/squid/proxy_info.py
@@ -8,6 +8,7 @@ import socket
 
 
 
+
 outcome = {}
 
 """
@@ -28,9 +29,6 @@ def get_asg(name=None):
     return ASG
 
 
-
-
-
 """
   function to get a list of instances with a "Healthy" status according to an autoscaling group
 
@@ -48,141 +46,6 @@ def get_healthy_instances_id(autoscaling_group):
 
 
 """
-  function to get a vpc base upon its name 
-  
-  @var string name name of the vpc you want
-  
-  @return dictionary with the vpc description
-"""
-def get_vpc(name):
-    client = boto3.client('ec2')
-    vpcs   = client.describe_vpcs(Filters=[{'Name':'tag:Name','Values':[name]}])
-    del client
-    for vpc in vpcs['Vpcs']:
-        return vpc
-
-"""
-  function that searches for a default gateway in a defined routing table
-
-  @var string rtid the id of the routing table to be queried
-
-  @return Dictionary with a boto response of the request
-"""
-def exist_default_gw(rtid):
-    client = boto3.client('ec2')
-    response = client.describe_route_tables(Filters=[{'Name': 'route.destination-cidr-block','Values':['0.0.0.0/0']}],RouteTableIds=[rtid])
-    del client
-    return response
-
-
-"""
-  function to get route tables information
-
-  @var String vpc_id id of the VPC where the route belongs to
-       String name name of the route in question
-
-  @return Dict containing the information of the table
-"""
-def get_route_table(vpc_id,name):
-    client = boto3.client('ec2')
-    route_table = client.describe_route_tables(Filters=[{'Name':'tag:Name','Values': [name]},{'Name':'vpc-id','Values':[vpc_id]}])
-    del client
-    return route_table
-
-
-
-"""
-  function to get the routing table id of a routing table
-
-  @var Dict route_table expexted the value of the function get_route_table
-
-  @return String the routing table id
-"""
-def get_route_table_id(route_table):
-    for table in route_table['RouteTables']:
-        for association in table['Associations']:
-            return association['RouteTableId']
-
-
-"""
-  function to delete a default gateway of a routing table
-
-  @var String rtid routing table id where you want the default GW deleted
-
-  @return Int with the response of the attemp. HTTP code
-"""
-def del_default_gw(rtid):
-    client = boto3.client('ec2')
-    response = client.delete_route(DestinationCidrBlock='0.0.0.0/0',RouteTableId=rtid)
-    del client
-    return response
-
-"""
-  function to set a default route to a routing table
-
-  @var String eni eni id that will be handing the traffic
-       String rtid routing table id you want to set the default GW
-
-  @return Int with the response of the attemp. HTTP code
-"""
-def set_default_gw(eni,rtid):
-    client = boto3.client('ec2')
-    #print(eni)
-
-    response = exist_default_gw(rtid)
-    if len(response['RouteTables']) > 0:
-        response = del_default_gw(rtid)
-
-    if response['ResponseMetadata'] and response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        response = client.create_route(DestinationCidrBlock='0.0.0.0/0',NetworkInterfaceId=eni,RouteTableId=rtid)
-        if response['Return'] and response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            outcome[rtid] = "Route %s default GW successfully changed to %s" % (rtid,eni)
-            #print("Route %s changed successfully" % rtid)
-        else:
-            outcome[eni] = response
-            #print(response)
-    else:
-        outcome[eni] = response
-        #print(response)
-
-    del client
-    return response
-
-
-"""
-  function to set a recordset in a hosted zone
-
-  @var String zone_id the zone you want the recordset created
-       String name of the record you want created
-       String action ["CREATE", "UPSERT", "DELETE"]
-       String type type of the record ["A", "AAAA", "CNAME", ...]
-       Int ttl time to live for the record
-       String value where the record will point to
-
-  @return nothing
-"""
-def change_resource_record_sets(zone_id,name,action,type,ttl,value):
-    try:
-        client = boto3.client('route53')
-        response = client.change_resource_record_sets(
-        HostedZoneId=zone_id,
-        ChangeBatch= {
-                        'Comment': '%s %s record' % (action, value),
-                        'Changes': [
-                            {
-                             'Action': action,
-                             'ResourceRecordSet': {
-                                 'Name': name,
-                                 'Type': type,
-                                 'TTL': ttl,
-                                 'ResourceRecords': [{'Value': value}]
-                            }
-                        }]
-        })
-    except Exception as e:
-        print(e)
-
-"""
   function to find the instance attribute sourceDestinationCheck for a single instance
            this must be set to false if we want our instance to serve as proxy and be 
            the default gateway for a specific route table
@@ -198,19 +61,6 @@ def get_sourceDestinationCheck_attr(instance_id):
     return response['SourceDestCheck']['Value']
 
 
-"""
-  function to set the attribute sourceDestinationCheck to false for a single instance
-
-
-  @var string instance_id  the id of the instance we want to know the value set to false
-
-  @return dictionary with the execution results 
-"""
-def set_sourceDestinationCheck_attr(instance_id,value=False):
-    client = boto3.client('ec2')
-    response = client.modify_instance_attribute(InstanceId=instance_id, SourceDestCheck={'Value': value})
-    del client
-    return response
 
 """
   function to ge the private ip of instances
@@ -237,7 +87,7 @@ def get_instances_priv_ip(instances_id):
 """
 def get_instances_pub_ip(instances_id):
     reservations = get_instances_info(instances_id)
-    priv_ip = []
+    pub_ip = []
     for reservation in reservations['Reservations']:
         for instance in reservation['Instances']:
             pub_ip.append(instance['PublicIpAddress'])
@@ -292,48 +142,65 @@ def get_instances_eni(instances_id):
 
 
 """
-  function to get a hosted zone in route53 based on the comments
+  function to get a vpc base upon its name 
+  
+  @var string name name of the vpc you want
+  
+  @return dictionary with the vpc description
+"""
+def get_vpc(name):
+    client = boto3.client('ec2')
+    vpcs   = client.describe_vpcs(Filters=[{'Name':'tag:Name','Values':[name]}])
+    del client
+    for vpc in vpcs['Vpcs']:
+        return vpc
 
-  @var String comment what you are looking for
+"""
+  function to get route tables information
 
-  @return Dict with the hosted zone information
-""" 
-def get_hosted_zone(comment): 
-     client = boto3.client('route53')
-     zones = client.list_hosted_zones()
-     del client
-     for zone in zones['HostedZones']:
-         if comment in zone['Config']['Comment']:
-             return zone
+  @var String vpc_id id of the VPC where the route belongs to
+       String name name of the route in question
+
+  @return Dict containing the information of the table
+"""
+def get_route_table(vpc_id,name):
+    client = boto3.client('ec2')
+    route_table = client.describe_route_tables(Filters=[{'Name':'tag:Name','Values': [name]},{'Name':'vpc-id','Values':[vpc_id]}])
+    del client
+    return route_table
+
 
 
 """
-  function to get the recordsets of a hosted zone
+  function to get the routing table id of a routing table
 
-  @var String zone_id id of the zone you want the recordsets
+  @var Dict route_table expexted the value of the function get_route_table
 
-  @return Dict with the recordsets 
+  @return String the routing table id
 """
-def get_record_sets(zone_id):
-     client = boto3.client('route53')
-     record_sets = client.list_resource_record_sets(HostedZoneId=zone_id)
-     del client
-     return record_sets
+def get_route_table_id(route_table):
+    for table in route_table['RouteTables']:
+        for association in table['Associations']:
+            return association['RouteTableId']
 
 
 """
-  function to determine if a recordset exist in a hosted zone
+  function that searches for a default gateway in a defined routing table
 
-  @var list record_sets expected the value of the function get_record_sets
-       String name name of the record set you are looking for
+  @var string rtid the id of the routing table to be queried
 
-  @return Boool if it exist or not
+  @return Dictionary with a boto response of the request
 """
-def exist_record_set(record_sets, name):
-    for record_set in record_sets['ResourceRecordSets']:
-        if name in record_set['Name']:
-            return True
-    return False
+def exist_default_gw(rtid):
+    client = boto3.client('ec2')
+    response = client.describe_route_tables(Filters=[{'Name': 'route.destination-cidr-block','Values':['0.0.0.0/0']}],RouteTableIds=[rtid])
+    del client
+    return response
+
+
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
 
 
 def main():
@@ -369,76 +236,29 @@ def main():
     available_proxies = get_healthy_instances_id(get_asg("squid-auto-%s" % os.environ.get('vpc_name')))
 
     
-    print("Current squid instance id: %s" % current_gw_instance_id)
-
     for instance_id in available_proxies:
-        if instance_id != current_gw_instance_id:
-            instance_priv_ip = get_instances_priv_ip([instance_id])
-            if get_sourceDestinationCheck_attr(instance_id):
-                set_sourceDestinationCheck_attr(instance_id)
-                outcome['sourceDestinationCheck'] = "sourceDestinationCheck set to false for %s" % instance_id
-            
-            print("Squid instance id to take place: %s" % instance_id)
-            print("Squid instance IP to take place: %s" %instance_priv_ip[0])
-            if check_port(instance_priv_ip[0],proxy_port) == 0:
-                healthy_instance_eni = get_instances_eni([instance_id])
-                healthy_instance_priv_ip = get_instances_priv_ip([instance_id])
+        instance = {}
+        instance["priv_ip"] = get_instances_priv_ip([instance_id])[0]
+        instance["pub_ip"] = get_instances_pub_ip([instance_id])[0]
 
-                #vpc_id = get_instance_vpc_id(get_instances_info([instance_id]))
+        #print(instance["priv_ip"])
+        if check_port(instance["priv_ip"],proxy_port) == 0:
+            instance["port_3128"] = "Open"
+        else:
+            instance["port_3128"] = "Closed"
 
-                print(vpc_id)
-                try:
-                    set_default_gw(healthy_instance_eni[0],eks_private_route_table_id)
-                    outcome['eks_private'] = 'succefully changed the default route for eks_private routing table'
-                    print('succefully changed the default route for eks_private routing table to: %s' % healthy_instance_eni[0])
-                except Exception as e:
-                    statusCode = statusCode + 1
-                    outcome['eks_private'] = e
-                    print(e)
+        instance["eni_id"] = get_instances_eni([instance_id])[0]
 
-                try:
-                    set_default_gw(healthy_instance_eni[0],private_kube_route_table_id)
-                    outcome['private_kube'] = 'succefully changed the default route for private_kube routing table'
-                    print('succefully changed the default route for private_kube routing table to: %s' % healthy_instance_eni[0])
-                except Exception as e:
-                    statusCode = statusCode + 1
-                    outcome['private_kube'] = e
-                    print(e)
+        if instance_id == current_gw_instance_id:
+            instance["active"] = True
+        else:
+            instance["active"] = False
 
-                zone = get_hosted_zone(os.environ['vpc_name'])
-                zone_id = zone['Id']
-                print(zone_id)
-                record_sets = get_record_sets(zone_id)
+        instances_info[instance_id] = instance
 
-                if exist_record_set(record_sets,'cloud-proxy'):
-                    try:
-                        change_resource_record_sets(zone_id,'cloud-proxy.internal.io','UPSERT','A',300,instance_priv_ip[0])
-                        outcome['cloud-proxy'] = "subdomain cloud-proxy.internal.io changed for %s with ip: %s" % (zone_id,instance_priv_ip[0])
-                    except Exception as e:
-                        statusCode = statusCode + 1
-                        outcome['cloud-proxy'] = e
-
-                    #outcome['cloud-proxy'] = "subdomain cloud-proxy.internal.io changed for %s" % zone_id
-                    print("subdomain cloud-proxy.internal.io changed for %s" % zone_id)
-                else:
-                    try:
-                        change_resource_record_sets(zone_id,'cloud-proxy.internal.io','CREATE','A',300,instance_priv_ip[0])
-                        outcome['cloud-proxy'] = "subdomain cloud-proxy.internal.io created for %s with ip: %s" % (zone_id,instance_priv_ip[0])
-                    except Exception as e:
-                        statusCode = statusCode + 1
-                        outcome['cloud-proxy'] = e
-                    print("subdomain cloud-proxy.internal.io created for %s" % zone_id)
-
-                if statusCode != 200:
-                    outcome['message'] = 'Proxy switch partially successfull'
-                else:
-                    outcome['message'] = 'Proxy switch successfull'
-            else:
-                print("Not happening")
-            break
-
-    print(outcome)
-    return json.dumps(outcome)
+         
+    print(json.dumps(instances_info))
+    return json.dumps(instances_info)
 
            
 
