f8e130c7bb7068226fc5ebefb157bc4fc29b5fb4
Andrew Prokhorenkov <aprokh@uchicago.edu>
2020-04-01 16:22:56 -0500

    feat: ETL job for IDPH data

---
+++ files/scripts/covid19-idph-etl-cronjob.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Runs daily COVID-19 Illinois Department for Public Health
+# Run as cron job in covid19@adminvm user account
+#
+# USER=USER
+# KUBECONFIG=path/to/kubeconfig
+# 0   0   *   *   *    (if [ -f $HOME/cloud-automation/files/scripts/covid19-idph-etl-cronjob.sh ]; then bash $HOME/cloud-automation/files/scripts/covid19-idph-etl-cronjob.sh; else echo "no codiv19-idph-etl-cronjob.sh"; fi) > $HOME/covid19-idph-etl-cronjob.log 2>&1
+
+# setup --------------------
+
+export GEN3_HOME="${GEN3_HOME:-"$HOME/cloud-automation"}"
+
+if ! [[ -d "$GEN3_HOME" ]]; then
+  echo "ERROR: this does not look like a gen3 environment - check $GEN3_HOME and $KUBECONFIG"
+  exit 1
+fi
+
+PATH="${PATH}:/usr/local/bin"
+
+source "${GEN3_HOME}/gen3/gen3setup.sh"
+
+# lib -------------------------
+
+help() {
+  cat - <<EOM
+Use: bash ./covid19-etl-cronjob.sh
+EOM
+}
+
+
+# main -----------------------
+
+if [[ -z "$USER" ]]; then
+  gen3_log_err "\$USER variable required"
+  help
+  exit 1
+fi
+
+accessToken="$(gen3 api access-token $USER)"
+gen3 job run covid19-idph-etl ACCESS_TOKEN "$accessToken"
