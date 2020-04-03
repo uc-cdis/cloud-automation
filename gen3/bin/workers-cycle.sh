d0788b317a7f72b95cbef7056522888f097dac06
Fauzi <fauzigo@gmail.com>
2020-04-03 12:05:49 -0500

    chore(code): optimization

--- gen3/bin/workers-cycle.sh
+++ gen3/bin/workers-cycle.sh
@@ -11,28 +11,40 @@ SCRIPT=$(basename ${BASH_SOURCE[0]})
 
 
 function drain_node(){
-
   local instanceName=${1}
   gen3_log_info "Draining ${instanceName}"
   g3kubectl drain ${instanceName} --ignore-daemonsets --delete-local-data
 }
 
 
-function stop_instance() {
+function remove_node(){
+  local instanceName=${1}
+  gen3_log_info "Moving ${instanceName} off kubernetes"
+  g3kubectl delete nodes ${instanceName}
+}
+
+function terminate_instance() {
 
   # Let's get the instance id
-  local instanceName=${1}
-  local instanceId=$(aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" --filter "Name=private-dns-name,Values=${instanceName}" --output text)
+  #local instanceName=${1}
+  #local instanceId=$(aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" --filter "Name=private-dns-name,Values=${instanceName}" --output text)
+  local instanceId=${1}
 
-  if [ $? == 0 ];
+  if [ $? == 0 ] && [ -n ${instanceId} ];
   then
-    gen3_log_info "Stopping instance ${instanceId}"
-    aws ec2 stop-instances --instance-ids ${instanceId}
+    gen3_log_info "Terminating instance ${instanceId}"
+    aws autoscaling terminate-instance-in-auto-scaling-group --instance-id ${instanceId} --no-should-decrement-desired-capacity
+    #aws ec2 stop-instances --instance-ids ${instanceId}
   else
     gen3_log_err "Couldn't find an instance with name ${instanceName}"
   fi
 }
 
+function get_instance_id_by_name() {
+  local instanceName=${1}
+  local instanceId=$(aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" --filter "Name=private-dns-name,Values=${instanceName}" --output text)
+  echo ${instanceId}
+}
 
 
 function main() {
@@ -42,14 +54,17 @@ function main() {
   if ! [ ${instance} == "ALL" ];
   then
     drain_node ${instance}
-    stop_instance ${instance}
+    terminate_instance $( get_instance_id_by_name ${instance} )
+    remove_node ${instance}
   else
     local nodeList=($(g3kubectl get node -o name | cut -d/ -f2))
-   
+
     for i in ${!nodeList[@]};
     do
       drain_node ${nodeList[$i]}
-      stop_instance ${nodeList[$i]}
+      #terminate_instance ${nodeList[$i]}
+      terminate_instance $( get_instance_id_by_name ${nodeList[$i]} )
+      remove_node ${nodeList[$i]}
       if ! [ $(( i + 1 )) == ${#nodeList[*]} ];
       then
         gen3_log_info "Next run in ${INTERVAL} seconds, @ $(date -d "+${INTERVAL} seconds")"
