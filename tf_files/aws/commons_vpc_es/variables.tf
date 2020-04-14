f03e538bf124ddea2203e83d111fb779ce81b814
Fauzi Gomez <fauzigo@gmail.com>
2020-04-14 09:38:50 -0500

    Chore/vpc es update (#1183)
    * add(variables): for version and linked_Role
    
    * fix(typo}:
    
    * chore(documentation): add new variables
    
    * fix(typo)
    
    * chore(documentation): additions and changes
    
    * chore(consistency): in variables across modules

--- tf_files/aws/commons_vpc_es/variables.tf
+++ tf_files/aws/commons_vpc_es/variables.tf
@@ -28,3 +28,13 @@ variable "organization_name" {
   description = "For tagging purposes"
   default     = "Basic Service"
 }
+
+variable "es_version" {
+  description = "What version to use when deploying ES"
+  default     = "6.8"
+}
+
+variable "es_linked_role" {
+  description = "Whether or no to deploy a linked roll for ES"
+  default     = true
+}
