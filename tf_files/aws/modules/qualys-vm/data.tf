e03216a7b6471040d24cdf1a5829b8a9bf49539d
Fauzi <fauzigo@gmail.com>
2020-04-20 12:39:19 -0500

    chore(update}: variables and data

---
+++ tf_files/aws/modules/qualys-vm/data.tf
@@ -0,0 +1,22 @@
+
+
+data "aws_ami" "qualys_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["${var.image_name_search_criteria}"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+#  filter {
+#    name   = "root-device-type"
+#    values = ["ebs"]
+#  }
+
+  owners = ["${var.ami_account_id}"]
+
+}
