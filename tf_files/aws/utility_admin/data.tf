e9ac0eee66e5e4dc7aade2aa850f91e2f1d4b54b
Fauzi <fauzigo@gmail.com>
2020-04-22 12:53:52 -0500

    fix(variable): bad reference

--- tf_files/aws/utility_admin/data.tf
+++ tf_files/aws/utility_admin/data.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "assume_role_policy_document" {
   statement {
     actions = [ "sts:AssumeRole" ]
     effect    = "Allow"
-    resources = ["arn:aws:iam::${var.account_id}:role/csoc_adminvm"]
+    resources = ["arn:aws:iam::${var.aws_account_id}:role/csoc_adminvm"]
   }
 }
 
