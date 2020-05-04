8d2706e36fbb847f145d7791e54f2d4aad58eb88
Reuben Pasquini <reuben@frickjack.com>
2020-05-04 11:59:05 -0500

    chore(gen3 api): api-key support

--- gen3/bin/api.sh
+++ gen3/bin/api.sh
@@ -8,6 +8,41 @@ gen3_api_help() {
   gen3 help api
 }
 
+
+gen3_access_token_to_cache() {
+  local key="$1"
+  if [[ -z "$key" ]]; then
+    gen3_log_err "invalid token cache save key: $key"
+    return 1
+  fi
+  local gen3CredsCache="${GEN3_CACHE_DIR}/${key}_access_token.json"
+  tee "$gen3CredsCache"
+}
+
+#
+# Return an access token from cache
+#
+gen3_access_token_from_cache() {
+  local key="$1"
+  if [[ -z "$key" ]]; then
+    gen3_log_err "invalid token cache key: $key"
+    return 1
+  fi
+  local gen3CredsCache="${GEN3_CACHE_DIR}/${key}_access_token.json"
+  if [[ -f "$gen3CredsCache" ]]; then
+    local expTime
+    local nowTime
+    nowTime=$(date '+%s')
+    expTime=$(cat "$gen3CredsCache" | awk -F . '{ print $2 }' | base64 --decode 2> /dev/null | jq -r .exp)
+    if [[ $((nowTime + 300)) -lt "$expTime" ]]; then # cache not out of date
+      gen3_log_info "using cached token at $gen3CredsCache"
+      cat $gen3CredsCache
+      return $?
+    fi
+  fi
+  return 1
+}
+
 #
 # Generate a new access token - or pull from cache if available
 #
@@ -18,23 +53,38 @@ gen3_access_token() {
     gen3_api_help
     return 1
   fi
-
-  local gen3CredsCache="${GEN3_CACHE_DIR}/${username}_access_token.json"
-  if [[ -f "$gen3CredsCache" ]]; then
-    local expTime
-    local nowTime
-    nowTime=$(date '+%s')
-    expTime=$(cat "$gen3CredsCache" | awk -F . '{ print $2 }' | base64 --decode 2> /dev/null | jq -r .exp)
-    if [[ $((nowTime + 300)) -lt "$expTime" ]]; then # cache not out of date
-      echo -e "INFO: using cached token at $gen3CredsCache" 1>&2
-      cat $gen3CredsCache
-      return $?
-    fi
+  if [[ -f "$username" ]]; then  # looks like an api-key
+    gen3_access_token_from_apikey "$@"
+    return $?
   fi
-  g3kubectl exec $(gen3 pod fence) -- fence-create token-create --scopes openid,user,fence,data,credentials,google_service_account --type access_token --exp 3600 --username ${username} | tail -1 | tee "$gen3CredsCache"
-  return $?
+
+  gen3_access_token_from_cache "$username" && return 0
+  g3kubectl exec $(gen3 pod fence) -- fence-create token-create --scopes openid,user,fence,data,credentials,google_service_account --type access_token --exp 3600 --username ${username} | tail -1 | gen3_access_token_to_cache "$username"
 }
 
+
+#
+# Try to retrieve an access token given an api key
+#
+gen3_access_token_from_apikey() {
+  if [[ $# -lt 1 || ! -f "$1" ]]; then
+    gen3_log_err "invalid api key: $1"
+    exit 1
+  fi
+  apiKey="$1"
+  shift
+  local cacheKey
+  cacheKey="$(realpath "$apiKey" | md5sum - | awk '{ print $1 }')"
+  gen3_access_token_from_cache "$cacheKey" && return 0
+  local url
+  if ! url="$(jq -r .api_key < "$apiKey" | awk -F . '{ print $2 }' | base64 --decode 2> /dev/null | jq -r .iss)" \
+    || [[ -z "$url" || ! "$url" =~ ^https://.+/user$ ]]; then
+    gen3_log_err "failed to derive valid url from access token - got: $url"
+    return 1
+  fi
+  url="${url}/credentials/cdis/access_token"
+  curl -s -H 'Content-type: application/json' -X POST "$url" "-d@$apiKey" | jq -r .access_token | gen3_access_token_to_cache "$cacheKey"
+}
 
 #
 # Little helper that acquires an access token
@@ -76,16 +126,19 @@ gen3_curl_json() {
     gen3_log_err "unable to acquire token for $userName"
     return 1
   fi
-  hostname="$(g3kubectl get configmap manifest-global -o json | jq -r '.data["hostname"]')"
-  if [[ -z "$hostname" ]]; then
+  if ! hostname="$(awk -F . '{ print $2 }' <<< "$accessToken" | base64 --decode 2> /dev/null | jq -r .iss | awk -F / '{ print $3 }')" \
+    || [[ -z "$hostname" ]]; then
     gen3_log_err "unable to determine hostname for commons API"
     return 1
   fi
+  local url="https://$hostname/$path"
 
   if [[ "$method" == "POST" ]]; then
-    curl -s -X POST "https://$hostname/$path" -H "Authorization: bearer $accessToken" -H "Content-Type: application/json" "-d@$jsonFile"
+    gen3_log_info "posting to $url"
+    curl -s -X POST "$url" -H "Authorization: bearer $accessToken" -H "Content-Type: application/json" -H "Accept: aplication/json" "-d@$jsonFile"
   else
-    curl -s -X GET "https://$hostname/$path" -H "Authorization: bearer $accessToken"
+    gen3_log_info "getting $url"
+    curl -s -X GET "$url" -H "Authorization: bearer $accessToken" -H "Accept: aplication/json"
   fi
   return $?
 }
