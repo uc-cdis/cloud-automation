e6487158318f636d436172d4bb1e0020bbd8f8cf
Fauzi <fauzigo@gmail.com>
2020-05-11 14:29:26 -0500

    chore(secret): for the user pool client

--- tf_files/aws/cognito/main.tf
+++ tf_files/aws/cognito/main.tf
@@ -22,11 +22,13 @@ resource "aws_cognito_identity_provider" "provider" {
 
 
 resource "aws_cognito_user_pool_client" "client" {
-  name                         = "${var.cognito_user_pool_name}"
-  user_pool_id                 = "${aws_cognito_user_pool.pool.id}"
+  name                                 = "${var.cognito_user_pool_name}"
+  user_pool_id                         = "${aws_cognito_user_pool.pool.id}"
 
-  callback_urls                = ["${var.cognito_callback_urls}"]
-  allowed_oauth_flows          = ["${var.cognito_oauth_flows}"]
-  allowed_oauth_scopes         = ["${var.cognito_oauth_scopes}"]
-  supported_identity_providers = ["${aws_cognito_identity_provider.provider.name}"]
+  callback_urls                        = ["${var.cognito_callback_urls}"]
+  allowed_oauth_flows_user_pool_client = true
+  generate_secret                      = true
+  allowed_oauth_flows                  = ["${var.cognito_oauth_flows}"]
+  allowed_oauth_scopes                 = ["${var.cognito_oauth_scopes}"]
+  supported_identity_providers         = ["${aws_cognito_identity_provider.provider.provider_name}"]
 }
