dd7d3c7dcf7a61f38b8cf55c467ffbd73891d1bd
Fauzi <fauzigo@gmail.com>
2020-05-11 11:51:50 -0500

    chore(cognito): first commit

---
+++ tf_files/aws/cognito/variables.tf
@@ -0,0 +1,37 @@
+
+
+variable "cognito_oauth_flows" {
+  description = "Allowed OAuth Flows"
+  type        = "list"
+  default     = ["code", "implicit"]
+}
+
+variable "cognito_user_pool_name" {
+  description = "App client"
+  default     = "fence"
+}
+
+variable "cognito_provider_type" {
+  description = "Provider type"
+  default     = "SAML"
+}
+
+variable "cognito_attribute_mapping" {
+  description = "Federation attribute mapping"
+  type        = "map"
+  default     = {
+    "email" = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"
+  }
+}
+
+variable "cognito_oauth_scopes" {
+  description = "Allowed OAuth Scopes"
+  type        = "list"
+  default     = ["email", "openid"]
+}
+
+variable "cognito_provider_details" {
+  description = "The identity provider details"
+  type        = "map"
+  default     = {}
+}
