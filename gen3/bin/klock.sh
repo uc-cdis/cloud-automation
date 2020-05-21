a3ca83f5e2d621032aa90a016356a91fd8558a83
Reuben Pasquini <reuben@frickjack.com>
2020-05-21 17:16:13 -0500

    fix(klock): k8s label value rules, exit codes

--- gen3/bin/klock.sh
+++ gen3/bin/klock.sh
@@ -8,7 +8,7 @@ if [[ -n "$GEN3_HOME" ]]; then  # load gen3 tools from cloud-automation
   source "${GEN3_HOME}/gen3/lib/utils.sh"
   gen3_load "gen3/gen3setup"
 else
-  echo "GEN3_HOME is not set"
+  gen3_log_err "GEN3_HOME is not set"
   exit 1
 fi
 
@@ -22,31 +22,34 @@ lock() {
     exit 0
   else
     if ! [[ $3 =~ ^[0-9]+$ ]]; then
-      echo "ERROR: max-age is $3, must be an integer"
+      gen3_log_err "max-age is $3, must be an integer"
       exit 1
     fi
     if [[ $4 = '-w' || $4 = '--wait' ]]; then
       wait=true
       if ! [[ $5 =~ ^[0-9]+$ ]]; then
-        echo "ERROR: wait-time is $5, must be an integer"
+        gen3_log_err "wait-time is $5, must be an integer"
         exit 1
       fi
       endWaitTime=$(($(date +%s)+$5))
     fi
     lockName="$1"
     owner="${2:0:45}"
+    # must begin and end with alphanumeric character
+    owner="$(sed 's/[-\._]$/x/' <<< "$owner")"
+    owner="$(sed 's/^[-\._]/x/' <<< "$owner")"
     lockDurationSecs=$3
   fi
 
   # create locks ConfigMap if it does not already exist, and set the lock we are 
   # currently trying to lock to unlocked with no owner
   if ! g3kubectl get configmaps locks > /dev/null 2>&1; then
-    echo "locks configmap not detected, creating one" 1>&2
-    g3kubectl create configmap locks
-    g3kubectl label configmap locks ${lockName}=false ${lockName}_owner=none ${lockName}_exp=0
+    gen3_log_info "locks configmap not detected, creating one"
+    g3kubectl create configmap locks || exit 1
+    g3kubectl label configmap locks ${lockName}=false ${lockName}_owner=none ${lockName}_exp=0 || exit 1
   else 
     if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.${lockName}}") = '' ]]; then
-      g3kubectl label configmap locks ${lockName}=false ${lockName}_owner=none ${lockName}_exp=0
+      g3kubectl label configmap locks ${lockName}=false ${lockName}_owner=none ${lockName}_exp=0 || exit 1
     fi
   fi
 
@@ -55,24 +58,25 @@ lock() {
   if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$lockName}") = "false" 
     || $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.${lockName}_exp}") -lt $(date +%s) ]]; then
     expTime=$(($(date +%s)+$lockDurationSecs))
-    g3kubectl label --overwrite configmap locks ${lockName}=true ${lockName}_owner=$owner ${lockName}_exp=$expTime
+    g3kubectl label --overwrite configmap locks ${lockName}=true ${lockName}_owner=$owner ${lockName}_exp=$expTime || exit 1
     sleep $(shuf -i 1-5 -n 1)
 
     if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$lockName}") = "true" 
       && $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.${lockName}_owner}") = $owner 
       && $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.${lockName}_exp}") -gt $(date +%s) ]]; then 
-      >&2 echo "($(date +%s)) locked $lockName as owner $owner until $expTime"
+      gen3_log_info "($(date +%s)) locked $lockName as owner $owner until $expTime"
       exit 0
     else
       if [[ $wait = true ]]; then
         while [[ $endWaitTime -gt $(date +%s) ]]; do
+          # sleep loop until the lock is released or it expires
           if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.$lockName}") = "true" 
             && $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.${lockName}_exp}") -gt $(date +%s) ]]; then
             sleep $(shuf -i 1-5 -n 1)
           else
             expTime=$(($(date +%s)+$lockDurationSecs))
-            g3kubectl label --overwrite configmap locks ${lockName}=true ${lockName}_owner=$owner ${lockName}_exp=$expTime
-            >&2 echo "($(date +%s)) locked $lockName as owner $owner until $expTime"
+            g3kubectl label --overwrite configmap locks ${lockName}=true ${lockName}_owner=$owner ${lockName}_exp=$expTime || exit 1
+            gen3_log_info "($(date +%s)) locked $lockName as owner $owner until $expTime"
             exit 0
           fi
         done
@@ -89,8 +93,8 @@ lock() {
           sleep $(shuf -i 1-5 -n 1)
         else
           expTime=$(($(date +%s)+$lockDurationSecs))
-          g3kubectl label --overwrite configmap locks ${lockName}=true ${lockName}_owner=$owner ${lockName}_exp=$expTime
-          >&2 echo "($(date +%s)) locked $lockName as owner $owner until $expTime"
+          g3kubectl label --overwrite configmap locks ${lockName}=true ${lockName}_owner=$owner ${lockName}_exp=$expTime || exit 1
+          gen3_log_info "($(date +%s)) locked $lockName as owner $owner until $expTime"
           exit 0
         fi
       done
@@ -108,6 +112,9 @@ unlock() {
   else
     lockName="$1"
     owner="${2:0:45}"
+    # must begin and end with alphanumeric character
+    owner="$(sed 's/[-\._]$/x/' <<< "$owner")"
+    owner="$(sed 's/^[-\._]/x/' <<< "$owner")"
   fi
 
   # create locks ConfigMap if it does not already exist, and set the lock we are 
@@ -121,8 +128,8 @@ unlock() {
       if [[ $(g3kubectl get configmap locks -o jsonpath="{.metadata.labels.${lockName}_owner}") != $owner ]]; then
         exit 1
       else 
-        g3kubectl label --overwrite configmap locks ${lockName}=false ${lockName}_owner=none ${lockName}_exp=0
-        >&2 echo "($(date +%s)) unlocked $lockName as owner $owner"
+        g3kubectl label --overwrite configmap locks ${lockName}=false ${lockName}_owner=none ${lockName}_exp=0 || exit 1
+        gen3_log_info "($(date +%s)) unlocked $lockName as owner $owner"
         exit 0
       fi
     fi
@@ -131,7 +138,7 @@ unlock() {
 
 list() {
   g3kubectl get configmap locks -o json | jq -r .metadata.labels
-  echo 'Note: use "date -d@timestamp" to convert timestamp to date, "date +%s" gives current timestamp' 1>&2
+  gen3_log_info 'Note: use "date -d@timestamp" to convert timestamp to date, "date +%s" gives current timestamp'
 }
 
 
--- gen3/test/klockTest.sh
+++ gen3/test/klockTest.sh
@@ -1,36 +1,37 @@
 test_kube_lock() {
+  local testuser="---a-bad-user-name---____......"
   # Setup - acquire test runner lock - running concurrent tests in the same env won't work
-  if ! gen3 klock lock testrunner testuser 360 -w 360; then
+  if ! gen3 klock lock testrunner "$testuser" 600 -w 600; then
     because $? "Failed to acquire testrunner lock"
     return 1
   fi
 
   gen3 klock lock | grep -e "gen3 klock lock lock-name owner max-age [--wait wait-time]"; because $? "calling klock lock without arguments should show the help documentation"
-  gen3 klock lock testlock testuser not-a-number | grep -e "ERROR: max-age is not-a-number, must be an integer"; because $? "calling klock lock without a number for max-age should show this error message"
-  gen3 klock lock testlock testuser 60 -w not-a-number | grep -e "ERROR: wait-time is not-a-number, must be an integer"; because $? "calling klock lock without a number for wait-time should show this error message"
+  gen3 klock lock testlock "$testuser" not-a-number 2>&1 | grep -e "max-age is not-a-number, must be an integer"; because $? "calling klock lock without a number for max-age should show this error message"
+  gen3 klock lock testlock "$testuser" 60 -w not-a-number 2>&1 | grep -e "wait-time is not-a-number, must be an integer"; because $? "calling klock lock without a number for wait-time should show this error message"
   # Do not do this - it break any klock's active in the test environment! :-(
   #g3kubectl delete configmap locks
-  gen3 klock lock testlock testuser 300; because $? "calling klock lock for the first time for a lock should successfully lock it, and it should create the configmap locks if it does not exist already"
-  ! gen3 klock lock testlock testuser 60; because $? "calling klock lock for the second time in a row for a lock should fail to lock it"
-  gen3 klock lock testlock2 testuser 60; because $? "klock lock should be able to handle multiple locks"
+  gen3 klock lock testlock "$testuser" 300; because $? "calling klock lock for the first time for a lock should successfully lock it, and it should create the configmap locks if it does not exist already"
+  ! gen3 klock lock testlock "$testuser" 60; because $? "calling klock lock for the second time in a row for a lock should fail to lock it"
+  gen3 klock lock testlock2 "$testuser" 60; because $? "klock lock should be able to handle multiple locks"
   gen3 klock lock testlock3 testuser2 60; because $? "klock lock should be able to handle multiple users"
   ! gen3 klock lock testlock testuser2 60; because $? "attempting to lock an already locked lock with a different user should fail"
-  gen3 klock lock testlock4 testuser 10
+  gen3 klock lock testlock4 "$testuser" 10
   sleep 11
-  gen3 klock lock testlock4 testuser 15; because $? "attempting to lock an expired lock should succeed"
-  gen3 klock lock testlock5 testuser 10
-  ! gen3 klock lock testlock5 testuser2 10 -w 2; because $? "wait is too short, so klock lock should fail to acquire lock"
-  gen3 klock lock testlock6 testuser 10
+  gen3 klock lock testlock4 "$testuser" 15; because $? "attempting to lock an expired lock should succeed"
+  gen3 klock lock testlock5 "$testuser" 20; because $? "locking testlock5 should be fine"
+  ! gen3 klock lock testlock5 testuser2 10 -w 2; because $? "wait for testlock5 is too short, so klock lock should fail to acquire lock"
+  gen3 klock lock testlock6 "$testuser" 10
   gen3 klock lock testlock6 testuser2 10 -w 20; because $? "wait is longer than expiry time on the first user, so klock lock should succeed to acquire lock"
 
   # cleanup
   for lock in testlock testlock2 testlock3 testlock4 testlock5 testlock6; do
-    for user in testuser testuser2; do
+    for user in "$testuser" testuser testuser2; do
       gen3 klock unlock $lock $user
     done
   done
 
-  gen3 klock unlock testrunner testuser
+  gen3 klock unlock testrunner "$testuser"; because $? "should release testrunner lock"
 
   # Do not do this - it break any klock's active in the test environment! :-(
   #g3kubectl delete configmap locks
