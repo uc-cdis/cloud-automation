3c9875bb499b1c70ae52433f2ba92a6d7db6ed56
Reuben Pasquini <reuben@frickjack.com>
2020-06-11 12:25:55 -0500

    chore(shutdown namespace): go dark at night

---
+++ gen3/bin/shutdown.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/gen3setup"
+
+
+#
+# Shutdown the current namespace plus anything in the corresponding jupyter namespace
+#
+gen3_shutdown_namespace() {
+  local namespace
+  local hostname
+  local namespaceList
+
+  hostname="$(gen3 api hostname)" || return 1
+  namespaceList=(
+    "$(gen3 db namespace)"
+    "$(gen3 jupyter j-namespace)"
+  )
+
+  if [[ -z "$hostname" || "$hostname" == "qa.planx-pla.net" || "$hostname" == "qaplanetv2.planx-pla.net" ]]; then
+    gen3_log_info "gen3_shutdown_namespace refuses to shutdown qa.planx-pla.net or qaplanetv2.planx-pla.net - Jenkins and other QA services run there"
+    namespaceList=(
+      "$(gen3 jupyter j-namespace)"
+    )
+  fi
+
+  for namespace in "${namespaceList[@]}"; do
+    (
+      export KUBECTL_NAMESPACE="$namespace"
+      g3kubectl delete --all deployments --now
+      # ssjdispatcher leaves jobs laying around when undeployed
+      g3kubectl delete --all "jobs" --now
+      # ssjdispatcher leaves jobs laying around when undeployed
+      g3kubectl delete --all "cronjobs" --now
+      # just delete every damn thing
+      g3kubectl delete --all "pods" --now
+    )
+  done
+}
+
+
+if [[ -z "$1" || "$1" =~ ^-*help$ ]]; then
+  gen3 help shutdown
+  exit 0
+fi
+
+command="$1"
+shift
+
+case "$command" in
+"namespace")
+  gen3_shutdown_namespace "$@"
+  ;;
+*)
+  gen3 help shutdown
+  exit 1
+  ;;
+esac
