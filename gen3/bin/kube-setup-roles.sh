54c4ebdda8ef9c6b97f16bc6216596740e601d72
Reuben Pasquini <reuben@frickjack.com>
2020-06-24 13:56:46 -0500

    chore(dashboard): sa linked role setup

--- gen3/bin/kube-setup-roles.sh
+++ gen3/bin/kube-setup-roles.sh
@@ -26,6 +26,12 @@ if [[ -z "$JENKINS_HOME" ]]; then
   if ! g3kubectl get serviceaccounts/jenkins-service > /dev/null 2>&1; then  
     g3kubectl apply -f "${GEN3_HOME}/kube/services/jenkins/serviceaccount.yaml"
   fi
+  if ! g3kubectl get sa gitops-sa > /dev/null 2>&1; then
+    roleName="$(gen3 api safe-name gitops)"
+    gen3 awsrole create "$roleName" gitops-sa
+    # do this here, since we added the new role to this binding
+    g3kubectl apply -f "${GEN3_HOME}/kube/services/jenkins/rolebinding-devops.yaml"
+  fi
   if ! g3kubectl get rolebindings/devops-binding > /dev/null 2>&1; then
     g3kubectl apply -f "${GEN3_HOME}/kube/services/jenkins/rolebinding-devops.yaml"
   fi
