efcead5d0ddef60caddc1519fc9f047a7fe9cd74
Di Wu <dwu20@hawk.iit.edu>
2020-07-13 00:02:51 -0500

    awsroile

--- gen3/bin/awsrole.sh
+++ gen3/bin/awsrole.sh
@@ -16,6 +16,79 @@ gen3_awsrole_help() {
 }
 
 #
+# Assume-role policy - allows SA's to assume role.
+# NOTE: service-account to role is 1 to 1
+#
+# @param serviceAccount to link to the role
+#
+function gen3_awsrole_ar_policy() {
+  local serviceAccount="$1"
+  shift || return 1
+  local issuer_url
+  local account_id
+  local vpc_name
+  vpc_name="$(gen3 api environment)" || return 1
+  issuer_url="$(aws eks describe-cluster \
+                       --name ${vpc_name} \
+                       --query cluster.identity.oidc.issuer \
+                       --output text)" || return 1
+  issuer_url="${issuer_url#https://}"
+  account_id=$(aws sts get-caller-identity --query Account --output text) || return 1
+
+  local provider_arn="arn:aws:iam::${account_id}:oidc-provider/${issuer_url}"
+
+  cat - <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {"Service": "ec2.amazonaws.com"},
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "${provider_arn}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${issuer_url}:aud": "sts.amazonaws.com",
+          "${issuer_url}:sub": "system:serviceaccount:$(gen3 db namespace):${serviceAccount}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+#
+# Annotate the given service account with the given IAM role
+#
+# @param saName
+# @param roleName
+#
+gen3_awsrole_sa_annotate() {
+  local saName="$1"
+  shift || return 1
+  local roleName="$1"
+  shift || return 1
+  local roleArn
+  local roleInfo
+  roleInfo="$(aws iam get-role --role-name "$roleName")" || return 1
+  roleArn="$(jq -e -r .Role.Arn <<< "$roleInfo")"
+
+  if ! g3kubectl get sa "$saName" > /dev/null; then
+    g3kubectl create sa "$saName" || return 1
+  fi
+
+  g3kubectl annotate --overwrite sa "$saName" "eks.amazonaws.com/role-arn=$roleArn"
+}
+
+#
 # Echos type of entity for given name. If not found, it returns non zero exit code
 #
 _get_entity_type() {
@@ -36,21 +109,30 @@ _get_entity_type() {
 # Util to tfplan creation of role
 #
 # @param rolename
+# @param saName for assume-role policy document
 #
 _tfplan_role() {
-  local rolename=$1
+  local rolename="$1"
+  shift || return 1
+  local saName="$1"
+  shift || return 1
+  local arDoc
+  arDoc="$(gen3_awsrole_ar_policy "$saName")" || return 1
   gen3 workon default "${rolename}_role"
   gen3 cd
   cat << EOF > config.tfvars
 rolename="$rolename"
 description="Role created with gen3 awsrole"
 path="/gen3_service/"
+ar_policy=<<EDOC
+$arDoc
+EDOC
 EOF
   gen3 tfplan 2>&1
 }
 
 #
-# Util for applying tfplan 
+# Util for applying tfplan
 #
 _tfapply_role() {
   local rolename=$1
@@ -65,23 +147,36 @@ _tfapply_role() {
     return 1
   fi
 
-  gen3 trash --apply
+  # leave the terraform artifacts
+  #gen3 trash --apply
 }
 
 #
-# Create aws role 
+# Create aws role - attach "assume-role" policy document that
+# allows the given service account to assume the role.
+# Also attempts to annotate the service account.
 #
 # @param rolename
+# @param serviceAccountName
 #
 gen3_awsrole_create() {
-  local rolename=$1
+  local rolename="$1"
+  if ! shift || [[ -z "$rolename" ]]; then
+    gen3_log_err "use: gen3 awsrole create roleName saName"
+    return 1
+  fi
+  local saName="$1"
+  if ! shift || [[ -z "$saName" ]]; then
+    gen3_log_err "use: gen3 awsrole create roleName saName"
+    return 1
+  fi
   # do simple validation of name
   local regexp="^[a-z][a-z0-9\-]*$"
   if [[ ! $rolename =~ $regexp ]];then
     local errMsg=$(cat << EOF
-ERROR: Username does not meet the following requirements:
+ERROR: name - $rolename - does not meet the following requirements:
   - starts with a-z
-  - contains only a-z, 0-9, and dashes, "-"
+  - contains only a-z, 0-9, _, and dashes, "-"
 EOF
     )
     gen3_log_err $errMsg
@@ -95,7 +190,8 @@ EOF
     # That name is already used.
     if [[ "$entity_type" =~ role ]]; then
       gen3_log_info "A role with that name already exists"
-      return 0
+      gen3_awsrole_sa_annotate "$saName" "$rolename"
+      return $?
     else
       gen3_log_err "A $entity_type with that name already exists"
       return 1
@@ -103,14 +199,14 @@ EOF
   fi
 
   TF_IN_AUTOMATION="true"
-  if ! _tfplan_role $rolename; then
+  if ! _tfplan_role $rolename $saName; then
     return 1
   fi
   if ! _tfapply_role $rolename; then
     return 1
   fi
 
-  return 0
+  gen3_awsrole_sa_annotate "$saName" "$rolename"
 }
 
 #
@@ -125,14 +221,14 @@ gen3_awsrole_info() {
 
 #
 # Attach policy to a role
-# 
+#
 # @param rolename
 # @param policyarn
 #
 gen3_awsrole_attachpolicy() {
   local rolename=$1
   local policyarn=$2
-  
+
   # verify policy and role exist
   if ! gen3_aws_run aws iam get-role --role-name $rolename > /dev/null 2>&1; then
     gen3_log_err "Unable to find role with given name"
@@ -170,148 +266,6 @@ gen3_awsrole_list() {
   gen3_aws_run aws iam list-roles --path-prefix /gen3_service/
 }
 
-##
-# function to create a assume policy document to later attach to a role upon creation
-#
-# no argument is passed to the function, however the following global variables must be set:
-#         vpc_name
-#
-# @return a path to the temporary file where the policy is
-#
-##
-function create_assume_role_policy() {
-  local saname=${1}
-  local account_id=$(gen3_aws_run aws sts get-caller-identity --query Account --output text)
-  local namespace=$(gen3 db namespace)
-  local tempFile=$(mktemp -p "$XDG_RUNTIME_DIR" "tmp_policy.XXXXXX")
-  local issuer_url=$(gen3_aws_run aws eks describe-cluster \
-                       --name ${vpc_name} \
-                       --query cluster.identity.oidc.issuer \
-                       --output text)
-
-  local issuer_hostpath=$(echo ${issuer_url}| cut -f 3- -d'/')
-  local account_id=$(gen3_aws_run aws sts get-caller-identity --query Account --output text)
-
-  local provider_arn="arn:aws:iam::${account_id}:oidc-provider/${issuer_hostpath}"
-  echo "${provider_arn}"
-
-  gen3_log_info "Entering create_assume_role_policy"
-
-  cat > ${tempFile} <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "${provider_arn}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "${issuer_hostpath}:aud": "sts.amazonaws.com",
-          "${issuer_hostpath}:sub": "system:serviceaccount:${namespace}:${saname}"
-        }
-      }
-    }
-  ]
-}
-EOF
-
-  echo ${tempFile}
-  gen3_log_info "Exiting create_assume_role_policy"
-
-}
-
-#
-# Create assume role for servce account
-# This is to create role ready for service account. Doesn't nessesary to have service account created.
-#
-gen3_awsrole_assumerole() {
-  local saname=${1}
-  #creat tmp policy and attach to the assume role
-  local namespace=$(gen3 db namespace)
-  local role_name="${namespace}-${saname}-role"
-  local assume_role_policy_path="$(create_assume_role_policy ${saname})"
-
-  gen3_log_info "Entering create_assumerole"
-
-  gen3_awsrole_create "${role_name}"
-
-  local role_json=$(gen3_aws_run aws iam update-assume-role-policy \
-                   --role-name ${role_name}\
-                   --policy-document file://${assume_role_policy_path})
-
-  if [ $? == 0 ];
-  then
-    echo ${role_json}
-  else
-   peae "There has been an error creating the role ${role_name}"
-  fi
-
-  gen3_log_info "Exiting create_role"
-}
-
-#
-# Create assume role for servce account
-#
-gen3_awsrole_createsa() {
-  local saname=${1}
-  local namespace=$(gen3 db namespace)
-
-  if g3kubectl get sa | grep "${saname}" > /dev/null 2>&1; then
-    gen3_log_info "Service account-${saname} exists. Skipping service account creation."
-    return 0
-  elif ! g3kubectl -n "${namespace}" create sa "${saname}" > /dev/null 2>&1; then
-    gen3_log_err "Unexpected error creating service account ${saname} "
-    return 1
-  fi
-
-  gen3_log_info "${saname} service account created."
-}
-
-#
-# Annotate existing service account with assume role
-#
-gen3_awsrole_annotatesa() {
-  local saname=${1}
-  local rolename=${2}
-  local namespace=$(gen3 db namespace)
-
-  gen3_log_info "Annotating service account-${saname} with role-${rolename} "
-
-  if ! _get_entity_type "${rolename}"> /dev/null 2>&1; then
-    gen3_log_err "Role-${rolename} doesn't exist. Role needs to be ceated."
-    return 1
-  fi
-
-  if ! g3kubectl get sa | grep "${saname}" > /dev/null 2>&1; then
-    gen3_log_err "Service account-${saname} doesn't exist. "
-    return 1
-  fi
-
-  rolearn=$(gen3_aws_run aws iam get-role --role-name ${rolename}| jq -r '.Role.Arn')
-  gen3_log_info "The Role ARN attached to service account is ${rolename}"
-
-  if g3kubectl describe sa ${saname} | grep eks.amazonaws.com/role-arn:  > /dev/null 2>&1; then
-    gen3_log_info "Service account ${saname} eks.amazonaws.com/role-arn got annotateed with role ${rolename} already. Exiting annotation."
-    return 0
-  elif ! g3kubectl annotate serviceaccount -n ${namespace} ${saname} eks.amazonaws.com/role-arn=${rolearn} > /dev/null 2>&1; then
-    gen3_log_err "Unexpected error when annotating service account-${saname} wtih role-${rolename}"
-    return 1
-  fi
-}
-
-##
-#
-# short for print error and exit
-#
-##
-function peae(){
-  print_error_and_exit ${1}
-}
-
 #---------- main
 
 gen3_awsrole() {
@@ -327,14 +281,14 @@ gen3_awsrole() {
     'attach-policy')
       gen3_awsrole_attachpolicy "$@"
       ;;
+    'ar-policy')
+      gen3_awsrole_ar_policy "$@"
+      ;;
+    'sa-annotate')
+      gen3_awsrole_sa_annotate "$@"
+      ;;
     'list' | 'ls')
       gen3_awsrole_list "$@"
-      ;;
-    'create-assumerole')
-      gen3_awsrole_assumerole "$@"
-      ;;
-    'annotate-sa')
-      gen3_awsrole_annotatesa "$@"
       ;;
     *)
       gen3_awsrole_help
