70aaca9f2231437d62f7498d9a2e1657ebca0500
Fauzi Gomez <fauzigo@gmail.com>
2020-07-15 16:00:50 -0500

    Chore/log dna log aggregation (#1324)
    * chore(logDNA): as part of the log pipeline
    
    * chore(try/except): better handling failures invoking logdna
    
    * fix(typo): carried by copy paste
    
    * chore(optimization): code optimization
    
    * chore(varibles): new additions, less hardcoded
    
    * chore(lifecycle): adjustments
    
    * chore(log_dna_function): default function changed
    
    * chore(memory_size): default value bumped
    
    * chore(documentation): improvements
    
    * add(validations): for logDNA lambda function ARN

--- tf_files/aws/csoc_common_logging/root.tf
+++ tf_files/aws/csoc_common_logging/root.tf
@@ -13,6 +13,9 @@ module "logging" {
   csoc_account_id  = "${var.csoc_account_id}"
   threshold        = "${var.threshold}"
   slack_webhook    = "${var.slack_webhook}"
+  log_dna_function = "${var.log_dna_function}"
+  memory_size      = "${var.memory_size}"
+  timeout          = "${var.timeout}"
 
   # put other variables here ...
 }
