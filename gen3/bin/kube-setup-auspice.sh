0ac4b3d02725dd86151382e98bb34266592e607e
Reuben Pasquini <reuben@frickjack.com>
2020-07-15 16:32:00 -0500

    chore(auspice): loadbalancer setup

--- gen3/bin/kube-setup-auspice.sh
+++ gen3/bin/kube-setup-auspice.sh
@@ -7,10 +7,10 @@
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 
-[[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
-
 gen3 roll auspice
 g3kubectl apply -f "${GEN3_HOME}/kube/services/auspice/auspice-service.yaml"
+ARN=$(g3kubectl get configmap global --output=jsonpath='{.data.revproxy_arn}')
+yq --arg ARN "$ARN" '.metadata.annotations["service.beta.kubernetes.io/aws-load-balancer-ssl-cert"] = $ARN' < "${GEN3_HOME}/kube/services/auspice/auspice-service-elb.yaml" | g3kubectl apply -f -
 
 cat <<EOM
 The auspice service has been deployed onto the k8s cluster.
