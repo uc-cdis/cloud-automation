348c3425d21b8749a641f17f0a6eabbb6180ec16
thanhnd <thanhnd@uchicago.edu>
2020-07-15 12:31:57 -0500

    fix(esproxy): remove esproxy setup

--- gen3/bin/kube-setup-auspice.sh
+++ gen3/bin/kube-setup-auspice.sh
@@ -8,18 +8,6 @@ source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 
 [[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
-gen3 kube-setup-aws-es-proxy || true
-
-COUNT=0
-while [[ 'true' != $(g3kubectl get pods --selector=app=esproxy -o json | jq -r '.items[].status.containerStatuses[0].ready' | tr -d '\n') ]]; do
-  if [[ COUNT -gt 50 ]]; then
-    echo "wait too long for esproxy"
-    exit 1
-  fi
-  echo "waiting for esproxy to be ready"
-  sleep 5
-  let COUNT+=1
-done
 
 gen3 roll auspice
 g3kubectl apply -f "${GEN3_HOME}/kube/services/auspice/auspice-service.yaml"
