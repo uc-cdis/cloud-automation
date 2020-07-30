e0ce48b50c715f503d208d61c670d11c766cce05
Reuben Pasquini <reuben@frickjack.com>
2020-07-30 09:28:00 -0500

    Chore/blacklist (#1344)
    * chore(gitops-sync-cronjob): delete
    
    * chore(revproxy): ip blacklist support
    
    * chore(revproxy): add csrf to /_status
    
    * chore(kube-setup-roles): mark default sa no-auto
    
    * docs(revproxy): blacklist details
    
    * chore(kube-setup-roles): j-namespace default sa
    
    no auto-mount property set

--- gen3/bin/kube-setup-roles.sh
+++ gen3/bin/kube-setup-roles.sh
@@ -9,9 +9,8 @@
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 
-if [[ -z "$_KUBES_SH" ]]; then
-  source "$GEN3_HOME/gen3/gen3setup.sh"
-fi # else already sourced this file ...
+g3kubectl patch serviceaccount default -p 'automountServiceAccountToken: false'
+g3kubectl patch serviceaccount --namespace "$(gen3 jupyter j-namespace)" default -p 'automountServiceAccountToken: false' > /dev/null || true
 
 # Don't do this in a Jenkins job
 if [[ -z "$JENKINS_HOME" ]]; then
