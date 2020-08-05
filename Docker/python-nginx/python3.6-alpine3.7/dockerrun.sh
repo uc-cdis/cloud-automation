8a566c751781adc5e35fc71a67887099bf4e2133
Marcelo Costa <marceloc@uchicago.edu>
2020-08-05 16:33:13 -0500

    create /var/www folder

--- Docker/python-nginx/python3.6-alpine3.7/dockerrun.sh
+++ Docker/python-nginx/python3.6-alpine3.7/dockerrun.sh
@@ -103,7 +103,7 @@ fi
   while true; do logrotate --force /etc/logrotate.d/nginx; sleep 86400; done
 ) &
 
-mkdir /var/www/metrics
+mkdir -p /var/www/metrics
 (
   while true; do
     curl -s http://127.0.0.1:9117/metrics > /var/www/metrics/metrics.txt
