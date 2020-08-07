a0978a123cf00b87717fddcd284f9284f1b7e9ca
Alexander VT <alexander.m.vantol@gmail.com>
2020-08-07 13:09:31 -0500

    feat(access-backend): always secrets sync on kube-setup

--- gen3/bin/kube-setup-sower-jobs.sh
+++ gen3/bin/kube-setup-sower-jobs.sh
@@ -147,13 +147,13 @@ EOM
   }
 }
 EOM
-    gen3 secrets sync 'setup sower-jobs credentials'
   fi
 }
 
 if [[ -f "$(gen3_secrets_folder)/creds.json" && -z "$JENKINS_HOME" ]]; then
     setup_sower_jobs
 fi
+gen3 secrets sync "setup sower-jobs secrets"
 
 cat <<EOM
 The sower-jobs bucket has been configured and the secret setup for use by sower jobs.
