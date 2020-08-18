c6b427eec23296709c62f7d825ef78eedf3883e4
mattreex <mattreex.9@gail.com>
2020-08-18 12:00:42 -0500

    Modifications based on PR comments

--- gen3/bin/config-env.sh
+++ gen3/bin/config-env.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 #
 # This script will copy the environment configuration of the specified environment or apply
-# version updates to all the services and may specify particular values for a given subset of services
+# version updates to all the services and may specify particular values for a given subset of services.
+# It is designed to be used in Dev or QA virtual machines.
+#
+# gen3 config-env copy {repo} {environment}
+# repo = The Github repository where the environment is located
+# environment = The Gen3 environment to be copied
+
+# gen3 config-env apply {version} {override} 
+# version = The version of services desired
+# override = (optional) Json-formatted string for assigning versions to specific services 
+
+# Example usage: 
+# gen3 config-env copy cdis-manifest gen3.theanvil.io
+# gen3 config-env apply 2020.09 
+# gen3 config-env apply 2020.09 {"ambassador":"quay.io/datawire/ambassador:2020.11"}
 
 source ${GEN3_HOME}/gen3/lib/utils.sh
 
 tgt_env=~/cdis-manifest/${USER}.planx-pla.net
 
 if [[ "$1" == "copy" ]]; then
-                git clone https://github.com/uc-cdis/${2}.git ~/temp_manifest
-                if [[ $? != 0 ]]; then
-        gen3_log_err "Something went wrong with getting source env check arguments"
-        return 1
+        git clone https://github.com/uc-cdis/${2}.git ~/temp_manifest
+        if [[ $? != 0 ]]; then
+        gen3_log_err "Something went wrong with getting source env check arguments\n Attempted to clone https://github.com/uc-cdis/${2}.git"
+                return 1
         fi
         srcenv=~/temp_manifest/$3
         cmd="copy -s ${srcenv} -e ${tgt_env}"
         
+# Assumes positional arguments apply {version} {overide}
 elif [[ "$1" == "apply" ]]; then
-        # Assumes positional arguments apply -v {version} -o {override}
         if [[ $# == 2 ]]; then 
                 cmd="$1 -v $2 -e ${tgt_env}"
+        # if the optional {override} param specified
         else
                 cmd="$1 -v $2 -o $3 -e ${tgt_env}"
         fi
