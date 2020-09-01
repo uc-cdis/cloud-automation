4a65f60fa215120c79e8abe093c56a26d58cd549
Jawad Qureshi <qureshi@uchicago.edu>
2020-09-01 14:59:55 -0500

    Add helm migrations to kube-setup-tiller

--- gen3/bin/kube-setup-tiller.sh
+++ gen3/bin/kube-setup-tiller.sh
@@ -3,15 +3,25 @@
 
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
-
-
+    
 if g3kubectl get deployments/tiller-deploy --namespace=kube-system > /dev/null 2>&1; then
-    g3kubectl delete -f "${GEN3_HOME}/kube/services/tiller/tiller-serviceaccount.yaml"
-    g3kubectl delete -f "${GEN3_HOME}/kube/services/tiller/tiller-clusterrolebinding.yaml"
-    g3kubectl delete deployments/tiller-deploy --namespace=kube-system
-    cat <<EOM
-tiller deployment has been removed.
-EOM
+  if helm version --short | grep v3 > /dev/null 2>&1; then
+    if ( g3kubectl --namespace=prometheus get deployment prometheus-server > /dev/null 2>&1) || ( g3kubectl --namespace=grafana get deployment grafana > /dev/null 2>&1); then
+      if ! helm plugin list | grep 2to3 > /dev/null 2>&1; then
+        helm plugin install https://github.com/helm/helm-2to3.git
+      fi
+      if ! helm ls --namespace grafana | grep grafana > /dev/null 2>&1; then
+        echo "Migrating grafana to helm3."
+        helm 2to3 convert grafana 
+      fi
+      if ! helm ls --namespace prometheus | grep prometheus > /dev/null 2>&1; then
+        echo "Migrating prometheus to helm3."
+        helm 2to3 convert prometheus
+      fi
+    fi
+    # delete tiller and other helm2 data/configs
+    helm 2to3 cleanup --skip-confirmation
+  fi
 else
   echo "tiller is already removed."
   exit 1
