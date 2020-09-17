a48f5d130855ca88b917b207e2f5c5a6235eda21
jawadqur <55899496+jawadqur@users.noreply.github.com>
2020-09-17 14:17:03 -0500

    Fix installation of 2to3 plugin for tiller cleanups (#1407)

--- gen3/bin/kube-setup-tiller.sh
+++ gen3/bin/kube-setup-tiller.sh
@@ -6,10 +6,10 @@ gen3_load "gen3/lib/kube-setup-init"
     
 if g3kubectl get deployments/tiller-deploy --namespace=kube-system > /dev/null 2>&1; then
   if helm version --short | grep v3 > /dev/null 2>&1; then
+    if ! helm plugin list | grep 2to3 > /dev/null 2>&1; then
+      helm plugin install https://github.com/helm/helm-2to3.git
+    fi
     if ( g3kubectl --namespace=prometheus get deployment prometheus-server > /dev/null 2>&1) || ( g3kubectl --namespace=grafana get deployment grafana > /dev/null 2>&1); then
-      if ! helm plugin list | grep 2to3 > /dev/null 2>&1; then
-        helm plugin install https://github.com/helm/helm-2to3.git
-      fi
       if ! helm ls --namespace grafana | grep grafana > /dev/null 2>&1; then
         echo "Migrating grafana to helm3."
         helm 2to3 convert grafana 
