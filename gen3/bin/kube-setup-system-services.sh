80e7e1fbbc798767999af395aee53446a72a2f6f
Reuben Pasquini <reuben@frickjack.com>
2020-10-01 09:13:16 -0500

    Chore/no443 (#1415)
    * chore(nginx): remove 443 listener
    
    * chore(k-setup-system-s): patch coredns configmap
    
    * chore(revproxy-deploy): remove 443 containerport

--- gen3/bin/kube-setup-system-services.sh
+++ gen3/bin/kube-setup-system-services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Run terraform as a script for updates on EKS.
-# This should not be ran as a crontjob, it might cause services disruption, so be carefull
+# This should not be ran as a cron job, it might cause services disruption, so be carefull
 #
 # Cases in which this script might be useful:
 #  1.) EKS reseleased a new minor version and said release came along with a new AMI that terraform picks up.
@@ -13,6 +13,8 @@
 #set -i
 
 # Based on https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/gen3setup"
 
 kubeproxy=${kubeproxy:-1.15.11}
 coredns=${coredns:-1.6.6}
@@ -25,47 +27,26 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-if ! [[ -d "$HOME/cloud-automation" ]]; then
-  echo "ERROR: this does not look like a commons environment"
-  exit 1
-fi
-
-export vpc_name="$(grep 'vpc_name=' $HOME/.bashrc |cut -d\' -f2)"
-export GEN3_HOME="$HOME/cloud-automation"
-#export KUBECONFIG="$HOME/${vpc_name}/kubeconfig"
-#PATH="${PATH}:/usr/local/bin"
-
-
-if [[ -z "$XDG_DATA_HOME" ]]; then
-  export XDG_DATA_HOME="$HOME/.local/share"
-fi
-if [[ -z "$USER" ]]; then
-  export USER="$(basename "$HOME")"
-fi
-if [[ -z "$XDG_RUNTIME_DIR" ]]; then
-  export XDG_RUNTIME_DIR="/tmp/gen3-$USER-$$"
-  mkdir -m 700 -p "$XDG_RUNTIME_DIR"
-fi
-
-source "${GEN3_HOME}/gen3/gen3setup.sh"
-#gen3 gitops tfplan $@
-
-
 kube_proxy_image="602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/kube-proxy:v${kubeproxy}"
-
-
 coredns_image="602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/coredns:v${coredns}"
-
 cni_image="https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/release-${cni}/config/v${cni}/aws-k8s-cni.yaml"
-
 
 g3kubectl set image daemonset.apps/kube-proxy -n kube-system kube-proxy=${kube_proxy_image}
 g3kubectl set image --namespace kube-system deployment.apps/coredns coredns=${coredns_image}
 g3kubectl apply -f ${cni_image}
 
-
-function get_current_minor_version(){
-  local mayor=$(g3kubectl version -o json | jq -r  '.serverVersion.minor' )
-  echo ${mayor}
-}
-
+# let's make sure the coredns configmap is up to date
+# see: https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html
+(
+  tempFile="$(mktemp "$XDG_RUNTIME_DIR/coredns.json_XXXXXX")"
+  if g3kubectl get configmap coredns -n kube-system -o json | jq -e -r .data.Corefile > "$tempFile" && [[ ${PIPESTATUS[0]} == 0 ]]; then
+    if grep 'proxy . /etc/resolv.conf' "$tempFile" > /dev/null && \
+      sed -i 's@proxy . /etc/resolv.conf@forward . /etc/resolv.conf@' "$tempFile"; then
+      gen3_log_info "patching coredns configmap"
+      g3kubectl patch configmap coredns -n kube-system --patch "$(jq -n -r --arg Corefile "$(cat $tempFile)" '.data.Corefile = $Corefile')"
+    fi
+  else
+    gen3_log_warn "Failed to retrieve coredns configmap from kube-system namespace"
+  fi
+  rm "$tempFile"
+)
