4bf59c25a1ec1567d6a063b764d872bea4828b59
emalinowski <emalinowski@uchicago.edu>
2020-10-15 16:16:11 -0500

    feat(access-roll): Added access-backend to kube-roll-all (#1431)
    Co-authored-by: Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>

--- gen3/bin/kube-roll-all.sh
+++ gen3/bin/kube-roll-all.sh
@@ -192,6 +192,12 @@ if g3k_manifest_lookup .versions.ssjdispatcher 2>&1 /dev/null; then
   gen3 kube-setup-ssjdispatcher
 fi
 
+if g3k_manifest_lookup .versions.access-backend 2> /dev/null; then
+  gen3 kube-setup-access-backend
+else
+  gen3_log_info "not deploying access-backend - no manifest entry for .versions.access-backend"
+fi
+
 gen3 kube-setup-revproxy
 
 if [[ "$GEN3_ROLL_FAST" != "true" ]]; then
