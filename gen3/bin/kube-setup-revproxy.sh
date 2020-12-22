4fb444666edbce650a623a722150951f9547df88
Marcelo R Costa <marceloc@uchicago.edu>
2020-12-22 09:39:02 -0600

    chore(observability): Enable metrics scraping for revproxy (#1376)
    * chore(observability): Enable metrics scraping for revproxy
    
    * add logrotate dependencies and setting up metrics aggregation loops
    
    * enable status endpoint and non-json access logs
    
    * add aggregated_metrics endpoint and prometheus annotations
    
    * move commands to proper command block
    
    * create configmap for logrotate config file
    
    * rename configmap
    
    * fixing logrotate stuff
    
    * introduce sleep5 and more logging for nginx-prometheus-exporter
    
    * remove white spaces
    
    * better logic to initialize nginx-prometheus-exporter sidecar
    
    * fix comparison of status code output
    
    * only collect metrics if logrotate is installed on the nginx img

--- gen3/bin/kube-setup-revproxy.sh
+++ gen3/bin/kube-setup-revproxy.sh
@@ -125,6 +125,7 @@ fi
 
 [[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
 gen3 update_config revproxy-nginx-conf "${scriptDir}/nginx.conf"
+gen3 update_config logrotate-nginx-conf "${scriptDir}/logrotate-nginx.conf"
 gen3 update_config revproxy-helper-js "${scriptDir}/helpers.js"
 
 if g3kubectl get configmap revproxy-nginx-subconf > /dev/null 2>&1; then
