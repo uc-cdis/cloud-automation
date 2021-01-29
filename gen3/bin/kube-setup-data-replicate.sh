5dab4fdec7aa1982c432f23d565aedf36ee68ec6
giangbui <giangbui0816@gmail.com>
2021-01-29 10:48:06 -0600

    chore(data_refresh): get mapping file from manifest

--- gen3/bin/kube-setup-data-replicate.sh
+++ gen3/bin/kube-setup-data-replicate.sh
@@ -7,16 +7,50 @@ set -e
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 
-if [[ -d ${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice ]]; then
-  g3kubectl delete secret dcf-aws-creds-secret
-  g3kubectl delete secret google-creds-secret
-  g3kubectl delete secret dcf-dataservice-json-secret
-  g3kubectl delete secret dcf-dataservice-settings-secrets
-  g3kubectl delete configmap project-map-manifest
+secret_folder="$(gen3_secrets_folder)"
 
-  g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/aws_creds_secret
-  g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/gcloud-creds-secret
-  g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json
-  g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/dcf_dataservice_settings
-  g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json
+if [[ -d ${secret_folder}/apis_configs/dcf_dataservice ]]; then
+  if g3kubectl get secret dcf-aws-creds-secret; then
+    g3kubectl delete secret dcf-aws-creds-secret
+  fi
+  if g3kubectl get secret google-creds-secret; then
+    g3kubectl delete secret google-creds-secret
+  fi
+  if g3kubectl get secret dcf-dataservice-json-secret
+    g3kubectl delete secret dcf-dataservice-json-secret
+  fi
+  if g3kubectl get secret dcf-dataservice-settings-secrets
+    g3kubectl delete secret dcf-dataservice-settings-secrets
+  fi
+  if g3kubectl get configmap project-map-manifest
+    g3kubectl delete configmap project-map-manifest
+  fi
+ 
+  if ! hostname="$(gen3 api hostname)"; then
+    gen3_log_err "could not determine hostname from manifest-global - bailing out of data refresh setup"
+    return 1
+  fi
+
+  cp ${GEN3_MANIFEST_HOME}/${hostname}/manifests/datarefresh/GDC_project_map.json ${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json
+
+  GDC_TOKEN=$(cat ${secret_folder}/apis_configs/dcf_dataservice/creds.json | jq '.GDC_TOKEN')
+  INDEXD_CRED=$(cat ${secret_folder}/apis_configs/dcf_dataservice/creds.json | jq '.INDEXD')
+
+  cat >${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings << EOL
+TOKEN = ${GDC_TOKEN}
+  
+INDEXD = ${INDEXD_CRED}
+
+DATA_ENDPT = "https://api.gdc.cancer.gov/data/"
+
+PROJECT_ACL = $(cat ${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json)
+
+IGNORED_FILES = "/dcf-dataservice/ignored_files_manifest.csv"
+EOL
+
+  g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${secret_folder}/apis_configs/dcf_dataservice/aws_creds_secret
+  g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${secret_folder}/apis_configs/dcf_dataservice/gcloud-creds-secret
+  g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${secret_folder}/apis_configs/dcf_dataservice/creds.json
+  g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings
+  g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json
 fi
