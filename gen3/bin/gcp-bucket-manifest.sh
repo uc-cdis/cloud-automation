147445d09dee719a36aa06f17d55c4653203dc70
Marcelo Costa <marceloc@uchicago.edu>
2021-03-01 10:44:59 -0600

    testing custom branch

--- gen3/bin/gcp-bucket-manifest.sh
+++ gen3/bin/gcp-bucket-manifest.sh
@@ -68,6 +68,7 @@ gen3_create_google_dataflow() {
   virtualenv venv
   source venv/bin/activate
   git clone https://github.com/uc-cdis/google-bucket-manifest && cd google-bucket-manifest
+  git checkout chore/potential_fix_for_gcp_bucket_manifest
   pip install -r requirements.txt
 
   # Build a template
