8e484109b37efc3611ba06d93b39188f4074d2e2
Marcelo R Costa <marceloc@uchicago.edu>
2021-03-02 13:21:22 -0600

    Chore/test custom gcp bucket manifest branch (#1537)
    * testing custom branch
    
    * try the poetry version
    
    * poetry run
    
    * set proxy only for gcloud / gsutil operations
    
    * generate setup.py
    
    * set https_proxy
    
    * set proxy and then remove it after starting runner
    
    * testing to check if we still need the https_proxy
    
    * more comments for posterity
    
    * re-enable unset https_proxy and add more comments
    
    * remove unnecessary gcloud commands
    
    * re-enable unset https_proxy and add more comments
    
    * remove custom branch checkout

--- gen3/bin/gcp-bucket-manifest.sh
+++ gen3/bin/gcp-bucket-manifest.sh
@@ -65,13 +65,20 @@ gen3_create_google_dataflow() {
   gen3 cd
 
   # Download code to build a google template
-  virtualenv venv
-  source venv/bin/activate
   git clone https://github.com/uc-cdis/google-bucket-manifest && cd google-bucket-manifest
-  pip install -r requirements.txt
+  
+  poetry install -vv --no-dev
+  poetry run poetry2setup > setup.py
+
+  # the following proxy config unblocks apache_beam.runners.dataflow.internal.apiclient GCS upload operations
+  # however, this blocks the g3kubectl commands
+  # (causes "Unable to connect to the server: Forbidden" error)
+  # that is why it can only be declared after "g3kubectl get secret gcp-bucket-manifest-g3auto"
+  # and needs to be unset before we try any "g3kubectl commands", hence the unset later in this script
+  export https_proxy='http://cloud-proxy.internal.io:3128'
 
   # Build a template
-  python bucket_manifest_pipeline.py --runner DataflowRunner  --project "$project" --bucket "$bucket" --temp_location gs://"$temp_bucket"/temp  --template_location gs://"$temp_bucket"/templates/pipeline_template --region us-central1 --setup_file ./setup.py --service_account_email "${service_account}"
+  poetry run python bucket_manifest_pipeline.py --runner DataflowRunner  --project "$project" --bucket "$bucket" --temp_location gs://"$temp_bucket"/temp  --template_location gs://"$temp_bucket"/templates/pipeline_template --region us-central1 --setup_file ./setup.py --service_account_email "${service_account}"
   gen3 cd
 
   local pubsub_topic_name=$(echo "${prefix}-pubsub_topic_name" | head -c63)
@@ -109,7 +116,12 @@ EOF
   fi
   n_messages="$(gsutil ls -r gs://${bucket}/** | wc -l)"
   #gen3 gitops filter $HOME/cloud-automation/kube/services/jobs/google-bucket-manifest-job.yaml PROJECT $project PUBSUB_SUB ${pubsub_sub} AUTHZ ${metadata_file} N_MESSAGES ${n_messages} OUT_BUCKET ${temp_bucket} | sed "s|sa-#SA_NAME_PLACEHOLDER#|$saName|g" | sed "s|gcp-bucket-manifest#PLACEHOLDER#|gcp-bucket-manifest-${jobId}|g" > ./google-bucket-manifest-${jobId}-job.yaml
+
+  # unset proxy, otherwise, this will break the g3k_manifest_init function
+  unset https_proxy
+
   gen3 gitops filter $GEN3_HOME/kube/services/jobs/google-bucket-manifest-job.yaml PROJECT $project PUBSUB_SUB ${pubsub_sub_name} METADATA_FILE "${metadata_file}" N_MESSAGES $n_messages OUT_BUCKET $temp_bucket | sed "s|google-bucket-manifest#PLACEHOLDER#|google-bucket-manifest-${jobId}|g" > ./google-bucket-manifest-${jobId}-job.yaml
+
   gen3 secrets sync "initialize gcp-bucket-manifest/config.json"
   gen3 job run ./google-bucket-manifest-${jobId}-job.yaml
   gen3_log_info "The job is started. Job ID: ${jobId}"
