5621cb5a2753db0c0cbaedaed96482a889852e7e
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2021-04-08 16:33:28 +1000

    Update TF AWS EKS module
    Remove dependency on hardcoded default AvailabilityZones. Replace
    with dynamic lookup from current aws_availability_zones data source.

--- tf_files/aws/modules/eks/variables.tf
+++ tf_files/aws/modules/eks/variables.tf
@@ -88,7 +88,7 @@ variable "oidc_eks_thumbprint" {
 variable "availability_zones" {
   description = "AZ to be used by EKS nodes"
   type        = "list"
-  default     = ["us-east-1a", "us-east-1c", "us-east-1d"]
+  default     = []
 }
 
 variable "domain_test" {
