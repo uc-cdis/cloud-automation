953177114ba948b00eea55f51c19e84d23d39add
Marcelo Costa <themarcelor@gmail.com>
2021-05-04 12:42:26 -0500

    chore(ci-debugging): Capture logs from restarting pods

--- gen3/bin/save-failed-pod-logs.sh
+++ gen3/bin/save-failed-pod-logs.sh
@@ -28,7 +28,8 @@ for pod in "${array_of_img_pull_errors[@]}"; do
   g3kubectl describe pod $pod_name > img_pull_error_${pod_name}.log
 done
 
-# container / service startup errors
+# get array of pods
+container / service startup errors
 array_of_svc_startup_errors=($(g3kubectl get pods | grep -E "Failed|CrashLoopBackOff" | xargs -I {} echo {} | awk '{ print $1 }' | tr "\n" " "))
 
 for pod in "${array_of_svc_startup_errors[@]}"; do
@@ -36,6 +37,24 @@ for pod in "${array_of_svc_startup_errors[@]}"; do
   gen3_log_info "storing kubectl logs output into svc_startup_error_${pod_name}.log..."
   container_name=$(g3kubectl get pod ${pod_name} -o jsonpath='{.spec.containers[0].name}')
   g3kubectl logs $pod_name -c ${container_name} > svc_startup_error_${pod_name}.log
+done
+
+# check if the restartCount is greater than zero
+# kc get pod metadata-deployment-5b897b7cfd-s2kmt -o jsonpath='{.status.containerStatuses[0].restartCount}'
+
+array_of_pods=($(g3kubectl get pods | xargs -I {} echo {} | awk '{ print $1 }' | tr "\n" " "))
+
+for pod in "${array_of_pods[@]}"; do
+  # trim whitespaces
+  pod_name=$(echo $pod | xargs)
+  # check if the restartCount is greater than zero
+  # e.g., g3kubectl get pod metadata-deployment-5b897b7cfd-s2kmt -o jsonpath='{.status.containerStatuses[0].restartCount}'
+  restart_count=$(g3kubectl get pod ${pod_name} -o jsonpath='{.status.containerStatuses[0].restartCount}')
+  if [[ $restart_count -gt 0 ]]; then
+    gen3_log_info "Pod $pod_name restarted $restart_count times... let us capture some logs."
+    container_name=$(g3kubectl get pod ${pod_name} -o jsonpath='{.spec.containers[0].name}')
+    g3kubectl logs $pod_name -c ${container_name} > svc_startup_error_${pod_name}.log
+  fi
 done
 
 echo "$(date): Done capturing logs" > save-failed-pod-logs.log
