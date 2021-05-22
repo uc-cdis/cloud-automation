2f99eb24cfbd67db2b9d8276901c35091de42b5b
Marcelo Costa <themarcelor@gmail.com>
2021-05-21 19:34:11 -0500

    chore(reliability): Allow fence-config to override Nginx rate limit

--- Docker/python-nginx/python3.6-alpine3.7/entrypoint.sh
+++ Docker/python-nginx/python3.6-alpine3.7/entrypoint.sh
@@ -2,8 +2,18 @@
 set -e
 
 if [ ! -z $NGINX_RATE_LIMIT ]; then
+  echo "Found NGINX_RATE_LIMIT environment variable..."
+  contains_rate_limit_override=$(cat /var/www/fence/fence-config.yaml | grep OVERRIDE_NGINX_RATE_LIMIT)
+  RC=$?
+  if [[ $RC -eq 0 ]]; then
+    rate_limit=$(echo $contains_rate_limit_override | cut -d"=" -f 2 | xargs)
+    echo "Applying new Nginx rate limit ${rate_limit}..."
+  else
+    rate_limit=$NGINX_RATE_LIMIT
+  fi
+
   # Add rate_limit config
-  rate_limit_conf="\ \ \ \ limit_req_zone \$binary_remote_addr zone=one:10m rate=${NGINX_RATE_LIMIT}r/s;"
+  rate_limit_conf="\ \ \ \ limit_req_zone \$binary_remote_addr zone=one:10m rate=${rate_limit}r/s;"
   sed -i "/http\ {/a ${rate_limit_conf}" /etc/nginx/nginx.conf
   if [ -f /etc/nginx/sites-available/uwsgi.conf ]; then
     limit_req_config="\ \ \ \ \ \ \ \ limit_req zone=one;"
