98947ad0ef083415106d45d98bb22e44a99e55f0
cterrazas2 <cterrazas69@gmail.com>
2021-06-04 12:36:23 -0700

    feat(wf-db): setup wf db for mariner deployment

--- gen3/bin/kube-setup-mariner.sh
+++ gen3/bin/kube-setup-mariner.sh
@@ -6,6 +6,21 @@
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 
+# TODO: Add logic to support other types of toggable storage (s3, postgres, redis, etc) 
+
+manifestPath=$(g3k_manifest_path)
+
+# extract storage settings from manifest
+withDB="$(jq -r ".[\"mariner\"][\"with_db\"]" < "$manifestPath")"
+withCache="$(jq -r ".[\"mariner\"][\"with_cache\"]" < "$manifestPath")"
+withS3="$(jq -r ".[\"mariner\"][\"with_s3\"]" < "$manifestPath")"
+
+if [ "$withDB" == "yes" ]; then
+  gen3_log_info "setting up mariner with database"
+else
+  gen3_log_info "setting up mariner without database, must set to yes in manifest.json"
+fi
+
 #- lib ---------------------------
 
 #
@@ -98,6 +113,31 @@ setup_mariner_service() {
   gen3 secrets sync 'chore(mariner): setup secrets'
 }
 
+#
+# setup mariner workflow database, if it is set in manifest.
+#
+setup_mariner_db() {
+ if ! g3kubectl describe secret mariner-g3auto | grep dbcreds.json > /dev/null 2>&1; then
+      echo "create mariner database"
+      if ! gen3 db setup mariner; then
+	  echo "Failed setting up mariner database"
+      fi
+      gen3 secrets sync
+ fi
+ if [[ -f "$(gen3_secrets_folder)/g3auto/mariner/dbcreds.json"]]; then
+  cd "$(gen3_secrets_folder)"
+  if [[ ! -f "$(gen3_secrets_folder)/.rendered_mariner_db" ]]; then
+    gen3 job run marinerdb-create
+    echo "Waiting 10 seconds for marinerdb-create job"
+    sleep 10
+    gen3 job logs marinerdb-create || true
+    echo "Leaving setup jobs running in background"
+    cd "$(gen3_secrets_folder)"
+  fi
+    touch "$(gen3_secrets_folder)/.rendered_mariner_db"
+ fi
+}
+
 #-- main ------------------------
 
 if [[ -z "$GEN3_SOURCE_ONLY" ]]; then
@@ -109,6 +149,11 @@ if [[ -z "$GEN3_SOURCE_ONLY" ]]; then
   [[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
 
   setup_mariner_service
+  
+  if [ "$withDB" ==  "yes" ]; then
+    setup_mariner_db
+  fi
+
   gen3 roll mariner 
   g3kubectl apply -f "${GEN3_HOME}/kube/services/mariner/mariner-service.yaml"
 
