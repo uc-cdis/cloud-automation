5b97120c2dc3ce4cd3a2201c3cf5bf00c245ae0f
Marcelo Costa <themarcelor@gmail.com>
2021-07-19 16:49:59 -0500

    also apply fix to guppy test mutation

--- gen3/bin/mutate-guppy-config-for-guppy-test.sh
+++ gen3/bin/mutate-guppy-config-for-guppy-test.sh
@@ -16,7 +16,7 @@ sed -i 's/\(.*\)"index": "\(.*\)_etl",$/\1"index": "jenkins_subject_alias",/' or
 sed -i 's/\(.*\)"index": "\(.*\)_file",$/\1"index": "jenkins_file_alias",/' original_guppy_config.yaml
 sed -i 's/\(.*\)"config_index": "\(.*\)_array-config",$/\1"config_index": "jenkins_configs_alias",/' original_guppy_config.yaml
 
-# mutating after guppy test (pre-defined canine config) and some qa-* env guppy configs
+# mutating back to expected canine config in case this runs after pfb export test
 sed -i 's/\(.*\)"index": "\(.*\)_subject_alias",$/\1"index": "'"${prNumber}"'.'"${repoName}"'.\2_subject",/' original_guppy_config.yaml
 sed -i 's/\(.*\)"index": "\(.*\)_file_alias",$/\1"index": "'"${prNumber}"'.'"${repoName}"'.\2_file",/' original_guppy_config.yaml
 sed -i 's/\(.*\)"config_index": "\(.*\)_configs_alias",$/\1"config_index": "'"${prNumber}"'.'"${repoName}"'.\2_array-config",/' original_guppy_config.yaml
