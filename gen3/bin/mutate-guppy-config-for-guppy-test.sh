e8859542876c034dee9c825e31fb6bde5dc14c50
Marcelo Costa <themarcelor@gmail.com>
2021-07-19 16:49:00 -0500

    apply same fix for guppy tests mutation

--- gen3/bin/mutate-guppy-config-for-guppy-test.sh
+++ gen3/bin/mutate-guppy-config-for-guppy-test.sh
@@ -15,6 +15,12 @@ g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
 sed -i 's/\(.*\)"index": "\(.*\)_etl",$/\1"index": "jenkins_subject_alias",/' original_guppy_config.yaml
 sed -i 's/\(.*\)"index": "\(.*\)_file",$/\1"index": "jenkins_file_alias",/' original_guppy_config.yaml
 sed -i 's/\(.*\)"config_index": "\(.*\)_array-config",$/\1"config_index": "jenkins_configs_alias",/' original_guppy_config.yaml
+
+# mutating after guppy test (pre-defined canine config) and some qa-* env guppy configs
+sed -i 's/\(.*\)"index": "\(.*\)_subject_alias",$/\1"index": "'"${prNumber}"'.'"${repoName}"'.\2_subject",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_file_alias",$/\1"index": "'"${prNumber}"'.'"${repoName}"'.\2_file",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"config_index": "\(.*\)_configs_alias",$/\1"config_index": "'"${prNumber}"'.'"${repoName}"'.\2_array-config",/' original_guppy_config.yaml
+
 g3kubectl delete configmap manifest-guppy
 g3kubectl apply -f original_guppy_config.yaml
 gen3 roll guppy
