f286a1132852243df6255f97fa735854286bf87a
Marcelo R Costa <marceloc@uchicago.edu>
2021-07-19 18:19:46 -0500

    Fix/guppy config mutation (#1681)
    * fix(ci): Add proper guppy config mutation for manifest repo PRs
    
    * apply same fix for guppy tests mutation
    
    * also apply fix to guppy test mutation
    
    * mutate guppy config according to qa envs
    
    * ultimate guppy config mutation solution

--- gen3/bin/mutate-guppy-config-for-pfb-export-test.sh
+++ gen3/bin/mutate-guppy-config-for-pfb-export-test.sh
@@ -20,11 +20,23 @@ if ! shift; then
  exit 1
 fi
 
+# capture the names from the ETL mapping
+etl_mapping_subject=$(g3kubectl get cm etl-mapping -o jsonpath='{.data.etlMapping\.yaml}' | yq .mappings[0].name)
+etl_mapping_file=$(g3kubectl get cm etl-mapping -o jsonpath='{.data.etlMapping\.yaml}' | yq .mappings[1].name)
+etl_config=$(echo $etl_mapping_subject | sed 's/\(.*\)_etl/\1_array-config/')
+
 g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
-sed -i 's/\(.*\)"index": "\(.*\)_subject",$/\1"index": "'"${prNumber}"'.'"${repoName}"'.\2_subject",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"index": "\(.*\)_etl",$/\1"index": "'"${prNumber}"'.'"${repoName}"'.\2_etl",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"index": "\(.*\)_file",$/\1"index": "'"${prNumber}"'.'"${repoName}"'.\2_file",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"config_index": "\(.*\)_array-config",$/\1"config_index": "'"${prNumber}"'.'"${repoName}"'.\2_array-config",/' original_guppy_config.yaml
+# mutating permanent jenkins config
+sed -i 's/\(.*\)"index": "\(.*\)_subject",$/\1"index": "'"${etl_mapping_subject}"'",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_etl",$/\1"index": "'"${etl_mapping_subject}"'",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_file",$/\1"index": "'"${etl_mapping_file}"'",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"config_index": "\(.*\)_array-config",$/\1"config_index": "'"${etl_config}"'",/' original_guppy_config.yaml
+
+# mutating after guppy test (pre-defined canine config) and some qa-* env guppy configs
+sed -i 's/\(.*\)"index": "\(.*\)_subject_alias",$/\1"index": "'"${etl_mapping_subject}"'",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_file_alias",$/\1"index": "'"${etl_mapping_file}"'",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"config_index": "\(.*\)_configs_alias",$/\1"config_index": "'"${etl_config}"'",/' original_guppy_config.yaml
+
 g3kubectl delete configmap manifest-guppy
 g3kubectl apply -f original_guppy_config.yaml
 gen3 roll guppy
