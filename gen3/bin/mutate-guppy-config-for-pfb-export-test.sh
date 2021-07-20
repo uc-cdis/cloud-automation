e4ad01ebf5866ac0278e16bc13e10acdd9b16784
Marcelo Costa <themarcelor@gmail.com>
2021-07-20 00:39:15 -0500

    fix(ci): Mutate manifest-guppy cm properly

--- gen3/bin/mutate-guppy-config-for-pfb-export-test.sh
+++ gen3/bin/mutate-guppy-config-for-pfb-export-test.sh
@@ -21,21 +21,25 @@ if ! shift; then
 fi
 
 # capture the names from the ETL mapping
+echo "debugging"
 etl_mapping_subject=$(g3kubectl get cm etl-mapping -o jsonpath='{.data.etlMapping\.yaml}' | yq .mappings[0].name)
+echo "### ## etl_mapping_subject: ${etl_mapping_subject}"
 etl_mapping_file=$(g3kubectl get cm etl-mapping -o jsonpath='{.data.etlMapping\.yaml}' | yq .mappings[1].name)
+echo "echo "### ## etl_mapping_file: ${etl_mapping_file}"
 etl_config=$(echo $etl_mapping_subject | sed 's/\(.*\)_etl/\1_array-config/')
+echo "### ## etl_config: ${etl_config}"
 
 g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
 # mutating permanent jenkins config
-sed -i 's/\(.*\)"index": "\(.*\)_subject",$/\1"index": "'"${etl_mapping_subject}"'",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"index": "\(.*\)_etl",$/\1"index": "'"${etl_mapping_subject}"'",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"index": "\(.*\)_file",$/\1"index": "'"${etl_mapping_file}"'",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"config_index": "\(.*\)_array-config",$/\1"config_index": "'"${etl_config}"'",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_subject",$/\1"index": '"${etl_mapping_subject}"',/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_etl",$/\1"index": '"${etl_mapping_subject}"',/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_file",$/\1"index": '"${etl_mapping_file}"',/' original_guppy_config.yaml
+sed -i 's/\(.*\)"config_index": "\(.*\)_array-config",$/\1"config_index": '"${etl_config}"',/' original_guppy_config.yaml
 
 # mutating after guppy test (pre-defined canine config) and some qa-* env guppy configs
-sed -i 's/\(.*\)"index": "\(.*\)_subject_alias",$/\1"index": "'"${etl_mapping_subject}"'",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"index": "\(.*\)_file_alias",$/\1"index": "'"${etl_mapping_file}"'",/' original_guppy_config.yaml
-sed -i 's/\(.*\)"config_index": "\(.*\)_configs_alias",$/\1"config_index": "'"${etl_config}"'",/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_subject_alias",$/\1"index": '"${etl_mapping_subject}"',/' original_guppy_config.yaml
+sed -i 's/\(.*\)"index": "\(.*\)_file_alias",$/\1"index": '"${etl_mapping_file}"',/' original_guppy_config.yaml
+sed -i 's/\(.*\)"config_index": "\(.*\)_configs_alias",$/\1"config_index": '"${etl_config}"',/' original_guppy_config.yaml
 
 g3kubectl delete configmap manifest-guppy
 g3kubectl apply -f original_guppy_config.yaml
