d8f1c37e7a7c4c0abd5896b24350e0f1f4bc392b
Marcelo Costa <themarcelor@gmail.com>
2021-07-27 17:43:34 -0500

    fix(ci): Guppy test config mutation needs to switch from case to subject

--- gen3/bin/mutate-guppy-config-for-guppy-test.sh
+++ gen3/bin/mutate-guppy-config-for-guppy-test.sh
@@ -15,6 +15,8 @@ g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
 sed -i 's/\(.*\)"index": "\(.*\)_etl",$/\1"index": "jenkins_subject_alias",/' original_guppy_config.yaml
 # for bloodpac-like envs
 sed -i 's/\(.*\)"index": "\(.*\)_case",$/\1"index": "jenkins_subject_alias",/' original_guppy_config.yaml
+# the pre-defined Canine index works with subject ONLY (never case)
+sed -i 's/\(.*\)"type": "case"$/\1"type": "subject",/' original_guppy_config.yaml
 sed -i 's/\(.*\)"index": "\(.*\)_file",$/\1"index": "jenkins_file_alias",/' original_guppy_config.yaml
 sed -i 's/\(.*\)"config_index": "\(.*\)_array-config",$/\1"config_index": "jenkins_configs_alias",/' original_guppy_config.yaml
 
