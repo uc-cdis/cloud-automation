a868fcef48abc9075a67402a0091910910734b20
Alexander VT <alexander.m.vantol@gmail.com>
2021-09-02 13:53:34 -0500

    feat(nginx): deploy on branches but not specific versions unless >= given version

--- gen3/bin/kube-setup-revproxy.sh
+++ gen3/bin/kube-setup-revproxy.sh
@@ -75,12 +75,13 @@ confFileList+=("--from-file" "$scriptDir/gen3.nginx.conf/README.md")
 # load priority confs first (who need to fallback on later confs)
 
 # add new nginx conf to route ga4gh access requests to fence instead of indexd for
-#    - master branch of fence
+#    - any branch of fence (technically it's just looking that the image doesn't START with a number)
 #    - semver >= 5.5.0
 fence_image_version=$([[ $(g3k_manifest_lookup .versions.fence) =~ \:(.*) ]] && echo "${BASH_REMATCH[1]}")
-if [[ "$fence_image_version"=="master" || ( $(fence_image_version =~ ^[[:digit:]]) && $(semver_ge "$fence_image_version" "5.5.0") ) ]]; then
+if [[ $(fence_image_version =~ ^[[:alpha:]]) || ( $(fence_image_version =~ ^[[:digit:]]) && $(semver_ge "$fence_image_version" "5.5.0") ) ]]; then
   filePath="$scriptDir/gen3.nginx.conf/fence-service-ga4gh.conf"
   if [[ -f "$filePath" ]]; then
+    echo "$filePath being added to nginx conf file list"
     confFileList+=("--from-file" "$filePath")
   fi
 fi
