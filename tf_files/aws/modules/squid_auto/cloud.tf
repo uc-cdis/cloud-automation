f5f5ed86083c633badf174f2e30c2bdf22f59701
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2021-10-14 10:11:51 -0500

    feat(qualys-agent-setup): Added qualys agent to squid and utility vm terraform

--- tf_files/aws/modules/squid_auto/cloud.tf
+++ tf_files/aws/modules/squid_auto/cloud.tf
@@ -143,6 +143,15 @@ CLOUD_AUTOMATION="$USER_HOME/cloud-automation"
   cd $CLOUD_AUTOMATION
   git checkout master
 ) > /var/log/bootstrapping_script.log
+
+# Install qualys agent if the activtion and customer id provided
+if [[ ! -z "${var.activation_id}" ]] || [[ ! -z "${var.customer_id}" ]]; then
+    aws s3 cp s3://qualys-agentpackage/QualysCloudAgent.rpm ./qualys-cloud-agent.x86_64.rpm
+    sudo rpm -ivh qualys-cloud-agent.x86_64.rpm
+    # Clean up rpm package after install
+    rm qualys-cloud-agent.x86_64.rpm
+    sudo /usr/local/qualys/cloud-agent/bin/qualys-cloud-agent.sh ActivationId=${activation_id} CustomerId=${customer_id}
+fi
 EOF
 
 lifecycle {
--- tf_files/aws/squid_auto/root.tf
+++ tf_files/aws/squid_auto/root.tf
@@ -36,7 +36,9 @@ module "squid_auto" {
   cluster_min_size           = "${var.cluster_min_size}"
   network_expansion          = "${var.network_expansion}"
   branch                     = "${var.branch}"
-  
+  activation_id              = "${var.activation_id}"
+  customer_id                = "${var.customer_id}"  
+
   # put other variables here ...
 }
 
