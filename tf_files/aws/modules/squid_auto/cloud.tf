274be723cb08ab2b5e9dbb3a48a4df78b79ca8ae
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2021-10-18 10:17:25 -0500

    feat(qualys-agent-setup): Added qualys agent to squid and utility vm terraform

--- tf_files/aws/modules/squid_auto/cloud.tf
+++ tf_files/aws/modules/squid_auto/cloud.tf
@@ -145,10 +145,10 @@ CLOUD_AUTOMATION="$USER_HOME/cloud-automation"
   # Install qualys agent if the activtion and customer id provided
   if [[ ! -z "${var.activation_id}" ]] || [[ ! -z "${var.customer_id}" ]]; then
     apt install awscli -y
-    aws s3 cp s3://qualys-agentpackage/QualysCloudAgent.rpm ./qualys-cloud-agent.x86_64.rpm
-    sudo rpm -ivh qualys-cloud-agent.x86_64.rpm
-    # Clean up rpm package after install
-    rm qualys-cloud-agent.x86_64.rpm
+    aws s3 cp s3://qualys-agentpackage/QualysCloudAgent.deb ./qualys-cloud-agent.x86_64.deb
+    dpkg -i ./qualys-cloud-agent.x86_64.deb
+    # Clean up deb package after install
+    rm qualys-cloud-agent.x86_64.deb
     sudo /usr/local/qualys/cloud-agent/bin/qualys-cloud-agent.sh ActivationId=${var.activation_id} CustomerId=${var.customer_id}
   fi
 ) > /var/log/bootstrapping_script.log
