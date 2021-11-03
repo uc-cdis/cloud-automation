96920589e0b7a275e779199f2352559cc8a07b48
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2021-11-03 15:22:38 -0500

    fix(squid-certs): Added a little script to remove the deprecated eltsencrypt cert

--- Docker/squid/certfix.sh
+++ Docker/squid/certfix.sh
@@ -3,5 +3,4 @@
 if [[ -z $(cat /etc/ca-certificates.conf | grep '!mozilla/DST_Root_CA_X3.crt') ]] && [[ ! -z $(cat /etc/ca-certificates.conf | grep 'mozilla/DST_Root_CA_X3.crt') ]]; then
   echo /etc/ca-certificates.conf | xargs sed -i 's/mozilla\/DST_Root_CA_X3.crt/!mozilla\/DST_Root_CA_X3.crt/g'
   update-ca-certificates
-  squid -k reconfigure
 fi
