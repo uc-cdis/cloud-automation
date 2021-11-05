dc2ef623c73bc0ba32309efd2e923829d1352bfc
Debra Venckus <dvenckus@uchicago.edu>
2021-11-05 09:48:51 -0500

    Bug Fix: python nginx buster; change echo -e to printf

--- Docker/python-nginx/python3.6-buster/dockerrun.sh
+++ Docker/python-nginx/python3.6-buster/dockerrun.sh
@@ -83,7 +83,7 @@ fi
 # Enable debug flag based on GEN3_DEBUG environment
 #
 if [ -f ./wsgi.py ] && [ "$GEN3_DEBUG" = "True" ]; then
-  echo -e "\napplication.debug=True\n" >> ./wsgi.py
+  printf "\napplication.debug=True\n" >> ./wsgi.py
 fi
 
 if [[ -z $DD_ENABLED ]]; then
