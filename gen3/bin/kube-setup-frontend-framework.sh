da01c5f16f1987d622e8aeb7136bf63617e73a65
Matthew Cannalte <mcannalte@uchicago.edu>
2022-01-28 15:53:21 -0600

    feat(landing):

--- gen3/bin/kube-setup-frontend-framework.sh
+++ gen3/bin/kube-setup-frontend-framework.sh
@@ -3,5 +3,5 @@ gen3_load "gen3/lib/kube-setup-init"
 
 [[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
 
-"${GEN3_HOME}/kube/services/frontend-framework/frontend-framework-service.yaml"
+g3kubectl apply -f "${GEN3_HOME}/kube/services/frontend-framework/frontend-framework-service.yaml"
 gen3 roll frontend-framework
