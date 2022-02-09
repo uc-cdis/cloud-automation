1a49b4d94e851ac86dc709143d3dbdf9f4c64094
Ramu Nerella <ramunerella@Ramus-MacBook-Pro.local>
2022-02-09 14:44:16 -0600

    updated ohif-viewer

--- gen3/bin/kube-setup-orthanc.sh
+++ gen3/bin/kube-setup-orthanc.sh
@@ -2,10 +2,11 @@
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 
-gen3 roll ohif-viewer
-g3kubectl apply -f "${GEN3_HOME}/kube/services/orthanc/ohif-viewer-service.yaml"
+
 gen3 roll orthanc
 g3kubectl apply -f "${GEN3_HOME}/kube/services/orthanc/orthanc-service.yaml"
+gen3 roll ohif-viewer
+g3kubectl apply -f "${GEN3_HOME}/kube/services/orthanc/ohif-viewer-service.yaml"
 
 cat <<EOM
 The dicom-viewer service has been deployed onto the k8s cluster.
