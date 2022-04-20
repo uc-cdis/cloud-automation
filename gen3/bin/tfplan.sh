3dec6275d7a7205bb0eacbeb09d1043c64562cac
Edward Malinowski <edwardmalinowski@Eds-MacBook-Pro.attlocal.net>
2022-04-19 19:23:57 -0500

    feat(tf1x): Added tf1.X base config files

--- gen3/bin/tfplan.sh
+++ gen3/bin/tfplan.sh
@@ -28,7 +28,7 @@ cd $GEN3_WORKDIR
 /bin/rm -f plan.terraform
 
 echo "Running terraform plan $destroyFlag "${targetList[@]}" --var-file ./config.tfvars -out plan.terraform $GEN3_TFSCRIPT_FOLDER/"
-gen3_terraform -chdir="$GEN3_TFSCRIPT_FOLDER/" plan $destroyFlag "${targetList[@]}" --var-file="${GEN3_WORKDIR}/config.tfvars" -out="${GEN3_WORKDIR}/plan.terraform" 2>&1 | tee plan.log
+gen3_terraform plan $destroyFlag "${targetList[@]}" --var-file="${GEN3_WORKDIR}/config.tfvars" -out="${GEN3_WORKDIR}/plan.terraform" 2>&1 | tee plan.log
 let exitCode=${PIPESTATUS[0]}
 if [[ $exitCode -ne 0 ]]; then
   echo -e "${RED_COLOR}non zero exit code from terraform plan: ${exitCode}${DEFAULT_COLOR}"
