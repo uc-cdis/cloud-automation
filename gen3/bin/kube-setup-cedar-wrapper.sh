6a4a7a09d28bdd98c223532b6a1cc3af67f55bdd
Matthew Cannalte <mcannalte@uchicago.edu>
2022-05-12 10:37:58 -0500

    feat(cedar-wrapper-service):

--- gen3/bin/kube-setup-cedar-wrapper.sh
+++ gen3/bin/kube-setup-cedar-wrapper.sh
@@ -3,5 +3,5 @@ gen3_load "gen3/lib/kube-setup-init"
 
 [[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
 
-g3kubectl apply -f "${GEN3_HOME}/kube/services/frontend-framework/frontend-framework-service.yaml"
-gen3 roll frontend-framework
+g3kubectl apply -f "${GEN3_HOME}/kube/services/cedar-wrapper/cedar-wrapper-service.yaml"
+gen3 roll cedar-wrapper
