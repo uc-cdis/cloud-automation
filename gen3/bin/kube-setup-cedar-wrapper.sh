b1b2e9936722612dc82814483f04545a2e30ddd6
Matthew Cannalte <mcannalte@uchicago.edu>
2022-05-12 11:13:19 -0500

    feat(cedar-wrapper-service):

--- gen3/bin/kube-setup-cedar-wrapper.sh
+++ gen3/bin/kube-setup-cedar-wrapper.sh
@@ -3,10 +3,15 @@ gen3_load "gen3/lib/kube-setup-init"
 
 [[ -z "$GEN3_ROLL_ALL" ]] && gen3 kube-setup-secrets
 
+
+cedar_api_key_file="$(gen3_secrets_folder)/g3auto/cedar/apikey.txt"
+
+if [[ ! -f cedar_api_key_file ]]; then
+    gen3_log_err "No CEDAR api key present in ${cedar_api_key_file}"
+else
+    g3kubectl create secret generic "cedar-service-apikey" --from-file=cedar_apikey.txt=${cedar_api_key_file}
+fi
+
 g3kubectl apply -f "${GEN3_HOME}/kube/services/cedar-wrapper/cedar-wrapper-service.yaml"
 gen3 roll cedar-wrapper
 
-
-if [[ -f "$(gen3_secrets_folder)/g3auto/cedar/apikey.txt" ]]; then
-    gen3_log_err "No CEDAR api key present in $(gen3_secrets_folder)/g3auto/cedar/apikey.txt"
-fi
