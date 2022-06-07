9f2dd95225a3a5f66473e5e24d0ad5f6f93e6a3d
Mingfei Shao <mshao1@uchicago.edu>
2022-06-07 15:41:27 -0500

    delete secret before creating

--- gen3/bin/kube-setup-cedar-wrapper.sh
+++ gen3/bin/kube-setup-cedar-wrapper.sh
@@ -9,6 +9,9 @@ cedar_api_key_file="$(gen3_secrets_folder)/cedar_api_key.txt"
 if [[ ! -f "$cedar_api_key_file" ]]; then
     gen3_log_err "No CEDAR api key present in ${cedar_api_key_file}"
 else
+    if g3kubectl get secret cedar-service-api-key > /dev/null 2>&1; then
+        g3kubectl delete secret cedar-service-api-key
+    fi
     g3kubectl create secret generic "cedar-service-api-key" --from-file=cedar_api_key.txt=${cedar_api_key_file}
 fi
 
