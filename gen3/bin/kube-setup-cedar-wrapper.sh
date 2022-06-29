5e28af2a7de911154ab90b32a21899aee22f3479
Mingfei Shao <2475897+mfshao@users.noreply.github.com>
2022-06-29 18:54:34 -0500

    Update kube-setup-cedar-wrapper.sh (#1986)

--- gen3/bin/kube-setup-cedar-wrapper.sh
+++ gen3/bin/kube-setup-cedar-wrapper.sh
@@ -7,14 +7,13 @@ gen3_load "gen3/lib/kube-setup-init"
 cedar_api_key_file="$(gen3_secrets_folder)/cedar_api_key.txt"
 
 if [[ ! -f "$cedar_api_key_file" ]]; then
-    gen3_log_err "No CEDAR api key present in ${cedar_api_key_file}"
+    gen3_log_err "No CEDAR api key present in ${cedar_api_key_file}, not rolling CEDAR wrapper"
 else
     if g3kubectl get secret cedar-service-api-key > /dev/null 2>&1; then
         g3kubectl delete secret cedar-service-api-key
     fi
     g3kubectl create secret generic "cedar-service-api-key" --from-file=cedar_api_key.txt=${cedar_api_key_file}
+    
+    g3kubectl apply -f "${GEN3_HOME}/kube/services/cedar-wrapper/cedar-wrapper-service.yaml"
+    gen3 roll cedar-wrapper
 fi
-
-g3kubectl apply -f "${GEN3_HOME}/kube/services/cedar-wrapper/cedar-wrapper-service.yaml"
-gen3 roll cedar-wrapper
-
