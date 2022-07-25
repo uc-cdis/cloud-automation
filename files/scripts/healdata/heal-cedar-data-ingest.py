119376c21a3ac137bcd1b515fc283f01b5ae6474
Mingfei Shao <mshao1@uchicago.edu>
2022-07-25 15:55:38 -0500

    test

--- files/scripts/healdata/heal-cedar-data-ingest.py
+++ files/scripts/healdata/heal-cedar-data-ingest.py
@@ -8,7 +8,8 @@ import os
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--directory", help="Cedar Directory ID for registering ")
-parser.add_argument("--access_token", help="User access token")\
+parser.add_argument("--access_token", help="User access token")
+parser.add_argument("--hostname", help="Hostname")
 
 
 args = parser.parse_args()
@@ -19,9 +20,13 @@ if not args.directory:
 if not args.access_token:
     print("User access token is required!")
     exit(1)
+if not args.hostname:
+    print("Hostname is required!")
+    exit(1)
 
 dir_id = args.directory
 access_token = args.access_token
+hostname = args.hostname
 
 def none_to_empty_str(items):
     return {k: v if v is not None else '' for k, v in items}
@@ -29,11 +34,12 @@ def none_to_empty_str(items):
 token_header = {"Authorization": 'bearer ' + access_token}
 
 # Get the metadata from cedar to register
-cedar = requests.get(f"http://revproxy-service/cedar/get-instance-by-directory/{dir_id}", headers=token_header)
+print("Querying CEDAR...")
+cedar = requests.get(f"https://{hostname}/cedar/get-instance-by-directory/{dir_id}", headers=token_header)
 
 # If we get metadata back now register with MDS
 if cedar.status_code == 200:
-    print("Successfully got records from cedar directory")
+    print("Successfully got records from CEDAR directory")
 
     metadata_return = cedar.json(object_pairs_hook=none_to_empty_str)
 
@@ -44,7 +50,7 @@ if cedar.status_code == 200:
         cedar_record_id = str(cedar_record["appl_id"])
 
         # Get the metadata record for the nih_application_id
-        mds = requests.get(f"https://revproxy-service/mds/metadata/{cedar_record_id}",
+        mds = requests.get(f"https://{hostname}/mds/metadata/{cedar_record_id}",
             headers=token_header
         )
         if mds.status_code == 200:
@@ -59,7 +65,7 @@ if cedar.status_code == 200:
 
             print("Metadata is now being registered.")
             print(mds_cedar_register)
-            mds_put = requests.put(f"https://revproxy-service/mds/metadata/{cedar_record_id}?merge=True",
+            mds_put = requests.put(f"http://{hostname}/mds/metadata/{cedar_record_id}?merge=True",
                 headers=token_header,
                 json = mds_cedar_register
             )
