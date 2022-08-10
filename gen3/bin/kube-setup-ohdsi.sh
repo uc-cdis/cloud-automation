7aa44671c04bba8916867616e8978a0812c485a2
Andrew Prokhorenkov <aprokh@uchicago.edu>
2022-08-10 18:55:46 -0500

    feat: ingress for kube-setup-ohdsi

--- gen3/bin/kube-setup-ohdsi.sh
+++ gen3/bin/kube-setup-ohdsi.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
+
+setup_ingress() {
+  local hostname=$(gen3 api hostname)
+  certs=$(aws acm list-certificates --certificate-statuses ISSUED | jq --arg hostname $hostname -c '.CertificateSummaryList[] | select(.DomainName | contains("*."+$hostname))')
+  if [ "$certs" = "" ]; then 
+    gen3_log_info "no certs found for *.${hostname}. exiting"
+    exit 22
+  fi
+  gen3_log_info "Found ACM certificate for *.$hostname"
+  export ARN=$(jq -r .CertificateArn <<< $certs)
+  export superset_hostname="atlas.${hostname}"
+  envsubst <${GEN3_HOME}/kube/services/ohdsi/ohdsi-ingress.yaml | g3kubectl apply -f -
+}
+
+setup_ingress
+
+gen3 roll ohdsi-webapi
+g3kubectl apply -f "${GEN3_HOME}/kube/services/ohdsi/ohdsi-webapi-service.yaml"
+gen3 roll ohdsi-atlas
+g3kubectl apply -f "${GEN3_HOME}/kube/services/ohdsi/ohdsi-atlas-service.yaml"
+
+cat <<EOM
+The Atlas/WebAPI service has been deployed onto the k8s cluster.
+EOM
