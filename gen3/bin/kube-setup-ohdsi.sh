7114088b45cbbfdbfbdf4aa97f817fe853211438
Andrew Prokhorenkov <aprokh@uchicago.edu>
2022-08-10 19:03:17 -0500

    fix: proper path

--- gen3/bin/kube-setup-ohdsi.sh
+++ gen3/bin/kube-setup-ohdsi.sh
@@ -19,10 +19,10 @@ setup_ingress() {
 setup_ingress
 
 gen3 roll ohdsi-webapi
-g3kubectl apply -f "${GEN3_HOME}/kube/services/ohdsi/ohdsi-webapi-service.yaml"
+g3kubectl apply -f "${GEN3_HOME}/kube/services/ohdsi-webapi/ohdsi-webapi-service.yaml"
 
 gen3 roll ohdsi-atlas
-g3kubectl apply -f "${GEN3_HOME}/kube/services/ohdsi/ohdsi-atlas-service.yaml"
+g3kubectl apply -f "${GEN3_HOME}/kube/services/ohdsi-atlas/ohdsi-atlas-service.yaml"
 
 cat <<EOM
 The Atlas/WebAPI service has been deployed onto the k8s cluster.
