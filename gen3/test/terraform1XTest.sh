96d86e6ddfc3d76dd3035aa1e9b5793fe92c6bb4
Edward Malinowski <edwardmalinowski@Eds-MacBook-Pro.attlocal.net>
2022-09-26 12:18:14 -0500

    feat(tf1x): Added tf1.X base config files

--- gen3/test/terraform1XTest.sh
+++ gen3/test/terraform1XTest.sh
@@ -257,7 +257,7 @@ test_commons_workspace() {
   [[ "$GEN3_TFSCRIPT_FOLDER" == "$GEN3_HOME/tf_files-1.0/aws/commons" ]]; because $? "a __commons workspace should use the ./aws/commons resources: $GEN3_TFSCRIPT_FOLDER"
   cat - > config.tfvars <<EOM
 vpc_name="testVPC"
-vpc_cidr_block="172.26.0.0/20	"
+vpc_cidr_block="172.26.0.0/20"
 ami_account_id="099720109477"
 users_bucket_name="cdis-gen3-users"
 config_folder="dev"
