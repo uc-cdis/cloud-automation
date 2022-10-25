813427a46a3f62ab201277cd918fe249272df559
EliseCastle23 <elisemcastle@uchicago.edu>
2022-10-25 17:09:06 -0600

    adding configuration files for the other indices and moved into another folder

---
+++ files/scripts/test-indices/create-test-indices.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#create guppy indices with dummy data to test explorer pagekubec
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/gen3setup"
+
+export ESHOST="${ESHOST:-"esproxy-service:9200"}"
+
+#create dev_case index with data and mappings
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/dev_case__mapping.json --output=http://esproxy-service:9200/dev_case --type mapping
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/dev_case__data.json --output=http://esproxy-service:9200/dev_case --type data
+
+#create dev_file index with data and mappings
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/dev_file__mapping.json --output=http://esproxy-service:9200/dev_case --type mapping
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/dev_file__data.json --output=http://esproxy-service:9200/dev_case --type data
+
+#create dev_case-array-config index with data and mappings
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/dev_case-array-config__mapping.json --output=http://esproxy-service:9200/dev_case --type mapping
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/dev_case-array-config__data.json --output=http://esproxy-service:9200/dev_case --type data
