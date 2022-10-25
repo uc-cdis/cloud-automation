f0ba0ca8049b822c280951a669fe19bb044bd7d6
EliseCastle23 <elisemcastle@uchicago.edu>
2022-10-25 15:34:07 -0600

    sourcing and adding additional gen3 commands

--- files/scripts/create-test-indices.sh
+++ files/scripts/create-test-indices.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #
 #create guppy indices with dummy data to test explorer pagekubec
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/gen3setup"
 
 export ESHOST="${ESHOST:-"esproxy-service:9200"}"
 
 #create dev_case index with data and mappings
-gen3 nrun elasticdump --input dev_case__mapping.json --output="${ESHOST}/dev_case" --type mapping
+gen3 nrun elasticdump --input dev_case__mapping.json --output="${ESHOST}/dev_case" --type data
 gen3 nrun elasticdump --input dev_case__data.json --output="${ESHOST}/dev_case" --type data
