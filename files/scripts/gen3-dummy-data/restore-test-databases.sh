d6fd54561c162fe4b2f336a9169d83fbcc1ac689
EliseCastle23 <elisemcastle@uchicago.edu>
2022-11-03 15:17:25 -0600

    removing local files as they are now stored in s3. Starting to develope the job for backing up the dbs.

---
+++ files/scripts/gen3-dummy-data/restore-test-databases.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#backup fake data from db into local postgres for testing
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/gen3setup"
+
+export ESHOST="${ESHOST:-"esproxy-service:9200"}"
+
+indices='dev_case dev_file dev_case-array-config'
+
+#Copy the pg dump files
+aws s3 cp s3://gen3-dummy-data/dbindexd.backup .
+
+#Backup the dbs from the dump files
+psql -h postgres-postgresql.postgres.svc.cluster.local -d indexd -U postgres dbindexd.backup
+
+
+rm db*
