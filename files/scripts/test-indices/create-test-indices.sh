73235bffe58c1564fd0c0a5172c0d72b308383ea
EliseCastle23 <elisemcastle@uchicago.edu>
2022-11-03 10:36:14 -0600

    modified script to utilize s3

--- files/scripts/test-indices/create-test-indices.sh
+++ files/scripts/test-indices/create-test-indices.sh
@@ -6,14 +6,16 @@ gen3_load "gen3/gen3setup"
 
 export ESHOST="${ESHOST:-"esproxy-service:9200"}"
 
-#create dev_case index with data and mappings
-gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/dev_case__mapping.json --output=http://esproxy-service:9200/dev_case --type mapping
-gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/dev_case__data.json --output=http://esproxy-service:9200/dev_case --type data
+indices='dev_case dev_file dev_case-array-config'
 
-#create dev_file index with data and mappings
-gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/dev_file__mapping.json --output=http://esproxy-service:9200/dev_file --type mapping
-gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/dev_file__data.json --output=http://esproxy-service:9200/dev_file --type data
+#Copy all mapping and index files from s3
+aws s3 cp s3://gen3-dummy-data/ . --recursive  
 
-#create dev_case-array-config index with data and mappings
-gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/dev_case-array-config__mapping.json --output=http://esproxy-service:9200/dev_case-array-config --type mapping
-gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/dev_case-array-config__data.json --output=http://esproxy-service:9200/dev_case-array-config --type data
+#Create indices
+for index in indices
+do
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/$index__mapping.json --output=http://esproxy-service:9200/$index --type mapping
+gen3 nrun elasticdump --input /home/ubuntu/cloud-automation/files/scripts/test-indices/$index__data.json --output=http://esproxy-service:9200/$index --type data
+done
+
+rm dev*
