decb9ee918e10e85cddbbe0bca57ab79a32e1c67
EliseCastle23 <elisemcastle@uchicago.edu>
2022-11-04 10:07:46 -0600

    forgot to add "-f" to the end of psql statement

--- files/scripts/gen3-dummy-data/restore-test-databases.sh
+++ files/scripts/gen3-dummy-data/restore-test-databases.sh
@@ -4,14 +4,10 @@
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/gen3setup"
 
-export ESHOST="${ESHOST:-"esproxy-service:9200"}"
-
-indices='dev_case dev_file dev_case-array-config'
-
 #Copy the pg dump files
 aws s3 cp s3://gen3-dummy-data/dbindexd.backup .
 
 #Backup the dbs from the dump files
-psql -h postgres-postgresql.postgres.svc.cluster.local -d indexd -U postgres dbindexd.backup
+psql -h postgres-postgresql.postgres.svc.cluster.local -d indexd -U indexd -f dbindexd.backup
 
 rm db*
