067cdcf20915bc7dba94737f35d117f5534caee3
EliseCastle23 <elisemcastle@uchicago.edu>
2022-12-02 13:27:18 -0700

    fixing the username output

--- gen3/bin/kube-setup-argocd.sh
+++ gen3/bin/kube-setup-argocd.sh
@@ -14,5 +14,5 @@ else
     kubectl apply -f "${GEN3_HOME}/kube/services/argocd/install.yaml" -n argocd
     gen3 kube-setup-revproxy
     export argocdsecret=`kubectl get secret argocd-initial-admin-secret -n argocd -o json | jq .data.password -r | base64 -d` # pragma: allowlist secret
-    gen3_log_info "You can now access the ArgoCD endpoint with the following credentials: Username= Admin and Password= $argocdsecret"
+    gen3_log_info "You can now access the ArgoCD endpoint with the following credentials: Username= admin and Password= $argocdsecret"
 fi
