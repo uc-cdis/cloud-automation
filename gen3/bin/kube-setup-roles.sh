b4a31f972ce343a434dbfa2dd5b06ea9b049e8cf
EliseCastle23 <elisemcastle@uchicago.edu>
2022-12-15 13:12:45 -0700

    forgot to add the namespace var

--- gen3/bin/kube-setup-roles.sh
+++ gen3/bin/kube-setup-roles.sh
@@ -12,6 +12,8 @@ gen3_load "gen3/gen3setup"
 g3kubectl patch serviceaccount default -p 'automountServiceAccountToken: false'
 g3kubectl patch serviceaccount --namespace "$(gen3 jupyter j-namespace)" default -p 'automountServiceAccountToken: false' > /dev/null || true
 
+local namespace="$(gen3 api namespace)"
+
 # Don't do this in a Jenkins job
 if [[ -z "$JENKINS_HOME" ]]; then
   if ! g3kubectl get serviceaccounts/useryaml-job > /dev/null 2>&1; then
