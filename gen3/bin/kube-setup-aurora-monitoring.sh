0c999cc6dfb69492e77f053ccb6a7e4729bebe02
Aidan Hilt <ahilt@uchicago.edu>
2023-02-20 21:44:39 +0000

    Mostly finished now, but we'll need to test the full end-to-end process

--- gen3/bin/kube-setup-aurora-monitoring.sh
+++ gen3/bin/kube-setup-aurora-monitoring.sh
@@ -4,12 +4,13 @@ gen3_load "gen3/gen3setup"
 # Verify that the DB is set up correctly. We'll need to either abort, or fix it ourselves if we can.
 
 # Create the Datadog user
-# TODO figure out how we're doing the password
-# gen3 psql server1 -c "CREATE USER datadog WITH password '<PASSWORD>';"
 datadogPsqlPassword="$(jq -r .datadog_db_password < $(gen3_secrets_folder)/datadog/datadog_db_user.json)"
 if [[ -z "$datadogPsqlPassword" || "null" == "$datadogPsqlPassword" ]]; then
   # Generate a new password for the datadog user in psql
-  datadogPsqlPassword="$(gen3 random)"
+  datadogPsqlPassword=$(random_alphanumeric)
+  echo $datadogPsqlPassword
+
+  exit 0
 
   # update creds.json
   if [ ! -d "$(gen3_secrets_folder)/datadog" ]
@@ -19,6 +20,9 @@ if [[ -z "$datadogPsqlPassword" || "null" == "$datadogPsqlPassword" ]]; then
 
   cp "$GEN3_HOME/kube/services/datadog/datadog_db_user.json" "$(gen3_secrets_folder)/datadog/datadog_db_user.json"
   jq ".datadog_db_password=\"$datadogPsqlPassword\"" "$GEN3_HOME/kube/services/datadog/datadog_db_user.json" > "$(gen3_secrets_folder)/datadog/datadog_db_user.json"
+
+  # Create the Datadog user in the database
+  gen3 psql server1 -c "CREATE USER datadog WITH password '<PASSWORD>';"
 fi
 
 # # Get all of the DBs, so that we can create the necessary components in all of them
@@ -32,7 +36,7 @@ fi
 # databaseArray=($databases)
 
 
-# # Loop through every database, creating the schema and function
+# Loop through every database, creating the schema and function
 
 # for db in "${databaseArray[@]}"
 # do
@@ -69,23 +73,24 @@ fi
 # SQL"
 # done
 
-# # Set up the agent
-# #==============================
+# Set up the agent
+#==============================
 
-# # Get the instances in the Aurora cluster
-#   # We'll take the name of the cluster as the first argument, so we won't need to go digging for that. Instead, we'll just
-#   # pull out connection strings and ports for each instance
+# Get the instances in the Aurora cluster
+  # We'll take the name of the cluster as the first argument, so we won't need to go digging for that. Instead, we'll just
+  # pull out connection strings and ports for each instance
 
 # instances=$(aws rds describe-db-instances --filters "Name=db-cluster-id,Values=$1" --no-paginate | jq '.DBInstances[].Endpoint.Address,.DBInstances[].Endpoint.Port')
 
 # postgresString=""
 # for instance in "${instances[@]}" 
 # do
+#   datadogUserPassword=$(jq .datadog_db_password $(gen3_secrets_folder)/datadog/datadog_db_user.json)
 #   instanceArray=($instance)
-#   postgresString+=$(cat /home/aidan/cloud-automation/kube/services/datadog/postgres.yaml | yq --arg url ${instanceArray[0]} '.instances[0].host = $url' | yq --yaml-output '.instances[0].password = "password"')
+#   postgresString+=$(cat /home/aidan/cloud-automation/kube/services/datadog/postgres.yaml | yq --arg url ${instanceArray[0]} '.instances[0].host = $url' | yq --arg password $datadogUserPassword --yaml-output '.instances[0].password = $password')
 # done
 
 # echo "$(cat kube/services/datadog/values.yaml | yq --arg endpoints "$postgresString" --yaml-output '.datadog.confd."postgres.yaml" = $endpoints')"
 
 
-# # Check that everything is working
+# Check that everything is working
