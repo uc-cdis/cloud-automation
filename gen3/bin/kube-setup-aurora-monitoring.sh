33b6f01757a33f17ef41e979ad185e29b89e739f
Aidan Hilt <ahilt@uchicago.edu>
2023-02-28 15:56:03 +0000

    Cleaned up some debugging items. We'll have to redo the method for connecting to the DBs, we're going to try and read from g3auto instead.

--- gen3/bin/kube-setup-aurora-monitoring.sh
+++ gen3/bin/kube-setup-aurora-monitoring.sh
@@ -5,12 +5,9 @@ gen3_load "gen3/gen3setup"
 
 # Create the Datadog user
 datadogPsqlPassword="$(jq -r .datadog_db_password < $(gen3_secrets_folder)/datadog/datadog_db_user.json)"
-if [[ -z "$datadogPsqlPassword" || "null" == "$datadogPsqlPassword" ]]; then
+if [[ -z "$datadogPsqlPassword" ]]; then
   # Generate a new password for the datadog user in psql
   datadogPsqlPassword=$(random_alphanumeric)
-  echo $datadogPsqlPassword
-
-  exit 0
 
   # update creds.json
   if [ ! -d "$(gen3_secrets_folder)/datadog" ]
@@ -22,56 +19,56 @@ if [[ -z "$datadogPsqlPassword" || "null" == "$datadogPsqlPassword" ]]; then
   jq ".datadog_db_password=\"$datadogPsqlPassword\"" "$GEN3_HOME/kube/services/datadog/datadog_db_user.json" > "$(gen3_secrets_folder)/datadog/datadog_db_user.json"
 
   # Create the Datadog user in the database
-  gen3 psql server1 -c "CREATE USER datadog WITH password '<PASSWORD>';"
+  gen3 psql server1 -c "CREATE USER datadog WITH password $datadogPsqlPassword;"
 fi
 
-# # Get all of the DBs, so that we can create the necessary components in all of them
-# databases=$(gen3 psql server1 -c "SELECT datname FROM pg_database;")
-# databases=$(echo $databases | cut -c 47-)
-# # For an explanation of this one, it uses parameter expansion: https://mywiki.wooledge.org/BashGuide/Parameters#Parameter_Expansion
-# # This matches the pattern, (* rows), against the end of the databases string, and deletes the shortest match
-# databases="${databases%(* rows)}"
+# Get all of the DBs, so that we can create the necessary components in all of them
+databases=$(gen3 psql server1 -c "SELECT datname FROM pg_database;")
+databases=$(echo $databases | cut -c 47-)
+# For an explanation of this one, it uses parameter expansion: https://mywiki.wooledge.org/BashGuide/Parameters#Parameter_Expansion
+# This matches the pattern, (* rows), against the end of the databases string, and deletes the shortest match
+databases="${databases%(* rows)}"
 
-# # Now we turn it into an array
-# databaseArray=($databases)
+# Now we turn it into an array
+databaseArray=($databases)
 
 
 # Loop through every database, creating the schema and function
 
-# for db in "${databaseArray[@]}"
-# do
-#   echo "gen3 psql server1 -d $db -t  <<SQL |
-#     CREATE SCHEMA datadog; 
-#       GRANT USAGE ON SCHEMA datadog TO datadog; 
-#       GRANT USAGE ON SCHEMA public TO datadog;
-#       GRANT pg_monitor TO datadog;
-#       CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-# SQL"
+for db in "${databaseArray[@]}"
+do
+  echo "gen3 psql server1 -d $db -t  <<SQL |
+    CREATE SCHEMA datadog; 
+      GRANT USAGE ON SCHEMA datadog TO datadog; 
+      GRANT USAGE ON SCHEMA public TO datadog;
+      GRANT pg_monitor TO datadog;
+      CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+SQL"
 
-#   echo "gen3 psql server1 -d $DB -t <<SQL |
-#     CREATE OR REPLACE FUNCTION datadog.explain_statement(
-#       l_query TEXT,
-#       OUT explain JSON
-#     )
+  echo "gen3 psql server1 -d $DB -t <<SQL |
+    CREATE OR REPLACE FUNCTION datadog.explain_statement(
+      l_query TEXT,
+      OUT explain JSON
+    )
    
-#     RETURNS SETOF JSON AS
-#     $$
-#     DECLARE
-#     curs REFCURSOR;
-#     plan JSON;
+    RETURNS SETOF JSON AS
+    $$
+    DECLARE
+    curs REFCURSOR;
+    plan JSON;
 
-#     BEGIN
-#       OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
-#       FETCH curs INTO plan;
-#       CLOSE curs;
-#       RETURN QUERY SELECT plan;
-#     END;
-#     $$
-#     LANGUAGE 'plpgsql'
-#     RETURNS NULL ON NULL INPUT
-#     SECURITY DEFINER;
-# SQL"
-# done
+    BEGIN
+      OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
+      FETCH curs INTO plan;
+      CLOSE curs;
+      RETURN QUERY SELECT plan;
+    END;
+    $$
+    LANGUAGE 'plpgsql'
+    RETURNS NULL ON NULL INPUT
+    SECURITY DEFINER;
+SQL"
+done
 
 # Set up the agent
 #==============================
@@ -80,17 +77,17 @@ fi
   # We'll take the name of the cluster as the first argument, so we won't need to go digging for that. Instead, we'll just
   # pull out connection strings and ports for each instance
 
-# instances=$(aws rds describe-db-instances --filters "Name=db-cluster-id,Values=$1" --no-paginate | jq '.DBInstances[].Endpoint.Address,.DBInstances[].Endpoint.Port')
+instances=$(aws rds describe-db-instances --filters "Name=db-cluster-id,Values=$1" --no-paginate | jq '.DBInstances[].Endpoint.Address,.DBInstances[].Endpoint.Port')
 
-# postgresString=""
-# for instance in "${instances[@]}" 
-# do
-#   datadogUserPassword=$(jq .datadog_db_password $(gen3_secrets_folder)/datadog/datadog_db_user.json)
-#   instanceArray=($instance)
-#   postgresString+=$(cat /home/aidan/cloud-automation/kube/services/datadog/postgres.yaml | yq --arg url ${instanceArray[0]} '.instances[0].host = $url' | yq --arg password $datadogUserPassword --yaml-output '.instances[0].password = $password')
-# done
+postgresString=""
+for instance in "${instances[@]}" 
+do
+  datadogUserPassword=$(jq .datadog_db_password $(gen3_secrets_folder)/datadog/datadog_db_user.json)
+  instanceArray=($instance)
+  postgresString+=$(cat /home/aidan/cloud-automation/kube/services/datadog/postgres.yaml | yq --arg url ${instanceArray[0]} '.instances[0].host = $url' | yq --arg password $datadogUserPassword --yaml-output '.instances[0].password = $password')
+done
 
-# echo "$(cat kube/services/datadog/values.yaml | yq --arg endpoints "$postgresString" --yaml-output '.datadog.confd."postgres.yaml" = $endpoints')"
+echo "$(cat kube/services/datadog/values.yaml | yq --arg endpoints "$postgresString" --yaml-output '.datadog.confd."postgres.yaml" = $endpoints')"
 
 
 # Check that everything is working
