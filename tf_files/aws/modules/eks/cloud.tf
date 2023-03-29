72ad3b2f34b0140d715cdbe0f60066d0a316562f
EliseCastle23 <elisemcastle@uchicago.edu>
2023-03-29 14:42:11 -0600

    using the fips enabled ami for the jupyter and workflow nodepools

--- tf_files/aws/modules/eks/cloud.tf
+++ tf_files/aws/modules/eks/cloud.tf
@@ -37,6 +37,7 @@ module "jupyter_pool" {
   nodepool_asg_min_size         = "${var.jupyter_asg_min_size}"
   activation_id                = "${var.activation_id}"
   customer_id                  = "${var.customer_id}"
+  fips_enabled_ami             = "${local.ami}"
 }
 
 module "workflow_pool" {
@@ -62,6 +63,7 @@ module "workflow_pool" {
   nodepool_asg_min_size         = "${var.workflow_asg_min_size}"
   activation_id                = "${var.activation_id}"
   customer_id                  = "${var.customer_id}"
+  fips_enabled_ami             = "${local.ami}"
 }
 
 
