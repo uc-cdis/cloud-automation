ee5e3ff178dd70bd87a5487f9d980b0a08c1ec41
EliseCastle23 <elisemcastle@uchicago.edu>
2023-04-24 11:04:22 -0600

    trying a different setup

--- flavors/squid_auto/squid_running_on_docker.sh
+++ flavors/squid_auto/squid_running_on_docker.sh
@@ -20,7 +20,7 @@ SQUID_CONFIG_DIR="/etc/squid"
 SQUID_LOGS_DIR="/var/log/squid"
 SQUID_CACHE_DIR="/var/cache/squid"
 SQUID_PID_DIR="/var/run/squid"
-SQUID_IMAGE_TAG="squid-5.3" #"feat_ha-squid"
+SQUID_IMAGE_TAG="master" #"feat_ha-squid"
 #SQUID_VERSION="squid-4.14"
 
 HOSTNAME=$(command -v hostname)
