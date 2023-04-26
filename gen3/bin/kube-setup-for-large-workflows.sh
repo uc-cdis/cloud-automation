4bdbfecfbb1b4bac227a734d774836ea2ed0bf7b
Aidan Hilt <11202897+AidanHilt@users.noreply.github.com>
2023-04-26 11:46:40 -0400

    Update kube-setup-for-large-workflows.sh

--- gen3/bin/kube-setup-for-large-workflows.sh
+++ gen3/bin/kube-setup-for-large-workflows.sh
@@ -7,15 +7,15 @@ kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op
 
 # Add options to the command for the container, if they are not already present
 if ! kubectl get deployment cluster-autoscaler -n kube-system -o jsonpath='{.spec.template.spec.containers[0].command}' | yq eval '.[]' | grep -q -- '--scale-down-delay-after-delete=2m'; then
-  kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--scale-down-delay-after-delete=2m"}]'
+  kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--scale-down-delay-after-delete=1m"}]'
 else
   echo "Flag --scale-down-delay-after-delete=1m already present"
 fi
 
 if ! kubectl get deployment cluster-autoscaler -n kube-system -o jsonpath='{.spec.template.spec.containers[0].command}' | yq eval '.[]' | grep -q -- '--scale-down-unneeded-time=2m'; then
-  kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--scale-down-unneeded-time=2m"}]'
+  kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--scale-down-unneeded-time=1m"}]'
 else
-  echo "Flag --scale-down-unneeded-time=2m already present"
+  echo "Flag --scale-down-unneeded-time=1m already present"
 fi
 
 if ! kubectl get deployment cluster-autoscaler -n kube-system -o jsonpath='{.spec.template.spec.containers[0].command}' | yq eval '.[]' | grep -q -- '--scan-interval=60s'; then
