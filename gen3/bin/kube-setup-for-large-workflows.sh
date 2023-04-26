3e57d01a6a516973a8f353cfe30e46b0872aac74
Aidan Hilt <ahilt@uchicago.edu>
2023-04-26 14:51:30 +0000

    Increased the memory limits and requests further for Prometheus, added in values for opencost optimizied for EKS, and further decreased the number of coredns pods.

--- gen3/bin/kube-setup-for-large-workflows.sh
+++ gen3/bin/kube-setup-for-large-workflows.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Set the resources block for the deployment
+# Set the resources block for the autoscaler deployment
 kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/resources", "value": {"limits":{"cpu":"6","memory":"30Gi"},"requests":{"cpu":"1","memory":"4Gi"}}}]'
+# Add PriorityClass to the cluster autoscaler pod
+kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/priorityClassName", "value": "system-node-critical"}]'
 
 # Add options to the command for the container, if they are not already present
 if ! kubectl get deployment cluster-autoscaler -n kube-system -o jsonpath='{.spec.template.spec.containers[0].command}' | yq eval '.[]' | grep -q -- '--scale-down-delay-after-delete=2m'; then
   kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--scale-down-delay-after-delete=2m"}]'
 else
-  echo "Flag --scale-down-delay-after-delete=2m already present"
+  echo "Flag --scale-down-delay-after-delete=1m already present"
 fi
 
 if ! kubectl get deployment cluster-autoscaler -n kube-system -o jsonpath='{.spec.template.spec.containers[0].command}' | yq eval '.[]' | grep -q -- '--scale-down-unneeded-time=2m'; then
@@ -19,8 +21,13 @@ fi
 if ! kubectl get deployment cluster-autoscaler -n kube-system -o jsonpath='{.spec.template.spec.containers[0].command}' | yq eval '.[]' | grep -q -- '--scan-interval=60s'; then
   kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--scan-interval=60s"}]'
 else
-  echo "Flag --scan-interval=60s already present"
+  echo "Flag --scan-interval=30s already present"
 fi
 
-# Add PriorityClass to the pod
-kubectl patch deployment cluster-autoscaler -n kube-system --type=json -p='[{"op": "add", "path": "/spec/template/spec/priorityClassName", "value": "system-node-critical"}]'
+#Scale down coredns
+kubectl patch configmap kube-dns-autoscaler -n kube-system --type merge -p '{"data":{"linear":"{\"coresPerReplica\":768,\"nodesPerReplica\":16,\"preventSinglePointFailure\":true}"}}'
+
+#Set resources for kubecost-prometheus and kubecost-cost-analyzer
+kubectl patch deployment kubecost-cost-analyzer -n kubecost --type=json -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/resources", "value": {"limits":{"cpu":"8","memory":"64Gi"},"requests":{"cpu":"7","memory":"64Gi"}}}]'
+
+kubectl patch deployment kubecost-prometheus -n kubecost --type=json -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/resources", "value": {"limits":{"cpu":"8","memory":"64Gi"},"requests":{"cpu":"7","memory":"64Gi"}}}]'
