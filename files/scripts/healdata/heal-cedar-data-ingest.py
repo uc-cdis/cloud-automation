374033fd629bd8599c288a23abaf458123600295
Michael Lukowski <lukowski9@gmail.com>
2023-05-04 13:20:14 -0500

    update script to fix mds call

--- files/scripts/healdata/heal-cedar-data-ingest.py
+++ files/scripts/healdata/heal-cedar-data-ingest.py
@@ -132,6 +132,9 @@ while((limit + offset <= total)):
                 elif mds_res["_guid_type"] == "unregistered_discovery_metadata":
                     print("Metadata is has not been registered. Registering it in MDS record")
                     continue
+
+                cedar_record_id = mds_res["gen3_discovery"]["_hdp_uid"]
+
                 pydash.merge(mds_discovery_data_body, mds_res["gen3_discovery"], cedar_record)
                 mds_discovery_data_body = update_filter_metadata(mds_discovery_data_body)
                 mds_cedar_register_data_body["gen3_discovery"] = mds_discovery_data_body
