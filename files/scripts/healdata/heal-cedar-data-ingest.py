5f87e82257f45acf82e3e3c9cbf8b69a8ae0fb04
Michael Lukowski <lukowski9@gmail.com>
2023-05-30 15:09:52 -0500

    HP-1117 Feat/cedar ingest update (#2232)
    * updating cedar ingestion script

--- files/scripts/healdata/heal-cedar-data-ingest.py
+++ files/scripts/healdata/heal-cedar-data-ingest.py
@@ -79,53 +79,82 @@ hostname = args.hostname
 
 token_header = {"Authorization": 'bearer ' + access_token}
 
-# Get the metadata from cedar to register
-print("Querying CEDAR...")
-cedar = requests.get(f"http://revproxy-service/cedar/get-instance-by-directory/{dir_id}", headers=token_header)
+limit = 10
+offset = 0
 
-# If we get metadata back now register with MDS
-if cedar.status_code == 200:
-    metadata_return = cedar.json()
-    if "metadata" not in metadata_return:
-        print("Got 200 from CEDAR wrapper but no metadata in body, something is not right!")
-        sys.exit(1)
+# initalize this to be bigger than our inital call so we can go through while loop
+total = 100
 
-    print(f"Successfully got {len(metadata_return['metadata'])} record(s) from CEDAR directory")
-    for cedar_record in metadata_return["metadata"]:
-        if "appl_id" not in cedar_record:
-            print("This record doesn't have appl_id, skipping...")
-            continue
-        cedar_record_id = str(cedar_record["appl_id"])
+while((limit + offset <= total)):
+    # Get the metadata from cedar to register
+    print("Querying CEDAR...")
+    cedar = requests.get(f"http://revproxy-service/cedar/get-instance-by-directory/{dir_id}?limit={limit}&offset={offset}", headers=token_header)
 
-        # Get the metadata record for the nih_application_id
-        mds = requests.get(f"http://revproxy-service/mds/metadata/{cedar_record_id}",
-            headers=token_header
-        )
-        if mds.status_code == 200:
-            mds_res = mds.json()
-            mds_cedar_register_data_body = {}
-            mds_discovery_data_body = {}
-            if mds_res["_guid_type"] == "discovery_metadata":
-                print("Metadata is already registered. Updating MDS record")
-            elif mds_res["_guid_type"] == "unregistered_discovery_metadata":
-                print("Metadata is has not been registered. Registering it in MDS record")
+    # If we get metadata back now register with MDS
+    if cedar.status_code == 200:
+        metadata_return = cedar.json()
+        if "metadata" not in metadata_return:
+            print("Got 200 from CEDAR wrapper but no metadata in body, something is not right!")
+            sys.exit(1)
+
+        total = metadata_return["metadata"]["totalCount"]
+        returned_records = len(metadata_return["metadata"]["records"])
+        print(f"Successfully got {returned_records} record(s) from CEDAR directory")
+        for cedar_record in metadata_return["metadata"]["records"]:
+            if "appl_id" not in cedar_record:
+                print("This record doesn't have appl_id, skipping...")
                 continue
-            pydash.merge(mds_discovery_data_body, mds_res["gen3_discovery"], cedar_record)
-            mds_discovery_data_body = update_filter_metadata(mds_discovery_data_body)
-            mds_cedar_register_data_body["gen3_discovery"] = mds_discovery_data_body
-            mds_cedar_register_data_body["_guid_type"] = "discovery_metadata"
 
-            print("Metadata is now being registered.")
-            mds_put = requests.put(f"http://revproxy-service/mds/metadata/{cedar_record_id}",
-                headers=token_header,
-                json = mds_cedar_register_data_body
-            )
-            if mds_put.status_code == 200:
-                print(f"Successfully registered: {cedar_record_id}")
+            # get the appl id from cedar for querying in our MDS
+            cedar_appl_id = str(cedar_record["appl_id"])
+
+            # Get the metadata record for the nih_application_id
+            mds = requests.get(f"http://revproxy-service/mds/metadata?gen3_discovery.appl_id={cedar_appl_id}&data=true")
+            if mds.status_code == 200:
+                mds_res = mds.json()
+
+                # the query result key is the record of the metadata. If it doesn't return anything then our query failed.
+                if len(list(mds_res.keys())) == 0 or len(list(mds_res.keys())) > 1:
+                    print("Query returned nothing for ", cedar_appl_id, "appl id")
+                    continue
+
+                # get the key for our mds record
+                cedar_record_id = list(mds_res.keys())[0]
+
+                mds_res = mds_res[cedar_record_id]
+                mds_cedar_register_data_body = {}
+                mds_discovery_data_body = {}
+                if mds_res["_guid_type"] == "discovery_metadata":
+                    print("Metadata is already registered. Updating MDS record")
+                elif mds_res["_guid_type"] == "unregistered_discovery_metadata":
+                    print("Metadata is has not been registered. Registering it in MDS record")
+                    continue
+
+                pydash.merge(mds_discovery_data_body, mds_res["gen3_discovery"], cedar_record)
+                mds_discovery_data_body = update_filter_metadata(mds_discovery_data_body)
+                mds_cedar_register_data_body["gen3_discovery"] = mds_discovery_data_body
+                mds_cedar_register_data_body["_guid_type"] = "discovery_metadata"
+
+                print("Metadata is now being registered.")
+                mds_put = requests.put(f"http://revproxy-service/mds/metadata/{cedar_record_id}",
+                    headers=token_header,
+                    json = mds_cedar_register_data_body
+                )
+                if mds_put.status_code == 200:
+                    print(f"Successfully registered: {cedar_record_id}")
+                else:
+                    print(f"Failed to register: {cedar_record_id}. Might not be MDS admin")
+                    print(f"Status from MDS: {mds_put.status_code}")
             else:
-                print(f"Failed to register: {cedar_record_id}. Might not be MDS admin")
-                print(f"Status from MDS: {mds_put.status_code}")
-        else:
-            print(f"Failed to get information from MDS: {mds.status_code}")
+                print(f"Failed to get information from MDS: {mds.status_code}")
+
+    if offset + limit == total:
+        break
+
+    offset = offset + limit
+    if (offset + limit) > total:
+        limit = (offset + limit) - total
+
+
 else:
     print(f"Failed to get information from CEDAR wrapper service: {cedar.status_code}")
