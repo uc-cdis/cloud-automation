21a2148135cfbcc7d50ac79f448089c220ebd1d8
Michael Lukowski <lukowski9@gmail.com>
2023-07-06 14:44:12 -0500

    updateing cedar records in ingestion job

--- files/scripts/healdata/heal-cedar-data-ingest.py
+++ files/scripts/healdata/heal-cedar-data-ingest.py
@@ -102,12 +102,22 @@ while((limit + offset <= total)):
         returned_records = len(metadata_return["metadata"]["records"])
         print(f"Successfully got {returned_records} record(s) from CEDAR directory")
         for cedar_record in metadata_return["metadata"]["records"]:
-            if "nih_application_id" not in cedar_record["Metadata Location"]:
+            updated_cedar_record = {}
+            keys = cedar_record.keys()
+            for key in keys:
+                if " " in key:
+                    new_key = key.replace(" ", "_").lower()
+                    updated_cedar_record[new_key] = cedar_record[key]
+                else:
+                    updated_cedar_record[key.lower()] = cedar_record[key]
+            cedar_record = updated_cedar_record
+
+            if "appl_id" not in cedar_record:
                 print("This record doesn't have appl_id, skipping...")
                 continue
 
             # get the appl id from cedar for querying in our MDS
-            cedar_appl_id = str(cedar_record["Metadata Location"]["nih_application_id"])
+            cedar_appl_id = str(cedar_record["appl_id"])
 
             # Get the metadata record for the nih_application_id
             mds = requests.get(f"http://revproxy-service/mds/metadata?gen3_discovery.study_metadata.Metadata Location.nih_application_id={cedar_appl_id}&data=true")
