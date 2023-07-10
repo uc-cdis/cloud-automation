12e5709cf908836d40467e4222de4dd9777b3a6c
Mingfei Shao <mshao1@uchicago.edu>
2023-07-10 16:59:51 -0500

    revert test

--- files/scripts/healdata/heal-cedar-data-ingest.py
+++ files/scripts/healdata/heal-cedar-data-ingest.py
@@ -179,17 +179,16 @@ while((limit + offset <= total)):
 
                 mds_cedar_register_data_body["_guid_type"] = "discovery_metadata"
 
-                if mds_record_guid == "HDP01065":
-                    print(f"Metadata {mds_record_guid} is now being registered.")
-                    mds_put = requests.put(f"http://revproxy-service/mds/metadata/{mds_record_guid}",
-                        headers=token_header,
-                        json = mds_cedar_register_data_body
-                    )
-                    if mds_put.status_code == 200:
-                        print(f"Successfully registered: {mds_record_guid}")
-                    else:
-                        print(f"Failed to register: {mds_record_guid}. Might not be MDS admin")
-                        print(f"Status from MDS: {mds_put.status_code}")
+                print(f"Metadata {mds_record_guid} is now being registered.")
+                mds_put = requests.put(f"http://revproxy-service/mds/metadata/{mds_record_guid}",
+                    headers=token_header,
+                    json = mds_cedar_register_data_body
+                )
+                if mds_put.status_code == 200:
+                    print(f"Successfully registered: {mds_record_guid}")
+                else:
+                    print(f"Failed to register: {mds_record_guid}. Might not be MDS admin")
+                    print(f"Status from MDS: {mds_put.status_code}")
             else:
                 print(f"Failed to get information from MDS: {mds.status_code}")
     
