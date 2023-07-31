02ab503ee6fcaae30f20c67a7b7d27c6e9aa22e3
Michael Lukowski <lukowski9@gmail.com>
2023-07-31 16:20:09 -0500

    HP-1083 Feat/cedar mds update (#2268)
    * updating cedar ingest for new MDS format for HEAL
    
    * fix for upper level metadata
    
    * change where appl_id comes from in cedar ingestion
    
    * testing alt appl_id
    
    * fix appl id again
    
    * update appl_id check
    
    * add debug statement for cedar ingestion
    
    * remove debug statements and finalize
    
    * updateing cedar records in ingestion job
    
    * test (#2281)
    
    * test
    
    * update
    
    * test
    
    * test
    
    * update
    
    * test
    
    * fix
    
    * fix
    
    * test
    
    * fix
    
    * update
    
    * update
    
    * final
    
    * update
    
    * test
    
    * revert test
    
    * fix
    
    * test
    
    * debug
    
    * revert debug
    
    * fix: dups
    
    * Created a setup script to install argo events, as well as any resourc… (#2287)
    
    * Created a setup script to install argo events, as well as any resources used by it. For now, all the resources are for Argo Workflows
    
    * Moving from using the public eventbus demo to a local copy, so that updates to the documentation don't break our setup
    
    * back to master
    
    ---------
    
    Co-authored-by: Mingfei Shao <2475897+mfshao@users.noreply.github.com>
    Co-authored-by: Mingfei Shao <mshao1@uchicago.edu>
    Co-authored-by: Aidan Hilt <11202897+AidanHilt@users.noreply.github.com>

--- files/scripts/healdata/heal-cedar-data-ingest.py
+++ files/scripts/healdata/heal-cedar-data-ingest.py
@@ -1,15 +1,17 @@
 import argparse
+import json
 import sys
 import requests
 import pydash
+from uuid import UUID
 
 # Defines how a field in metadata is going to be mapped into a key in filters
 FILTER_FIELD_MAPPINGS = {
-    "Study Type.study_stage": "Study Type",
-    "Data.data_type": "Data Type",
-    "Study Type.study_subject_type": "Subject Type",
-    "Human Subject Applicability.gender_applicability": "Gender",
-    "Human Subject Applicability.age_applicability": "Age"
+    "study_metadata.study_type.study_stage": "Study Type",
+    "study_metadata.data.data_type": "Data Type",
+    "study_metadata.study_type.study_subject_type": "Subject Type",
+    "study_metadata.human_subject_applicability.gender_applicability": "Gender",
+    "study_metadata.human_subject_applicability.age_applicability": "Age"
 }
 
 # Defines how to handle special cases for values in filters
@@ -31,8 +33,29 @@ SPECIAL_VALUE_MAPPINGS = {
 
 # Defines field that we don't want to include in the filters
 OMITTED_VALUES_MAPPING = {
-    "Human Subject Applicability.gender_applicability": "Not applicable"
+    "study_metadata.human_subject_applicability.gender_applicability": "Not applicable"
 }
+
+def is_valid_uuid(uuid_to_test, version=4):
+    """
+    Check if uuid_to_test is a valid UUID.
+    
+     Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+    
+     Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+    
+    """
+    
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
 
 def update_filter_metadata(metadata_to_update):
     filter_metadata = []
@@ -82,8 +105,12 @@ token_header = {"Authorization": 'bearer ' + access_token}
 limit = 10
 offset = 0
 
-# initalize this to be bigger than our inital call so we can go through while loop
+# initialize this to be bigger than our initial call so we can go through while loop
 total = 100
+
+if not is_valid_uuid(dir_id):
+    print("Directory ID is not in UUID format!")
+    sys.exit(1)
 
 while((limit + offset <= total)):
     # Get the metadata from cedar to register
@@ -101,60 +128,81 @@ while((limit + offset <= total)):
         returned_records = len(metadata_return["metadata"]["records"])
         print(f"Successfully got {returned_records} record(s) from CEDAR directory")
         for cedar_record in metadata_return["metadata"]["records"]:
-            if "appl_id" not in cedar_record:
+            # get the appl id from cedar for querying in our MDS
+            cedar_appl_id = pydash.get(cedar_record, "metadata_location.nih_application_id")
+            if cedar_appl_id is None:
                 print("This record doesn't have appl_id, skipping...")
                 continue
 
-            # get the appl id from cedar for querying in our MDS
-            cedar_appl_id = str(cedar_record["appl_id"])
-
             # Get the metadata record for the nih_application_id
-            mds = requests.get(f"http://revproxy-service/mds/metadata?gen3_discovery.appl_id={cedar_appl_id}&data=true")
+            mds = requests.get(f"http://revproxy-service/mds/metadata?gen3_discovery.study_metadata.metadata_location.nih_application_id={cedar_appl_id}&data=true")
             if mds.status_code == 200:
                 mds_res = mds.json()
 
                 # the query result key is the record of the metadata. If it doesn't return anything then our query failed.
                 if len(list(mds_res.keys())) == 0 or len(list(mds_res.keys())) > 1:
-                    print("Query returned nothing for ", cedar_appl_id, "appl id")
+                    print("Query returned nothing for", cedar_appl_id, "appl id")
                     continue
 
                 # get the key for our mds record
-                cedar_record_id = list(mds_res.keys())[0]
+                mds_record_guid = list(mds_res.keys())[0]
 
-                mds_res = mds_res[cedar_record_id]
-                mds_cedar_register_data_body = {}
+                mds_res = mds_res[mds_record_guid]
+                mds_cedar_register_data_body = {**mds_res}
                 mds_discovery_data_body = {}
+                mds_clinical_trials = {}
                 if mds_res["_guid_type"] == "discovery_metadata":
                     print("Metadata is already registered. Updating MDS record")
                 elif mds_res["_guid_type"] == "unregistered_discovery_metadata":
-                    print("Metadata is has not been registered. Registering it in MDS record")
-                    continue
+                    print("Metadata has not been registered. Registering it in MDS record")
 
-                pydash.merge(mds_discovery_data_body, mds_res["gen3_discovery"], cedar_record)
-                mds_discovery_data_body = update_filter_metadata(mds_discovery_data_body)
+                if "clinicaltrials_gov" in cedar_record:
+                    mds_clinical_trials = cedar_record["clinicaltrials_gov"]
+                    del cedar_record["clinicaltrials_gov"]
+
+                # some special handing for this field, because its parent will be deleted before we merging the CEDAR and MDS SLMD to avoid duplicated values
+                cedar_record_other_study_websites = cedar_record.get("metadata_location", {}).get("other_study_websites", [])
+                del cedar_record["metadata_location"]
+
+                mds_res["gen3_discovery"]["study_metadata"].update(cedar_record)
+                mds_res["gen3_discovery"]["study_metadata"]["metadata_location"]["other_study_websites"] = cedar_record_other_study_websites
+
+                # merge data from cedar that is not study level metadata into a level higher
+                deleted_keys = []
+                for key, value in mds_res["gen3_discovery"]["study_metadata"].items():
+                    if not isinstance(value, dict):
+                        mds_res["gen3_discovery"][key] = value
+                        deleted_keys.append(key)
+                for key in deleted_keys:
+                    del mds_res["gen3_discovery"]["study_metadata"][key]
+
+                mds_discovery_data_body = update_filter_metadata(mds_res["gen3_discovery"])
+
                 mds_cedar_register_data_body["gen3_discovery"] = mds_discovery_data_body
+                if mds_clinical_trials:
+                    mds_cedar_register_data_body["clinicaltrials_gov"] = {**mds_cedar_register_data_body.get("clinicaltrials_gov", {}), **mds_clinical_trials}
+
                 mds_cedar_register_data_body["_guid_type"] = "discovery_metadata"
 
-                print("Metadata is now being registered.")
-                mds_put = requests.put(f"http://revproxy-service/mds/metadata/{cedar_record_id}",
+                print(f"Metadata {mds_record_guid} is now being registered.")
+                mds_put = requests.put(f"http://revproxy-service/mds/metadata/{mds_record_guid}",
                     headers=token_header,
                     json = mds_cedar_register_data_body
                 )
                 if mds_put.status_code == 200:
-                    print(f"Successfully registered: {cedar_record_id}")
+                    print(f"Successfully registered: {mds_record_guid}")
                 else:
-                    print(f"Failed to register: {cedar_record_id}. Might not be MDS admin")
+                    print(f"Failed to register: {mds_record_guid}. Might not be MDS admin")
                     print(f"Status from MDS: {mds_put.status_code}")
             else:
                 print(f"Failed to get information from MDS: {mds.status_code}")
+    
+    else:
+        print(f"Failed to get information from CEDAR wrapper service: {cedar.status_code}")
 
     if offset + limit == total:
         break
 
     offset = offset + limit
     if (offset + limit) > total:
-        limit = (offset + limit) - total
-
-
-else:
-    print(f"Failed to get information from CEDAR wrapper service: {cedar.status_code}")
+        limit = total - offset
