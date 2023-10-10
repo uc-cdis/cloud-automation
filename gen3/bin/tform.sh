e4975de9799f1e3360284c3155dad35728314761
Edward Malinowski <edwardmalinowski@Eds-MacBook-Pro.attlocal.net>
2023-10-10 10:04:31 -0500

    fixed tform for migration

--- gen3/bin/tform.sh
+++ gen3/bin/tform.sh
@@ -4,20 +4,6 @@ gen3_load "gen3/lib/terraform"
 
 
 declare -a commandtList=()
-if [[ -z "$GEN3_SOURCE_ONLY" ]]; then
-  for flag in $@; do
-    gen3_log_info "$flag"
-    case "$flag" in
-      --tf1)
-        USE_TF_1="True"
-        shift
-        ;;
-      *)
-        commandList+=( "$1" )
-        shift
-    esac
-  done
-fi
 
 if [[ ! -z $USE_TF_1 ]]; then
   gen3_terraform -chdir="$GEN3_TFSCRIPT_FOLDER/" "$@"
