887d916b185cd6826fe7fc99c84ab1d79ccf5dab
MaribelleHGomez <maribellehgomez@gmail.com>
2023-10-12 14:50:43 -0500

    modified job setup for dataservice add indexing job yaml

--- gen3/bin/kube-setup-data-replicate.sh
+++ gen3/bin/kube-setup-data-replicate.sh
@@ -10,20 +10,17 @@ gen3_load "gen3/lib/kube-setup-init"
 secret_folder="$(gen3_secrets_folder)"
 
 if [[ -d ${secret_folder}/apis_configs/dcf_dataservice ]]; then
-  if g3kubectl get secret dcf-aws-creds-secret; then
-    g3kubectl delete secret dcf-aws-creds-secret
+  if g3kubectl get secret aws-creds-secret; then
+    g3kubectl delete secret aws-creds-secret
   fi
   if g3kubectl get secret google-creds-secret; then
     g3kubectl delete secret google-creds-secret
   fi
-  if g3kubectl get secret dcf-dataservice-json-secret; then
-    g3kubectl delete secret dcf-dataservice-json-secret
+  if g3kubectl get secret dataservice-settings-secrets; then
+    g3kubectl delete secret dataservice-settings-secrets
   fi
   if g3kubectl get secret dcf-dataservice-settings-secrets; then
     g3kubectl delete secret dcf-dataservice-settings-secrets
-  fi
-  if g3kubectl get configmap project-map-manifest; then
-    g3kubectl delete configmap project-map-manifest
   fi
  
   if ! hostname="$(gen3 api hostname)"; then
@@ -31,26 +28,13 @@ if [[ -d ${secret_folder}/apis_configs/dcf_dataservice ]]; then
     return 1
   fi
 
-  cp ${GEN3_MANIFEST_HOME}/${hostname}/manifests/datarefresh/GDC_project_map.json ${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json
+  cat >${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings.json << EOL
 
-  GDC_TOKEN=$(cat ${secret_folder}/apis_configs/dcf_dataservice/creds.json | jq '.GDC_TOKEN')
-  INDEXD_CRED=$(cat ${secret_folder}/apis_configs/dcf_dataservice/creds.json | jq '.INDEXD')
-
-  cat >${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings << EOL
-TOKEN = ${GDC_TOKEN}
-  
-INDEXD = ${INDEXD_CRED}
-
-DATA_ENDPT = "https://api.gdc.cancer.gov/data/"
-
-PROJECT_ACL = $(cat ${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json)
-
-IGNORED_FILES = "/dcf-dataservice/ignored_files_manifest.csv"
 EOL
 
-  g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${secret_folder}/apis_configs/dcf_dataservice/aws_creds_secret
+  g3kubectl create secret generic aws-creds-secret --from-file=credentials=${secret_folder}/apis_configs/dcf_dataservice/aws_creds_secret
   g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${secret_folder}/apis_configs/dcf_dataservice/gcloud-creds-secret
-  g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${secret_folder}/apis_configs/dcf_dataservice/creds.json
-  g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings
-  g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json
+  g3kubectl create secret generic dataservice-settings-secrets --from-file=dataservice_settings.json=${secret_folder}/apis_configs/dcf_dataservice/dataservice_settings.json
+  g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings.json=${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings.json
+
 fi
