9c01535e156ce0ea196beb8e5cff9bfa458c801a
Alex VanTol <avantol@uchicago.edu>
2023-10-27 15:05:39 -0500

    fix(ai): fix sync

--- gen3/bin/kube-setup-gen3-discovery-ai.sh
+++ gen3/bin/kube-setup-gen3-discovery-ai.sh
@@ -141,7 +141,7 @@ gen3_log_info "Setup complete, syncing configuration to bucket"
 
 if [ -d "$(dirname $(g3k_manifest_path))/gen3-discovery-ai/knowledge/chromadb" ]; then
   bucketName="$( (gen3 secrets decode 'gen3-discovery-ai-g3auto' 'storage_config.json' || echo ERROR) | jq -r .bucket)" || exit 1
-  aws s3 sync "$(dirname $(g3k_manifest_path))/gen3-discovery-ai/knowledge/" "s3://$bucketName/chromadb"
+  aws s3 sync "$(dirname $(g3k_manifest_path))/gen3-discovery-ai/knowledge/chromadb" "s3://$bucketName/chromadb"
 fi
 
 gen3 roll gen3-discovery-ai
