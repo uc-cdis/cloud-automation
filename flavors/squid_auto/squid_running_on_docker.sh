8b974b61851f356cdfc796ac45946b6d9bf9f44f
Edward Malinowski <edwardmalinowski@Eds-MacBook-Pro.attlocal.net>
2024-02-02 09:23:38 -0600

    chore(squid-al23): Updated squid bootstrap script to work with al23

--- flavors/squid_auto/squid_running_on_docker.sh
+++ flavors/squid_auto/squid_running_on_docker.sh
@@ -333,6 +333,18 @@ function main(){
       --volume ${SQUID_CACHE_DIR}:${SQUID_CACHE_DIR} \
       --volume ${SQUID_CONFIG_DIR}:${SQUID_CONFIG_DIR}:ro \
        quay.io/cdis/squid:${SQUID_IMAGE_TAG}
+
+  max_attempts=3
+  attempt_counter=0
+  while [ $attempt_counter -lt $max_attempts ]; do
+    sleep 10
+    if [[ -z "$(sudo lsof -i:3128)" ]]; then
+      echo "Squid not healthy, restarting."
+      docker restart squid
+    else
+      echo "Squid healthy"
+    fi
+  done
 }
 
 main
