99d4f6d8cf51d0de8d7bf236b727172198016e6f
Jawad Qureshi <qureshi@uchicago.edu>
2024-02-12 15:42:42 -0600

    Add ubuntu20 vpn scripts

--- files/openvpn_management_scripts/reset_totp_token.sh
+++ files/openvpn_management_scripts/reset_totp_token.sh
@@ -41,9 +41,9 @@ update_password_file() {
 
 generate_qr_code() {
     uuid=$(uuidgen)
-    qrcode_out=/var/www/qrcode/${uuid}.svg
+    qrcode_out=/var/www/qrcode/${uuid}.png
     string=$( python -c "import pyotp; print( pyotp.totp.TOTP('$totp_secret').provisioning_uri('$vpn_username', issuer_name='$CLOUD_NAME') )" )
-    $( python -c "import pyqrcode; pyqrcode.create('$string').svg('${qrcode_out}', scale=8)" )
+    $( python -c "import qrcode; qrcode.make('$string').save('${qrcode_out}', scale=8)" )
     vpn_creds_url="https://${FQDN}/$uuid.svg"
 }
 
