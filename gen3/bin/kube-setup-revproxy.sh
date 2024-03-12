d3e06e150c38ea6883d5f501d36aad6080b6b6ca
Aidan Hilt <ahilt@uchicago.edu>
2024-03-12 15:57:09 -0400

    Adding a gen3 db namespace to the temp files so they don't overlap

--- gen3/bin/kube-setup-revproxy.sh
+++ gen3/bin/kube-setup-revproxy.sh
@@ -114,8 +114,8 @@ done
 
 if g3k_manifest_lookup .argo.argo_server_service_url 2> /dev/null; then
   argo_server_service_url=$(g3k_manifest_lookup .argo.argo_server_service_url)
-  g3k_kv_filter "${scriptDir}/gen3.nginx.conf/argo-server.conf" SERVICE_URL "${argo_server_service_url}" > /tmp/argo-server-with-url.conf
-  filePath="/tmp/argo-server-with-url.conf"
+  g3k_kv_filter "${scriptDir}/gen3.nginx.conf/argo-server.conf" SERVICE_URL "${argo_server_service_url}" > /tmp/argo-server-with-url$(gen3 db namespace).conf
+  filePath="/tmp/argo-server-with-url$(gen3 db namespace).conf"
   if [[ -f "$filePath" ]]; then
     confFileList+=("--from-file" "$filePath")
   fi 
