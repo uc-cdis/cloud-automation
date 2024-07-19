c6023835393c6fe2f4cb5bbc8a9b8f7b8f028799
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2024-07-18 21:34:35 -0700

    Update python to 3.9 in jenkins pods

--- Docker/jenkins/Jenkins/install-python3.8.sh
+++
@@ -1,7 +0,0 @@
-wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tar.xz
-tar xf Python-3.8.0.tar.xz
-rm Python-3.8.0.tar.xz
-cd Python-3.8.0
-./configure 
-make 
-make altinstall
---
+++ Docker/jenkins/Jenkins2/install-python3.9.sh
@@ -0,0 +1,7 @@
+wget https://www.python.org/ftp/python/3.9.19/Python-3.9.19.tar.xz
+tar xf Python-3.9.19.tar.xz
+rm Python-3.9.19.tar.xz
+cd Python-3.9.19
+./configure 
+make 
+make altinstall
