dfe9451d5df731a2f3ba7d3368923374870ae1e4
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2024-09-12 10:06:53 -0500

    Add funnel service

---
+++ gen3/bin/kube-setup-funnel.sh
@@ -0,0 +1,83 @@
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/gen3setup"
+
+setup_funnel_infra() {
+  gen3_log_info "setting up funnel"
+
+  g3kubectl apply -f "${GEN3_HOME}/kube/services/funnel/funnel-service.yaml"
+  g3kubectl get services funnel --output=yaml | grep clusterIP # TODO parse it
+  funnelClusterIp=""
+  # TODO replace FUNNEL_SERVICE_CLUSTER_IP_PLACEHOLDER in funnel-worker-config.yaml
+
+  # TODO add to funnel-worker-config.yaml:
+  # AmazonS3:
+  #   Disabled: false
+  #   MaxRetries: 3
+  #   Key: ""
+  #   Secret: ""
+
+  g3kubectl create configmap funnel-config --from-file="${GEN3_HOME}/kube/services/funnel/funnel-server-config.yaml" --from-file="${GEN3_HOME}/kube/services/funnel/funnel-worker-config.yaml"
+
+  g3kubectl create serviceaccount funnel-sa --namespace default # TODO not sure about the namespace
+  g3kubectl create -f "${GEN3_HOME}/kube/services/funnel/funnel-role.yaml"
+  g3kubectl create -f "${GEN3_HOME}/kube/services/funnel/funnel-role-binding.yaml"
+
+
+#   if g3kubectl describe secret orthanc-g3auto > /dev/null 2>&1; then
+#     gen3_log_info "orthanc-g3auto secret already configured"
+#     return 0
+#   fi
+#   if [[ -n "$JENKINS_HOME" || ! -f "$(gen3_secrets_folder)/creds.json" ]]; then
+#     gen3_log_err "skipping db setup in non-adminvm environment"
+#     return 0
+#   fi
+
+#   # Setup config files that dicom-server consumes
+#   local secretsFolder="$(gen3_secrets_folder)/g3auto/orthanc"
+#   if [[ ! -f "$secretsFolder/orthanc_config_overwrites.json" ]]; then
+#     if [[ ! -f "$secretsFolder/dbcreds.json" ]]; then
+#       if ! gen3 db setup orthanc; then
+#         gen3_log_err "Failed setting up orthanc database for dicom-server"
+#         return 1
+#       fi
+#     fi
+#     if [[ ! -f "$secretsFolder/dbcreds.json" ]]; then
+#       gen3_log_err "dbcreds not present in Gen3Secrets/"
+#       return 1
+#     fi
+
+#     # TODO: generate and mount a cert
+#     # "SslEnabled": true,
+#     # "SslCertificate": ""
+#     cat - > "$secretsFolder/orthanc_config_overwrites.json" <<EOM
+# { 
+#   "AuthenticationEnabled": false,
+#   "PostgreSQL": {
+#     "EnableIndex": true,
+#     "EnableStorage": true,
+#     "Port": 5432,
+#     "Host": "$(jq -r .db_host < $secretsFolder/dbcreds.json)",
+#     "Database": "$(jq -r .db_database < $secretsFolder/dbcreds.json)",
+#     "Username": "$(jq -r .db_username < $secretsFolder/dbcreds.json)",
+#     "Password": "$(jq -r .db_password < $secretsFolder/dbcreds.json)",
+#     "IndexConnectionsCount": 5,
+#     "Lock": false
+#   },
+#   "PythonScript": "/etc/orthanc/authz_filter.py"
+# }
+# EOM
+#   fi
+#   gen3 secrets sync 'setup orthanc-g3auto secrets'
+}
+
+if ! setup_funnel_infra; then
+  gen3_log_err "kube-setup-funnel bailing out - database/config failed setup"
+  exit 1
+fi
+
+gen3 roll funnel
+g3kubectl apply -f "${GEN3_HOME}/kube/services/funnel/funnel-service.yaml"
+
+cat <<EOM
+The funnel service has been deployed onto the k8s cluster.
+EOM
