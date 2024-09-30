384f31c32a4e356a911e71848b8f8c6311704fca
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2024-09-30 10:49:46 -0500

    gen3workflow -> gen3-workflow

--- gen3/bin/kube-roll-all.sh
+++ gen3/bin/kube-roll-all.sh
@@ -261,10 +261,10 @@ else
   gen3_log_info "not deploying cohort-middleware - no manifest entry for .versions[\"cohort-middleware\"]"
 fi
 
-if g3k_manifest_lookup .versions.gen3workflow 2> /dev/null; then
-  gen3 kube-setup-gen3workflow &
+if g3k_manifest_lookup '.versions["gen3-workflow"]' 2> /dev/null; then
+  gen3 kube-setup-gen3-workflow &
 else
-  gen3_log_info "not deploying gen3workflow - no manifest entry for .versions.gen3workflow"
+  gen3_log_info "not deploying gen3-workflow - no manifest entry for .versions[\"gen3-workflow\"]"
 fi
 
 gen3 kube-setup-revproxy
