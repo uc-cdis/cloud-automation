134803ed0746ede1ad5ba7ab251a56b99d43ca11
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-11-20 10:54:05 -0700

    removing old squid file from cloud auto as it now lives in "base_images" repo" (#2675)

--- Docker/squid/certfix.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-if [[ -z $(cat /etc/ca-certificates.conf | grep '!mozilla/DST_Root_CA_X3.crt') ]] && [[ ! -z $(cat /etc/ca-certificates.conf | grep 'mozilla/DST_Root_CA_X3.crt') ]]; then
-  echo /etc/ca-certificates.conf | xargs sed -i 's/mozilla\/DST_Root_CA_X3.crt/!mozilla\/DST_Root_CA_X3.crt/g'
-  update-ca-certificates
-fi
