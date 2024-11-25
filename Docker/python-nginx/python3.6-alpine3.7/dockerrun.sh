67e452e8f5e15e86e80ef82ebed4c37709aaf8c1
Ed <emalinowski@uchicago.edu>
2024-11-25 05:40:08 -0600

    chore(remove-dd): removed references to datadog in cloud-auto

--- Docker/python-nginx/python3.6-alpine3.7/dockerrun.sh
+++ Docker/python-nginx/python3.6-alpine3.7/dockerrun.sh
@@ -86,16 +86,9 @@ if [ -f ./wsgi.py ] && [ "$GEN3_DEBUG" = "True" ]; then
   echo -e "\napplication.debug=True\n" >> ./wsgi.py
 fi
 
-if [[ -z $DD_ENABLED ]]; then
 (
   run uwsgi --ini /etc/uwsgi/uwsgi.ini
 ) &
-else
-echo "import=ddtrace.bootstrap.sitecustomize" >> /etc/uwsgi/uwsgi.ini
-(
-  ddtrace-run uwsgi --enable-threads --ini /etc/uwsgi/uwsgi.ini
-) &
-fi
 
 run nginx -g 'daemon off;'
 wait
