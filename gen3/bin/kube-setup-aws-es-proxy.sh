e2e9e1c0782cb63847f60e0ade54b9937a4efd05
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2025-01-16 13:04:05 -0700

    fixing syntax

--- gen3/bin/kube-setup-aws-es-proxy.sh
+++ gen3/bin/kube-setup-aws-es-proxy.sh
@@ -78,6 +78,9 @@ else
       if [ -n "$envname" ]; then
         ES_ENDPOINT="$(aws es describe-elasticsearch-domains --domain-names "${envname}"-gen3-metadata --query "DomainStatusList[*].Endpoints" --output text)" 
         ES_ARN="$(aws es describe-elasticsearch-domains --domain-names "${envname}"-gen3-metadata --query "DomainStatusList[*].ARN" --output text)"
+      else
+        deploy=false
+      fi
     else
       if [ -n "$envname" ]; then
         ES_ENDPOINT="$(aws es describe-elasticsearch-domains --domain-names "${envname}"-gen3-metadata-2 --query "DomainStatusList[*].Endpoints" --output text)" 
