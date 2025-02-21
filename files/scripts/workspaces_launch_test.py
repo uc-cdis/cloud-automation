72b79c881daf0d3b18bd82301478fcb73fea1e5b
BinamB <numb.baj@gmail.com>
2025-02-21 15:14:55 -0600

    add log

--- files/scripts/workspaces_launch_test.py
+++ files/scripts/workspaces_launch_test.py
@@ -115,7 +115,7 @@ class WorkspaceLaunchTest:
         
         logging.info("Completed all launch tests...")
         for result in final_result:
-            logging.info(json.dumps(result))
+            logging.info({"final_reuslt": json.dumps(result)})
 
     def start_workspace_launch_test(self, image_name, workspace_id):
 
