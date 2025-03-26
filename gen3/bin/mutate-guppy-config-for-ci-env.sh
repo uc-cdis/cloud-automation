0da3a34664bcfc7beb66e9a5f7cf23ce92e2d018
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2025-03-26 08:11:32 -0500

    Update mutate-guppy-config-for-ci-env.sh

--- gen3/bin/mutate-guppy-config-for-ci-env.sh
+++ gen3/bin/mutate-guppy-config-for-ci-env.sh
@@ -11,9 +11,8 @@ indexname=$1
 # gen3 mutate-guppy-config-for-ci-env jenkins
 
 g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
-sed -i "s/\(.*\)\"index\": \"\(.*\)_subject_alias\",$/\1\"index\": \"${indexname}_subject_alias\",/" original_guppy_config.yaml
-sed -i "/\"index\": \".*file_alias\"/ { /midrc/! s/\"index\": \"\(.*\)_file_alias\"/\"index\": \"${indexname}_file_alias\"/ }" original_guppy_config.yaml
-sed -i "s/\(.*\)\"config_index\": \"\(.*\)_configs_alias\",$/\1\"config_index\": \"${indexname}_configs_alias\",/" original_guppy_config.yaml
+sed -i "s/\(.*\)\"index\": \"\(.*\)_subject\",$/\1\"index\": \"${indexname}_subject_alias\",/" original_guppy_config.yaml
+sed -i "/\"index\": \".*file_alias\"/ { /midrc/! s/\"index\": \"\(.*\)_file\"/\"index\": \"${indexname}_file_alias\"/ }" original_guppy_config.yaml
 
 g3kubectl delete configmap manifest-guppy
 g3kubectl apply -f original_guppy_config.yaml
