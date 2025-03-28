63f4c46176ac2a04d1d8bb416ad67a8dfc8c5b35
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2025-03-28 06:32:48 -0500

    Update mutate-guppy-config-for-ci-env.sh

--- gen3/bin/mutate-guppy-config-for-ci-env.sh
+++ gen3/bin/mutate-guppy-config-for-ci-env.sh
@@ -10,11 +10,22 @@ indexname=$1
 # how to run:
 # gen3 mutate-guppy-config-for-ci-env jenkins
 
-g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
-sed -i "s/\(.*\)\"index\": \"\(.*\)_subject_alias\",$/\1\"index\": \"${indexname}_subject_alias\",/" original_guppy_config.yaml
-sed -i "/\"index\": \".*file_alias\"/ { /midrc/! s/\"index\": \"\(.*\)_file_alias\"/\"index\": \"${indexname}_file_alias\"/ }" original_guppy_config.yaml
-sed -i "s/\(.*\)\"config_index\": \"\(.*\)_configs_alias\",$/\1\"config_index\": \"${indexname}_configs_alias\",/" original_guppy_config.yaml
-
+if [ "$indexname" == "jenkins" ]; then
+ echo "Executing commands for jenkins..."
+ g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
+ sed -i "s/\(.*\)\"index\": \"\(.*\)_subject\",$/\1\"index\": \"${indexname}_subject_alias\",/" original_guppy_config.yaml
+ sed -i "/\"index\": \".*file\"/ { /midrc/! s/\"index\": \"\(.*\)_file\"/\"index\": \"${indexname}_file_alias\"/ }" original_guppy_config.yaml
+ sed -i "s/\(.*\)\"config_index\": \"\(.*\)-config\",$/\1\"config_index\": \"${indexname}_configs_alias\",/" original_guppy_config.yaml
+else
+ echo "Executing other commands..."
+ g3kubectl get configmap manifest-guppy -o yaml > original_guppy_config.yaml
+ yaml_content=$(kubectl get cm etl-mapping -o jsonpath='{.data.etlMapping\.yaml}')
+ json_content=$(python -c "import sys, yaml, json; print(json.dumps(yaml.safe_load(sys.stdin.read())))" <<< "$yaml_content")
+ subject_index=$(echo "$json_content" | jq -r '.mappings[].name' | grep subject)
+ file_index=$(echo "$json_content" | jq -r '.mappings[].name' | grep file)
+ sed -i "s/\(.*\)\"index\": \"\(.*\)_subject_alias\",$/\1\"index\": \"${subject_index}\",/" original_guppy_config.yaml
+ sed -i "/\"index\": \".*file_alias\"/ { /midrc/! s/\"index\": \"\(.*\)_file_alias\"/\"index\": \"${file_index}\"/ }" original_guppy_config.yaml
+fi
 g3kubectl delete configmap manifest-guppy
 g3kubectl apply -f original_guppy_config.yaml
 gen3 roll guppy
