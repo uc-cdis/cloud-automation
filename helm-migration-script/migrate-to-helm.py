11ef25763f9077ae4fd5a77e68a83abe252aad55
Aidan Hilt <ahilt@uchicago.edu>
2025-04-29 13:30:34 -0400

    :eyeroll_emoji:

--- helm-migration-script/migrate-to-helm.py
+++ helm-migration-script/migrate-to-helm.py
@@ -376,7 +376,6 @@ def process_g3auto_secrets(gen3_secrets_path: str):
 # Dear god, is this fragile. We're all but locked into running on admin VMs (which is fine)
 def get_commons_name():
   commons_name = subprocess.run("kubectl config view --minify | yq .contexts[0].context.namespace | tr -d '\"'", shell=True, stdout=open(os.devnull, 'wb')).stdout
-  commons_name="aidan"
   if commons_name == "default":
      commons_name = subprocess.run("echo $vpc_name")
 
