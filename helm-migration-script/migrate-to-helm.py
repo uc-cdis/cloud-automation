60347675e7bdf6aa629307442d25361b219ea40a
Aidan Hilt <ahilt@uchicago.edu>
2025-05-02 16:56:29 -0400

    What?

--- helm-migration-script/migrate-to-helm.py
+++ helm-migration-script/migrate-to-helm.py
@@ -434,7 +434,8 @@ def process_fence_config(gen3_secrets_path: str):
     if os.path.getsize(FENCE_GOOGLE_STORAGE_CREDS_PATH):
       with open(FENCE_GOOGLE_STORAGE_CREDS_PATH) as file:
         # May I be forgiven for what I am about to do here
-        final_structure = {"fence_google_app_creds_secret.json": json.load(file.read())}
+        original_contents = file.read()
+        final_structure = {"fence_google_app_creds_secret.json": json.load(original_contents)}
         upload_secret(f"{commons_name}-fence-google-storage-creds", json.dumps(final_structure))
     else:
       print(f"A file exists at {FENCE_GOOGLE_STORAGE_CREDS_PATH}, but it's empty. Skipping")
