a7f1b68105379363f6b82a310cc2cb1c47aea0b9
Jian <52763034+tianj7@users.noreply.github.com>
2025-05-29 14:49:17 -0500

    Update data replicate script (#2787)
    * update data replicate script
    
    * improve data-replicate script
    
    * add comments
    
    * revert change to troubleshoot ci
    
    * revert to most recent change
    
    * recommit change
    
    ---------
    
    Co-authored-by: Maribelle Hannah Gomez <maribellehgomez@gmail.com>

--- gen3/bin/kube-setup-data-replicate.sh
+++ gen3/bin/kube-setup-data-replicate.sh
@@ -34,7 +34,13 @@ if [[ -d ${secret_folder}/apis_configs/dcf_dataservice ]]; then
     return 1
   fi
 
-  cp ${GEN3_MANIFEST_HOME}/${hostname}/manifests/datarefresh/GDC_project_map.json ${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json
+  if [ -e ${GEN3_MANIFEST_HOME}/${hostname}/manifests/datarefresh/GDC_project_map.json ]
+    then cp ${GEN3_MANIFEST_HOME}/${hostname}/manifests/datarefresh/GDC_project_map.json ${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json
+  else
+    echo "Warning: ${GEN3_MANIFEST_HOME}/${hostname}/manifests/datarefresh/GDC_project_map.json IS NOT FOUND!"
+    echo "Please make sure the file is in the correct location. It is necessary for data replication jobs."
+    exit 1
+  fi
 
   GDC_TOKEN=$(cat ${secret_folder}/apis_configs/dcf_dataservice/creds.json | jq '.GDC_TOKEN')
   INDEXD_CRED=$(cat ${secret_folder}/apis_configs/dcf_dataservice/creds.json | jq '.INDEXD')
@@ -50,11 +56,13 @@ PROJECT_ACL = $(cat ${secret_folder}/apis_configs/dcf_dataservice/GDC_project_ma
 
 IGNORED_FILES = "/dcf-dataservice/ignored_files_manifest.csv"
 EOL
-
-  g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${secret_folder}/apis_configs/dcf_dataservice/aws_creds_secret
-  g3kubectl create secret generic dcf-aws-fence-creds-secret --from-file=credentials=${secret_folder}/apis_configs/dcf_dataservice/aws_fence_bot_secret
-  g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${secret_folder}/apis_configs/dcf_dataservice/gcloud-creds-secret
-  g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${secret_folder}/apis_configs/dcf_dataservice/creds.json
-  g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings
-  g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json
+# "|| true" is added to make the script continue to try to create other secrets if one or more create command fails 
+# So we know which secrets are failing to create all at once and need solutions
+# Error messages will still show from the gen3 commands
+  g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${secret_folder}/apis_configs/dcf_dataservice/aws_creds_secret || true
+  g3kubectl create secret generic dcf-aws-fence-creds-secret --from-file=credentials=${secret_folder}/apis_configs/dcf_dataservice/aws_fence_bot_secret || true
+  g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${secret_folder}/apis_configs/dcf_dataservice/gcloud-creds-secret || true
+  g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${secret_folder}/apis_configs/dcf_dataservice/creds.json || true
+  g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${secret_folder}/apis_configs/dcf_dataservice/dcf_dataservice_settings || true
+  g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${secret_folder}/apis_configs/dcf_dataservice/GDC_project_map.json || true
 fi
